1a72b79bdb9a825d15826d7cea9509a2ae5af396
Shyam Visamsetty <svisamsetty@navaratan.com>
2014-03-25 18:38:20 +0000

    orders_controller.rb edited online with Bitbucket added sales record number instead of increment id.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -191,12 +191,13 @@ class OrdersController < ApplicationController
         end
       end
       seller_list.orderArray.each do |order|
-        if Order.where(:increment_id=>order.orderID).length == 0 &&
+        if !order.shippingDetails.nil? && 
+            Order.where(:increment_id=>order.shippingDetails.sellingManagerSalesRecordNumber).length == 0 &&
             order.checkoutStatus.status == 'Complete'
           @order = Order.new
           @order.status = 'awaiting'
           @order.store = @store
-          @order.increment_id = order.orderID
+          @order.increment_id = order.order.shippingDetails.sellingManagerSalesRecordNumber
           @order.order_placed_time = order.createdTime
 
           order.transactionArray.each do |transaction|
