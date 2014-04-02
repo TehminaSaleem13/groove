eeeabff09b26072569889068af7288bb871bcd7c
Piyush <mail@ofpiyush.in>
2014-04-03 02:15:10 +0530

    Added capability of running from everywhere

--- restart.sh
+++ restart.sh
@@ -5,10 +5,12 @@ cd ~/groove
 git pull origin master
 exit
 EOF
-
+cd /home/groovepacker/groove
 RAILS_ENV=production rvmsudo bundle install
 
 RAILS_ENV=production rvmsudo rake db:migrate
 RAILS_ENV=production rvmsudo rake assets:precompile
 
 sudo service nginx restart
+
+cd -
