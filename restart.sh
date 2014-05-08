a39186c82d8faf66e6ed8c341dfd836457fe2378
Piyush <mail@ofpiyush.in>
2014-05-09 02:41:39 +0530

    changed restart script to run from groovepacker user entirely

--- restart.sh
+++ restart.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
+sudo chown groovepacker:groovepacker /home/groovepacker/groove -R
+
 sudo su groovepacker <<'EOF'
+source /usr/local/rvm/scripts/rvm
+
 cd ~/groove
+git remote set-url origin git@bitbucket.org:jonnyclean/groovepacker.git
+
 git pull origin master
+
+RAILS_ENV=production bundle install --deployment
+RAILS_ENV=production rake db:migrate
+RAILS_ENV=production rake assets:precompile
+
 exit
 EOF
-cd /home/groovepacker/groove
-RAILS_ENV=production rvmsudo bundle install
-
-rvmsudo rake db:migrate RAILS_ENV=production
-rvmsudo rake assets:precompile RAILS_ENV=production
 
 sudo service nginx restart
-
-cd -
