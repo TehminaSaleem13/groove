7151e72abff3faffea50087f7003c6982f025027
Piyush <mail@ofpiyush.in>
2014-05-10 23:00:58 +0530

    added delayed job to script

--- restart.sh
+++ restart.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo service nginx stop
+
 sudo chown groovepacker:groovepacker /home/groovepacker/groove -R
 
 sudo su groovepacker <<'EOF'
@@ -10,11 +12,13 @@ git remote set-url origin git@bitbucket.org:jonnyclean/groovepacker.git
 
 git pull origin master
 
+RAILS_ENV=production script/delayed_job stop
 RAILS_ENV=production bundle install --deployment
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
+RAILS_ENV=production script/delayed_job start
 
 exit
 EOF
 
-sudo service nginx restart
+sudo service nginx start
