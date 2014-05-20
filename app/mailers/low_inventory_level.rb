8a0613d63009ef850067863f04ef326d51031a7d
nmora <nmora@navaratan.com>
2014-05-20 23:37:44 +0530

    add support for inventory email layout styling GROOV-113 #time 1h 30m add initial support for inventory email layout styling

--- app/mailers/low_inventory_level.rb
+++ app/mailers/low_inventory_level.rb
@@ -6,16 +6,16 @@ class LowInventoryLevel < ActionMailer::Base
   	mail to: general_settings.low_inventory_email_address, 
   		subject: "GroovePacker Low Inventory Alert"
 
-  	date = DateTime.now
-  	date = date + 1.day
-  	job_scheduled = false
+  # 	date = DateTime.now
+  # 	date = date + 1.day
+  # 	job_scheduled = false
 
-  	while !job_scheduled do
-	   if general_settings.should_send_email(date)
-	   	job_scheduled = general_settings.schedule_job(date)
-	   else
-	   	date = date + 1.day
-	   end
-		end
+  # 	while !job_scheduled do
+	 #   if general_settings.should_send_email(date)
+	 #   	job_scheduled = general_settings.schedule_job(date)
+	 #   else
+	 #   	date = date + 1.day
+	 #   end
+		# end
   end
 end
