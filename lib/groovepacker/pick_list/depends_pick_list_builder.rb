d9dbe4e91a529dde6c81d92ff0a4c95d5fe40368
Kalakar Sahoo <ksahoo@navaratan.com>
2014-06-06 18:21:48 +0530

    analyzed the flaw for the products with kit_parsing as depends FROOV-160

--- lib/groovepacker/pick_list/depends_pick_list_builder.rb
+++ lib/groovepacker/pick_list/depends_pick_list_builder.rb
@@ -2,8 +2,15 @@ module Groovepacker
 	module PickList
 		class DependsPickListBuilder < PickListBuilder
 			def build(qty, product, pick_list, inventory_warehouse_id)
-					
 				single_pick_list = []
+				individual_pick_list = []
+
+				# check if sku is found or not, get index if found
+
+
+			  # if sku is found, then single_pick_list = pick_list[index]['single']
+
+
 				single_pick_list_builder = SinglePickListBuilder.new
 				single_pick_list = single_pick_list_builder.build(
 					qty,
@@ -11,7 +18,6 @@ module Groovepacker
 					single_pick_list, 
 					inventory_warehouse_id) 
 				
-				individual_pick_list = []
 				individual_pick_list_builder = IndividualPickListBuilder.new
 				individual_pick_list = individual_pick_list_builder.build(
 					qty,
@@ -19,9 +25,14 @@ module Groovepacker
 					individual_pick_list, 
 					inventory_warehouse_id)
 				
+				#if sku is not found
 				pick_list.push({
+					"sku" => sku,
 					"single" => single_pick_list,
 				  "individual" => individual_pick_list})
+				#else
+				#pick_list[index]['single']= single_pick_list
+
 				pick_list
 			end
 		end
