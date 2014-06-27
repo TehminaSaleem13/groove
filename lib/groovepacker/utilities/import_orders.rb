e734f7a69c7c1b28d42ddb66e47969683ca1b7b5
Kalakar Sahoo <ksahoo@navaratan.com>
2014-06-27 13:21:42 +0530

    Manipulated status of order_import_summaries GROOV-93

--- lib/groovepacker/utilities/import_orders.rb
+++ lib/groovepacker/utilities/import_orders.rb
@@ -3,41 +3,57 @@ class ImportOrders
 		# @result = Hash.new
 		# we will remove all the jobs pertaining to import which are not started
 
-		order_import_summaries = OrderImportSummary.where(status: 'not started')
-		if !order_import_summaries.empty?
-			order_import_summary = order_import_summaries.first
+		# order_import_summaries = OrderImportSummary.where(status: 'not started')
+		# if !order_import_summaries.empty?
+		# 	order_import_summary = order_import_summaries.first
 
-			puts "order_import_summary:"+order_import_summary.inspect
-			order_import_summary.status = 'in progress'
-			order_import_summary.save
-		end
+		# 	puts "order_import_summary:"+order_import_summary.inspect
+		# 	order_import_summary.status = 'in progress'
+		# 	order_import_summary.save
+		# end
 
 		# we will also remove all the import summary which are not started.
-		# order_import_summaries = OrderImportSummary.all
-		# order_import_summaries.each do |order_import_summary|
-		# 	if order_import_summary.status == 'not started'
-		# 		order_import_summary.delete
-		# 	end
-		# end
-		puts "inside import_order......"
-		sleep 10
-		puts "awaken up..."
+		order_import_summaries = OrderImportSummary.where(status: ['not started','completed'])
+		if !order_import_summaries.empty?
+			order_import_summaries.each do |order_import_summary|
+				if order_import_summary == order_import_summaries.first
+					order_import_summary.status = 'in progress'
+					order_import_summary.save
+				end
+				if order_import_summary.status != 'in progress'
+					order_import_summary.delete
+				end
+			end
+		end
 
-		
+		stores = Store.where("status = '1' AND store_type != 'system'")
+		if stores.length != 0	
+			stores.each do |store|
+				if store.store_type == 'Amazon'
+      		# context = Groovepacker::Store::Context.new(
+        #  	Groovepacker::Store::Handlers::AmazonHandler.new(store))
+       	# 	result = context.import_orders
+       	# 	puts result.inspect
+     		elsif store.store_type == 'Ebay1'
+       		context = Groovepacker::Store::Context.new(
+         	Groovepacker::Store::Handlers::EbayHandler.new(store))
+       		puts context.import_orders.inspect
+     		end
+				# result['imported_orders'] = importorders
+				# if result['imported_orders'].length != 0
+				# 	import_item = ImportItem.new
+				# 	import_item.store_id = store.id
+				# 	import_item.pervious_imported = result['imported_orders']['previous_imported']
+				# 	import_item.success_imported = result['imported_orders']['success_imported']
 
-		# result['stores'] = getactivestores
-		# if result['stores'].length != 0
-		# 	result['stores'].each do |store|
-		# 		result['imported_orders'] = importorders
-		# 		if result['imported_orders'].length != 0
-		# 			import_item = ImportItem.new
-		# 			import_item.store_id = store.id
-		# 			import_item.pervious_imported = result['imported_orders']['previous_imported']
-		# 			import_item.success_imported = result['imported_orders']['success_imported']
-
-		# 		end
-		# 		import_items.save
-		# 	end
-		# end
+				# end
+				# import_items.save
+			end
+		end
+		order_import_summaries = OrderImportSummary.all
+		if !order_import_summaries.first.nil?
+			order_import_summaries.first.status = 'completed'
+			order_import_summaries.first.save
+		end
 	end
 end
