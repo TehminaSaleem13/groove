6fdd7df5d22050f3a2c59d01f2a146610ea941bb
Kalakar Sahoo <ksahoo@navaratan.com>
2014-06-28 21:52:55 +0530

    Displayed required information in the popover GROOV-93

--- app/assets/javascripts/controllers/appCtrl.js
+++ app/assets/javascripts/controllers/appCtrl.js
@@ -1,50 +1,44 @@
 groovepacks_controllers.
-    controller('appCtrl', [ '$rootScope', '$scope', '$http', '$timeout', '$stateParams', '$location', '$state', '$cookies','auth','notification','importOrders',
-        function( $rootScope, $scope, $http, $timeout, $stateParams, $location, $state, $cookies,auth,notification,importOrders) {
-            $scope.$on("user-data-reloaded", function(){
-                $scope.current_user = auth;
-            });
+    controller('appCtrl', [ '$rootScope', '$scope', '$http', '$timeout', '$interval', '$stateParams', '$location', '$state', '$cookies','auth','notification','importOrders',
+        function( $rootScope, $scope, $http, $timeout, $interval, $stateParams, $location, $state, $cookies,auth,notification,importOrders) {
+        $scope.$on("user-data-reloaded", function(){
+            $scope.current_user = auth;
+        });
 
-            $scope.$on("editing-a-var",function(event,data) {
-                $scope.current_editing = data.ident;
-            });
-            $scope.notify = function(msg,type) {
-                notification.notify(msg,type);
-            }
-            $rootScope.import_in_progress = true;
-            $rootScope.import_summary = {};
-            $rootScope.import_items = [];
-            var myscope = {};
-            //call a method at timeout of say 60 seconds.
-            myscope.get_status = function() {
-                $http.get('/orders/import_status.json').success(function(data) {
-                    if (data['import_summary']['status'] == 'in_progress') {
-                        $rootScope.import_in_progress = true;
-                        $rootScope.import_summary.last_import_time = data['import_summary']['last_import_time'];
+        $scope.$on("editing-a-var",function(event,data) {
+            $scope.current_editing = data.ident;
+        });
+        $scope.notify = function(msg,type) {
+            notification.notify(msg,type);
+        }
+        $rootScope.import_summary = {};
+        var myscope = {};
+        //call a method at timeout of say 60 seconds.
+        myscope.get_status = function() {
+            $http.get('/orders/import_status.json').success(
+              function(response) {
+                if (response.status) {
+                  $rootScope.import_summary = response.data.import_summary;
+                }
+            }).error(function(data) {});
+        }
+        myscope.get_status();      
 
-                    }
-                    else {
-                        $rootScope.import_summary.last_import_time = data['import_summary']['last_import_time'];
-                        $rootScope.import_in_progress = false;
-                    }
-                }).error(function(data) {});
-            }
-            myscope.get_status();
-            // setTimeout(get_status, 60);
-            $scope.get_import_summary = function() {
-                console.log('importing mouse over');
+        $interval(myscope.get_status, 2000);
 
-                
-                //console.log($(".popover-order a"));
-                $("#ordersitem a").popover({
-                    placement: 'bottom',
-                    html: true, 
-                    content: function() {
-                      return $('#popoverExampleTwoHiddenContent').html();
-                    },
-                    title: function() {
-                      return $('#popoverExampleTwoHiddenTitle').html();
-                    }                    
-                });
-            }
+        // setTimeout(get_status, 60);
+        $scope.get_import_summary = function() {
+          console.log('importing mouse over');
+          //console.log($(".popover-order a"));
+          $("#ordersitem a").popover({
+              placement: 'bottom',
+              html: true, 
+              content: function() {
+                return $('#popoverExampleTwoHiddenContent').html();
+              },
+              title: function() {
+                return $('#popoverExampleTwoHiddenTitle').html();
+              }                    
+          });
+      }
 }]);
