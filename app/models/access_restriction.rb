28229e85d45cfa263d58aded8f377204074e09e6
Kalakar Sahoo <ksahoo@navaratan.com>
2014-07-31 21:59:43 +0530

    modified webhook.rb GROOV-145

--- app/models/access_restriction.rb
+++ app/models/access_restriction.rb
@@ -1,4 +1,4 @@
 class AccessRestriction < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :tenant_id, :num_users, :num_shipments, :num_import_sources
   has_one :tenant
 end
