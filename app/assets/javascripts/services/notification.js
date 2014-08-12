d611f1bb2e7f8049a7f5a0936a263d0fc3a49957
Piyush <mail@ofpiyush.in>
2014-08-13 02:52:07 +0530

    Previous notification is removed before new one is created in case of duplicates

--- app/assets/javascripts/services/notification.js
+++ app/assets/javascripts/services/notification.js
@@ -7,27 +7,37 @@ groovepacks_services.factory("notification", ['$timeout','$rootScope',function($
         default: 0
     };
     var notifications = {};
-    var queue_remove =function(notif_id) {
+
+    var delete_notif = function(id) {
+        delete notifications[id];
+        $rootScope.$broadcast('notification',{data:notifications});
+    };
+
+    var queue_remove = function(notif_id) {
         $timeout(
-            function() {
-                delete notifications[notif_id];
-                $rootScope.$broadcast('notification',{data:notifications});
-            },
+            function() {delete_notif(notif_id);},
             //timeout based on number of notifications and
             Object.keys(notifications).length*1500 + (notifications[notif_id].msg.length*50)
         );
     };
-    var notify =  function (msg,type) {
+
+    var notify =  function (msgs,type) {
         if(typeof type != "number" ||  typeof notif_types[type] == "undefined") {
             type = notif_types["default"];
         }
         var alert = notif_types[type];
-        if(typeof msg == "string") {
-            msg = [msg];
+        if(typeof msgs == "string") {
+            msgs = [msgs];
         }
-        for(i in msg) {
+        for(var i = 0; i< msgs.length; i++) {
+            for (var notif_id in notifications) {
+                if(notifications.hasOwnProperty(notif_id) && notifications[notif_id].msg == msgs[i]) {
+                    delete_notif(notif_id);
+                    break;
+                }
+            }
             id++;
-            notifications[id] = {show:true , alert: alert, msg: msg[i]};
+            notifications[id] = {show:true , alert: alert, msg: msgs[i]};
             queue_remove(id);
         }
         $rootScope.$broadcast('notification',{data:notifications});
