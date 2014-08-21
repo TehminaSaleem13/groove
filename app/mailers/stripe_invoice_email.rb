da00dc22bedb6cc405e8f0c79dc43e0cebb86f14
Kalakar Sahoo <ksahoo@navaratan.com>
2014-08-21 20:37:31 +0530

    modified stripe_invoice mailer GROOV-145

--- app/mailers/stripe_invoice_email.rb
+++ app/mailers/stripe_invoice_email.rb
@@ -2,9 +2,11 @@ class StripeInvoiceEmail < ActionMailer::Base
   default from: "app@groovepacker.com"
   
   def send_invoice(invoice, tenant_name)
-    Apartment::Tenant.switch(tenant)
-    tenant = Tenat.where(name: tenant_name).first unless Tenat.where(name: tenant_name).nil?
-
+    Apartment::Tenant.switch()
+    tenant = Tenant.where(name: tenant_name).first unless Tenant.where(name: tenant_name).nil?
+    Apartment::Tenant.switch(tenant_name)
+    @tenant_name = tenant_name
+    @invoice = invoice
   	mail to: tenant.subscription.email, 
   		subject: "GroovePacker Invoice Email"
   end
