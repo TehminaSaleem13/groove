7f939951bb86b8b33bc82d24e435df2bc3781b0d
Shyam Visamsetty <svisamsetty@navaratan.com>
2014-09-18 20:19:25 +0530

    Fix issue with ship station orders, order duplication and importing warehouse location for SS products.

--- lib/groovepacker/store/importers/shipstation/products_importer.rb
+++ lib/groovepacker/store/importers/shipstation/products_importer.rb
@@ -59,13 +59,13 @@ module Groovepacker
           def set_product_fields(product, ssproduct,credential)
             result = false 
             product.name = ssproduct.name
-            product.inv_wh1 = ssproduct.warehouse_location
 
             unless credential.store.nil? or 
               credential.store.inventory_warehouse_id.nil? or 
               product.product_inventory_warehousess.pluck(:inventory_warehouse_id).include?(credential.store.inventory_warehouse_id) then
               inv_wh = ProductInventoryWarehouses.new
               inv_wh.inventory_warehouse_id = credential.store.inventory_warehouse_id
+              inv_wh.location_primary = ssproduct.warehouse_location
               product.product_inventory_warehousess << inv_wh
             end
  
