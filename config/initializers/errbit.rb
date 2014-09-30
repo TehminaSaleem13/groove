20fb014c383cd843578ceb5f925b56f02d3e6774
Piyush <mail@ofpiyush.in>
2014-10-01 00:52:26 +0530

    Errbit production settings

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,6 @@
-  Airbrake.configure do |config|
-    config.api_key = '3e545c7d440c1b46e79e4e141e75bf8f'
-    config.host    = 'errbit.dev'
-    config.port    = 3000
-    config.secure  = config.port == 443
-  end
+Airbrake.configure do |config|
+  config.api_key = '2a7a92294f148823be87621957298e72'
+  config.host    = 'groove-errbit-app-server'
+  config.port    = 80
+  config.secure  = config.port == 443
+end
