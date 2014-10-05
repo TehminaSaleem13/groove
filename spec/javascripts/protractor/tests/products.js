452b4734b9aaaba8a7edb741aa6234e9613f3d9b
Piyush <mail@ofpiyush.in>
2014-10-05 07:41:15 +0530

    Added more products tests

--- spec/javascripts/protractor/tests/products.js
+++ spec/javascripts/protractor/tests/products.js
@@ -31,31 +31,88 @@ describe('Products:',function() {
         });
     });
     describe('Modal:',function() {
+        var name_text = element(by.binding('products.single.basicinfo.name'));
         describe('Name:',function() {
             var name = {};
-
             name.input = element(by.model('products.single.basicinfo.name'));
-            name.text = element(by.binding('products.single.basicinfo.name'));
             name.default = 'New Product';
             name.new = 'Product Test 1';
 
             it('Product should have the name New Product',function() {
                 expect(name.input.getAttribute('value')).toEqual(name.default);
-                expect(name.text.getText()).toEqual(name.default);
+                expect(name_text.getText()).toEqual(name.default);
             });
 
             it('Editing name should work',function() {
                 name.input.clear();
                 name.input.sendKeys(name.new);
                 expect(name.input.getAttribute('value')).toEqual(name.new);
-                expect(name.text.getText()).toEqual(name.new);
+                expect(name_text.getText()).toEqual(name.new);
             });
 
             it('Reloading the modal should\'ve saved the name', function() {
-                name.text.click();
+                name_text.click();
                 browser.refresh();
-                expect(name.text.getText()).toEqual(name.new);
+                expect(name_text.getText()).toEqual(name.new);
                 expect(name.input.getAttribute('value')).toEqual(name.new);
+            });
+        });
+        
+        describe('Sku:',function(){
+            var sku = {};
+        });      
+
+        describe('Special Instructions:',function() {
+            var special_instructions = {};
+            special_instructions.textarea = element(by.model('products.single.basicinfo.spl_instructions_4_packer'));
+            special_instructions.default = '';
+            special_instructions.new = 'Test Instructions';
+
+            it('Product should have empty test instructions',function() {
+                expect(special_instructions.textarea.getAttribute('value')).toEqual(special_instructions.default);
+            });
+
+            it('Editing name should work',function() {
+                special_instructions.textarea.clear();
+                special_instructions.textarea.sendKeys(special_instructions.new);
+                expect(special_instructions.textarea.getAttribute('value')).toEqual(special_instructions.new);
+            });
+
+            it('Reloading the modal should\'ve saved the test instructions', function() {
+                name_text.click();
+                browser.refresh();
+                expect(special_instructions.textarea.getAttribute('value')).toEqual(special_instructions.new);
+            });
+        });
+        
+        describe('Packing Placement:',function() {
+            var packing_placement = {};
+            packing_placement.input = element(by.model('products.single.basicinfo.packing_placement'));
+            
+            packing_placement.default = '50';
+            packing_placement.new = '55';
+            packing_placement.bad ='asd';
+
+            
+
+            it('Editing packing placement should work',function() {
+                packing_placement.input.clear();
+                packing_placement.input.sendKeys(packing_placement.new);
+                expect(packing_placement.input.getAttribute('value')).toEqual(packing_placement.new);
+            });
+
+            it('Reloading the modal should\'ve saved the packing placement value', function() {
+                name_text.click();
+                browser.refresh();
+                expect(packing_placement.input.getAttribute('value')).toEqual(packing_placement.new);
+            });
+
+            it('Feeding non numbers to packing placement should reset to default',function() {
+                packing_placement.input.clear();
+                packing_placement.input.sendKeys(packing_placement.bad);
+                name_text.click();
+                browser.refresh();
+                expect(packing_placement.input.getAttribute('value')).toEqual(packing_placement.default);
             });
         });
 
