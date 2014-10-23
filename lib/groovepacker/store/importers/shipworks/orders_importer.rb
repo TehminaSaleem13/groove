6f08a8c4443ce7a6341eda641735257326430693
Shyam Visamsetty <svisamsetty@navaratan.com>
2014-10-23 18:40:06 +0530

    Fix issue with helper methods definition

--- lib/groovepacker/store/importers/shipworks/orders_importer.rb
+++ lib/groovepacker/store/importers/shipworks/orders_importer.rb
@@ -49,56 +49,6 @@ module Groovepacker
               order_m.update_order_status
             end
 
-            private
-
-            def get_ship_address(order)
-              result = nil
-
-              order["Address"].each do |addr|
-                if addr["type"] == "ship"
-                  result = addr
-                  break
-                end
-              end
-
-              result
-            end
-
-            def import_product(item, store)
-              product = Product.create(
-                store: store, 
-                name: item["Name"],
-                weight: item["Weight"],
-                store_product_id: item["ID"]
-              )
-
-              #SKU
-              if item["SKU"].nil?
-                sku = ProductSku.get_temp_sku
-              else
-                sku = item["SKU"]
-              end
-              product.product_skus.create(sku: sku)
-
-              #Barcodes
-              product.product_barcodes.create(
-                barcode: item["UPC"]
-              ) unless item["UPC"].nil?
-
-              #Images
-              product.product_images.create(
-                image: item["Image"]
-              ) unless item["Image"].nil?
-
-              #Location
-              product.product_inventory_warehousess.create(
-                inventory_warehouse: store.inventory_warehouse,
-                location_primary: item["Location"]
-              ) unless item["Location"].nil?
-
-              product.set_product_status
-            end
-
             #create order items
             # shipstation_order.postcode = order.ship_postal_code unless order.ship_postal_code.nil?
             # shipstation_order.country = order.ship_country_code 
@@ -106,6 +56,55 @@ module Groovepacker
             # shipstation_order.order_total = order.order_total
             # shipstation_order.notes_from_buyer = order.notes_from_buyer unless order.notes_from_buyer.nil?
             # shipstation_order.weight_oz = order.weight_oz unless order.weight_oz.nil?
+          end
+
+          private
+          def get_ship_address(order)
+            result = nil
+
+            order["Address"].each do |addr|
+              if addr["type"] == "ship"
+                result = addr
+                break
+              end
+            end
+
+            result
+          end
+
+          def import_product(item, store)
+            product = Product.create(
+              store: store, 
+              name: item["Name"],
+              weight: item["Weight"],
+              store_product_id: item["ID"]
+            )
+
+            #SKU
+            if item["SKU"].nil?
+              sku = ProductSku.get_temp_sku
+            else
+              sku = item["SKU"]
+            end
+            product.product_skus.create(sku: sku)
+
+            #Barcodes
+            product.product_barcodes.create(
+              barcode: item["UPC"]
+            ) unless item["UPC"].nil?
+
+            #Images
+            product.product_images.create(
+              image: item["Image"]
+            ) unless item["Image"].nil?
+
+            #Location
+            product.product_inventory_warehousess.create(
+              inventory_warehouse: store.inventory_warehouse,
+              location_primary: item["Location"]
+            ) unless item["Location"].nil?
+
+            product.set_product_status
           end
         end
       end
