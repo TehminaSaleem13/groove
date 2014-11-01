d85ecf9062d86bf4ce5706b4dd7917469713d874
Piyush <mail@ofpiyush.in>
2014-11-01 23:41:49 +0530

    Ready the code for testing with sockets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -114,5 +114,5 @@ Groovepacks::Application.configure do
   ENV['STRIPE_API_KEY'] = "sk_test_4QS2OJ8BkMWcuzCWrHrKGlz9"
   ENV['STRIPE_PUBLIC_KEY'] = "pk_test_4QS2UN3famIPlHtp2Q7ykpDf"
 
-  ENV['HOST_NAME'] = 'localpacker.com:3001'
+  ENV['HOST_NAME'] = 'localpacker.com'
 end
