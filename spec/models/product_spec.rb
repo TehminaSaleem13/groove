d1d081b009edb34fa833b4927affdca766a206ff
kalakar <ksahoo@navaratan.com>
2015-01-12 12:19:34 +0530

    reloaded product to resolve error in the test case

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe Product do
    	it "should count total available local" do
@@ -10,9 +10,11 @@ describe Product do
       product_barcode = FactoryGirl.create(:product_barcode, :product=> product)
       product_inv_wh = FactoryGirl.create(:product_inventory_warehouse, :product=> product,
                    :inventory_warehouse_id =>inv_wh.id, :available_inv => 25)
+
       product_inv_wh2 = FactoryGirl.create(:product_inventory_warehouse, :product=> product,
                    :inventory_warehouse_id =>inv_wh_1.id, :available_inv => 23)
 
+      product.reload
       count = product.get_total_avail_loc
 
       expect(count).to eq(48)
