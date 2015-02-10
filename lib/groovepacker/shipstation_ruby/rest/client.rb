8c07240ff4c3b1bf594ff6d5ea0741a93bbd3b68
Shyam Visamsetty <svisamsetty@navaratan.com>
2015-02-10 21:13:56 +0530

    Add support to import tracking number of an order when available

--- lib/groovepacker/shipstation_ruby/rest/client.rb
+++ lib/groovepacker/shipstation_ruby/rest/client.rb
@@ -46,6 +46,25 @@ module Groovepacker
           response.parsed_response
         end
 
+        def get_tracking_number(orderNumber)
+          tracking_number = nil
+          unless orderNumber.nil?
+            orderNumber = '&orderNumber=' + orderNumber.to_s
+            Rails.logger.info "Getting shipment with order number: " + orderNumber
+            response = HTTParty.get('https://shipstation.p.mashape.com/Shipments/List?' + 
+                'page=1&pageSize=100' + orderNumber,
+                headers: {
+                  "Authorization" => "Basic "+ Base64.encode64(@auth[:api_key] + ":" + @auth[:api_secret]).gsub(/\n/, ''),
+                  "X-Mashape-Key" => "E6cSux0BVQmshJh0VacUkqXP1sJgp1I1APKjsntC26JSOTy0pP"
+                })
+            handle_exceptions(response)
+            unless response.parsed_response["shipments"].empty?
+              tracking_number = response.parsed_response["shipments"].first["trackingNumber"]
+            end
+          end
+          tracking_number
+        end
+
         def inspect
           "#<ShipStationRuby::Client:#{object_id}>"
         end
