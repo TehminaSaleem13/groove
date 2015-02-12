c3bb6c00d242562b74d9d3c70905edbbd1161863
Shyam Visamsetty <svisamsetty@navaratan.com>
2015-02-13 02:05:39 +0530

    Add support to show proper error message for ship station

--- app/models/shipworks_credential.rb
+++ app/models/shipworks_credential.rb
@@ -6,9 +6,14 @@ class ShipworksCredential < ActiveRecord::Base
 
   validates_uniqueness_of :auth_token
 
-  attr_accessible :auth_token, :store, :shall_import_in_process, 
+  attr_accessible :auth_token, :store, shall_import_in_process, 
     :shall_import_new_order, :shall_import_not_shipped, :shall_import_shipped,
     :shall_import_no_status, :import_store_order_number
 
   belongs_to :store
+
+  def can_import_an_order?
+    shall_import_in_process || shall_import_new_order || shall_import_not_shipped || 
+    shall_import_shipped || shall_import_no_status
+  end
 end
