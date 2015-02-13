684145aa4d8fd11f2d1fabb7fd50148fc54829ce
kalakar <ksahoo@navaratan.com>
2015-02-13 15:25:00 +0530

    selects and deselects a table row in the cards table

--- app/assets/javascripts/controllers/settings/system/paymentsCtrl.js
+++ app/assets/javascripts/controllers/settings/system/paymentsCtrl.js
@@ -1,10 +1,11 @@
 groovepacks_controllers. 
-controller('paymentsCtrl', [ '$scope', '$http', '$timeout', '$location', '$state', '$cookies', 'payments', 'groov_translator',
-function( $scope, $http, $timeout, $location, $state, $cookies, payments, groov_translator) {
+controller('paymentsCtrl', [ '$scope', '$http', '$timeout', '$location', '$state', '$cookies', '$modal', 'payments', 'groov_translator',
+function( $scope, $http, $timeout, $location, $state, $cookies, $modal, payments, groov_translator) {
 
     var myscope = {};
-
+    $scope.selectedPayments = [];
     myscope.init = function() {
+        $scope.show_table_data = false;
         $scope.setup_page('system','payment_details');
         $scope.translations = {
             "headings": {
@@ -19,9 +20,39 @@ function( $scope, $http, $timeout, $location, $state, $cookies, payments, groov_
         };
         groov_translator.translate('settings.system.general',$scope.translations);
         $scope.payments = payments.model.get();
-        payments.list.get($scope.payments);
-        payments.single.get($scope.payments);
+        payments.list.get($scope.payments).then(function(list) {
+            for(var i=0; i<list.length; i++) {
+                list[i].checked = false;
+            }
+        });
+        payments.single.get($scope.payments).then(function() {
+            $scope.show_table_data = true;
+        });
     };
+
+    $scope.setSelected = function (idSelectedPayment) {
+        var i;
+        for(i=0; i<$scope.payments.list.length; i++) {
+            if($scope.payments.list[i].id == idSelectedPayment) {
+                $scope.payments.list[i].checked = !$scope.payments.list[i].checked;
+            }
+        }
+    };
+
+    $scope.openNewForm = function () {
+        console.log("openNewForm");
+        // var alias_modal = $modal.open({
+        //     controller: 'aliasModal',
+        //     templateUrl: '/assets/views/modals/product/alias.html',
+        //     size:'lg',
+        //     resolve: {
+        //         // type: function(){return type},
+        //         // exceptions: function(){return exceptions},
+        //         // id: function(){return id;}
+        //     }
+        // });
+        // alias_modal.result.then(scope.add_item_order);
+    }
 
     // $scope.get_card_list = function() {
     //     payments.list.get($scope.payments);
