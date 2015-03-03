96156d590e94e479cf74cfdf12c5b402392d9721
kalakar <ksahoo@navaratan.com>
2015-03-03 13:11:15 +0530

    rspec tests for aliasing product and updating the corresponding order item or kit item

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -36,6 +36,96 @@ RSpec.describe ProductsController, :type => :controller do
       order_item.reload
       expect(order_item.product_id).to eq(product_orig.id)
     end
+    it "chooses aliases for a product and copies skus and barcodes also updates order items" do
+      request.accept = "application/json"
+
+      product_orig = FactoryGirl.create(:product)
+      product_orig_sku = FactoryGirl.create(:product_sku, :product=> product_orig)
+      product_orig_barcode = FactoryGirl.create(:product_barcode, :product=> product_orig)
+
+      product1 = FactoryGirl.create(:product, :name=>"product1")
+      product1_sku = FactoryGirl.create(:product_sku, :product=> product1, :sku=>'product_sku1')
+      product1_barcode = FactoryGirl.create(:product_barcode, :product=> product1, :barcode=>"BARCODE1")
+
+      product2 = FactoryGirl.create(:product, :name=>"product2")
+      product2_sku = FactoryGirl.create(:product_sku, :product=> product2, :sku=>'product_sku2')
+      product2_barcode = FactoryGirl.create(:product_barcode, :product=> product2, :barcode=>"BARCODE2")
+
+      order = FactoryGirl.create(:order, :status=>'awaiting')
+      order_item = FactoryGirl.create(:order_item, :product_id=>product1.id,
+                    :qty=>1, :price=>"10", :row_total=>"10", :order=>order, :name=>product1.name)
+
+      put :setalias, { :product_orig_id => product_orig.id, product_alias_ids: [product1.id, product2.id] }
+
+      expect(response.status).to eq(200)
+      result = JSON.parse(response.body)
+      expect(result["status"]).to eq(true)
+      product_orig.reload
+      expect(product_orig.product_skus.length).to eq(3)
+      expect(product_orig.product_barcodes.length).to eq(3)
+      expect(Product.all.length).to eq(1)
+
+      order_item.reload
+      expect(order_item.product_id).to eq(product_orig.id)
+    end
+
+    it "sets an alias and copies skus and barcodes also updates kit items" do
+      request.accept = "application/json"
+
+      product_orig = FactoryGirl.create(:product)
+      product_orig_sku = FactoryGirl.create(:product_sku, :product=> product_orig)
+      product_orig_barcode = FactoryGirl.create(:product_barcode, :product=> product_orig)
+
+      product_alias = FactoryGirl.create(:product, :name=>"Apple iPhone5C")
+      product_alias_sku = FactoryGirl.create(:product_sku, :product=> product_alias, :sku=>'iPhone5C')
+      product_alias_barcode = FactoryGirl.create(:product_barcode, :product=> product_alias, :barcode=>"2456789")
+
+      kit_product = FactoryGirl.create(:product, is_kit: 1)
+      product_kit_sku = FactoryGirl.create(:product_kit_sku, product_id: kit_product.id, option_product_id: product_alias.id)
+
+      put :setalias, { :product_orig_id => product_orig.id, product_alias_ids: [product_alias.id] }
+
+      expect(response.status).to eq(200)
+      result = JSON.parse(response.body)
+      expect(result["status"]).to eq(true)
+      product_orig.reload
+      expect(product_orig.product_skus.length).to eq(2)
+      expect(product_orig.product_barcodes.length).to eq(2)
+      
+      product_kit_sku.reload
+      expect(product_kit_sku.option_product_id).to eq(product_orig.id)
+    end
+
+    it "chooses aliases for a product and copies skus and barcodes also updates kit items" do
+      request.accept = "application/json"
+
+      product_orig = FactoryGirl.create(:product)
+      product_orig_sku = FactoryGirl.create(:product_sku, :product=> product_orig)
+      product_orig_barcode = FactoryGirl.create(:product_barcode, :product=> product_orig)
+
+      product1 = FactoryGirl.create(:product, :name=>"product1")
+      product1_sku = FactoryGirl.create(:product_sku, :product=> product1, :sku=>'product_sku1')
+      product1_barcode = FactoryGirl.create(:product_barcode, :product=> product1, :barcode=>"BARCODE1")
+
+      product2 = FactoryGirl.create(:product, :name=>"product2")
+      product2_sku = FactoryGirl.create(:product_sku, :product=> product2, :sku=>'product_sku2')
+      product2_barcode = FactoryGirl.create(:product_barcode, :product=> product2, :barcode=>"BARCODE2")
+
+      kit_product = FactoryGirl.create(:product, is_kit: 1)
+      product_kit_sku = FactoryGirl.create(:product_kit_sku, product_id: kit_product.id, option_product_id: product1.id)
+
+      put :setalias, { :product_orig_id => product_orig.id, product_alias_ids: [product1.id, product2.id] }
+
+      expect(response.status).to eq(200)
+      result = JSON.parse(response.body)
+      expect(result["status"]).to eq(true)
+      product_orig.reload
+      expect(product_orig.product_skus.length).to eq(3)
+      expect(product_orig.product_barcodes.length).to eq(3)
+
+      product_kit_sku.reload
+      expect(product_kit_sku.option_product_id).to eq(product_orig.id)
+    end
   end
 
   describe "inventory warehouse count" do
