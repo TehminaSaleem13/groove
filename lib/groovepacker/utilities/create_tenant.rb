c2ab88c6c296dd5a3ebd2a78710f6cb511010a4d
kalakar <ksahoo@navaratan.com>
2015-03-05 15:40:15 +0530

    added access restrictions for annual plans

--- lib/groovepacker/utilities/create_tenant.rb
+++ lib/groovepacker/utilities/create_tenant.rb
@@ -1,6 +1,6 @@
     class CreateTenant
-    	def self.create_tenant(subscription)
-          	Apartment::Tenant.create(subscription.tenant_name)
+        def self.create_tenant(subscription)
+            Apartment::Tenant.create(subscription.tenant_name)
             tenant = Tenant.create(name: subscription.tenant_name)
             subscription.tenant = tenant
             Apartment::Tenant.switch(subscription.tenant_name)
@@ -8,10 +8,10 @@
             SeedTenant.new.seed(true,subscription.user_name, subscription.email, subscription.password)
             TransactionEmail.delay(run_at: 2.hours.from_now).send_email(subscription)
             TransactionEmail.welcome_email(subscription).deliver
-    	end
+        end
 
-    	def self.apply_restrictions(plan)
-    		if plan == "groove-solo"
+        def self.apply_restrictions(plan)
+            if plan == "groove-solo"
                 AccessRestriction.create(
                     num_users: '1',
                     num_shipments: '500',
@@ -41,6 +41,36 @@
                     num_shipments: '50000',
                     num_import_sources: '8',
                     total_scanned_shipments: '0')
+            elsif plan == "annual-groove-solo"
+                AccessRestriction.create(
+                    num_users: '1',
+                    num_shipments: '500',
+                    num_import_sources: '1',
+                    total_scanned_shipments: '0')
+            elsif plan == 'annual-groove-duo'
+                AccessRestriction.create(
+                    num_users: '2',
+                    num_shipments: '2000',
+                    num_import_sources: '2',
+                    total_scanned_shipments: '0')
+            elsif plan == 'annual-groove-trio'
+                AccessRestriction.create(
+                    num_users: '3',
+                    num_shipments: '6000',
+                    num_import_sources: '3',
+                    total_scanned_shipments: '0')
+            elsif plan == 'annual-groove-quintet'
+                AccessRestriction.create(
+                    num_users: '5',
+                    num_shipments: '12000',
+                    num_import_sources: '5',
+                    total_scanned_shipments: '0')
+            elsif plan == 'annual-groove-symphony'
+                AccessRestriction.create(
+                    num_users: '12',
+                    num_shipments: '50000',
+                    num_import_sources: '8',
+                    total_scanned_shipments: '0')
             end
-    	end
+        end
     end
