29a651dd1faa96c1b6f6290883b6437ca75888e2
kalakar <ksahoo@navaratan.com>
2015-03-09 13:35:34 +0530

    modified protractor tests to resolve failure and added a new test case to delete the newly created products

--- spec/javascripts/protractor/tests/orders.js
+++ spec/javascripts/protractor/tests/orders.js
@@ -126,7 +126,7 @@ describe('Orders:',function() {
                     edit.button = element.all(by.buttonText('Edit')).first().click();
                     edit.parent = edit.button.element(by.xpath(".."));
                     edit.ul = edit.parent.element(by.tagName("ul"));
-                    edit.li = edit.ul.all(by.tagName("li")).get(2).click();
+                    edit.li = edit.ul.all(by.tagName("li")).get(1).click();
 
                     table.tbody = element.all(by.tagName("tbody")).first();
                     table.row = table.tbody.all(by.tagName("tr")).first();
@@ -196,7 +196,7 @@ describe('Orders:',function() {
                     table.thead = table.list_table.element(by.tagName('thead'));
                     table.thead.all(by.repeater('field in theads')).getText().then (function(text) {
                         new openOrderModal(text);
-                        order.first_field_set = element.all(by.tagName('fieldset')).first();
+                        order.first_field_set = element.all(by.tagName('fieldset')).get(1);
 
                         order.first_name_label = order.first_field_set.element(by.cssContainingText('.row .container-fluid .form-group label', 'First Name'));
                         order.first_name_div = order.first_name_label.element(by.xpath('..'));
@@ -258,7 +258,7 @@ describe('Orders:',function() {
                         order.country_value = order.country_div.element(by.tagName('input')).getAttribute('value');
                         expect(order.country_value).toEqual(element(by.model('orders.single.basicinfo.country')).getAttribute('value'));
 
-                        order.last_field_set = element.all(by.tagName('fieldset')).get(1);
+                        order.last_field_set = element.all(by.tagName('fieldset')).get(2);
                         order.scanned_on_label = order.last_field_set.element(by.cssContainingText('.row .form-group label', 'Scanned on'));
                         order.scanned_on_div = order.scanned_on_label.element(by.xpath('..'));
                         order.scanned_on_value = order.scanned_on_div.element(by.tagName('input')).getAttribute('value');
