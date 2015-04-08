873c1d7e203431dc3b6d825e55921af72c8e2a4b
kalakar <ksahoo@navaratan.com>
2015-04-08 12:30:55 +0530

    test cases for changing order quantity and disable auto-allocation when auto-allocation switch is off

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -232,6 +232,85 @@ describe OrdersController do
       expect(product_inv_wh1.available_inv).to eq(26)
       expect(product_inv_wh2.available_inv).to eq(26)
     end
+
+    it "inventory gets adjusted when order item quantity in order is changed" do
+      request.accept = "application/json"
+      inv_wh = FactoryGirl.create(:inventory_warehouse)
+      store = FactoryGirl.create(:store, :inventory_warehouse_id => inv_wh.id)
+      @user_role.add_edit_order_items = true
+      @user_role.save
+      general_setting = FactoryGirl.create(:general_setting, :inventory_tracking=>true, :hold_orders_due_to_inventory=>true, :inventory_auto_allocation=>true)
+      order = FactoryGirl.create(:order, :status=>'awaiting', :increment_id=>'1234567890', :store => store)
+      product1 = FactoryGirl.create(:product)
+      product2 = FactoryGirl.create(:product)
+      product_inv_wh1 = FactoryGirl.create(
+        :product_inventory_warehouse, :product=> product1,
+        :inventory_warehouse_id =>inv_wh.id, 
+        :available_inv => 25, :allocated_inv => 5)
+      product_inv_wh2 = FactoryGirl.create(
+        :product_inventory_warehouse, :product=> product2,
+        :inventory_warehouse_id =>inv_wh.id, 
+        :available_inv => 25, :allocated_inv => 5)
+      order_item1 = FactoryGirl.create(:order_item, :product_id=>product1.id,
+                    :qty=>1, :price=>"10", :row_total=>"10", :order=>order, :name=>product1.name)
+      order_item2 = FactoryGirl.create(:order_item, :product_id=>product2.id,
+                    :qty=>1, :price=>"10", :row_total=>"10", :order=>order, :name=>product2.name)
+      product_inv_wh1.reload
+      product_inv_wh2.reload
+      expect(product_inv_wh1.allocated_inv).to eq(6)
+      expect(product_inv_wh2.allocated_inv).to eq(6)
+      expect(product_inv_wh1.available_inv).to eq(24)
+      expect(product_inv_wh2.available_inv).to eq(24)
+      put :updateiteminorder, {:orderitem=>order_item1, :qty=>'2'}
+      expect(response.status).to eq(200)
+      result = JSON.parse(response.body)
+      product_inv_wh1.reload
+      product_inv_wh2.reload
+      expect(product_inv_wh1.allocated_inv).to eq(7)
+      expect(product_inv_wh2.allocated_inv).to eq(6)
+      expect(product_inv_wh1.available_inv).to eq(23)
+      expect(product_inv_wh2.available_inv).to eq(24)
+    end
+
+    it "inventory does not adjust when order item quantity in order is changed, if auto-allocation is off" do
+      request.accept = "application/json"
+      inv_wh = FactoryGirl.create(:inventory_warehouse)
+      store = FactoryGirl.create(:store, :inventory_warehouse_id => inv_wh.id)
+      @user_role.add_edit_order_items = true
+      @user_role.save
+      general_setting = FactoryGirl.create(:general_setting, :inventory_tracking=>true, :hold_orders_due_to_inventory=>true, :inventory_auto_allocation=>false)
+      order = FactoryGirl.create(:order, :status=>'awaiting', :increment_id=>'1234567890', :store => store)
+      product1 = FactoryGirl.create(:product)
+      product2 = FactoryGirl.create(:product)
+      product_inv_wh1 = FactoryGirl.create(
+        :product_inventory_warehouse, :product=> product1,
+        :inventory_warehouse_id =>inv_wh.id, 
+        :available_inv => 25, :allocated_inv => 5)
+      product_inv_wh2 = FactoryGirl.create(
+        :product_inventory_warehouse, :product=> product2,
+        :inventory_warehouse_id =>inv_wh.id, 
+        :available_inv => 25, :allocated_inv => 5)
+      order_item1 = FactoryGirl.create(:order_item, :product_id=>product1.id,
+                    :qty=>1, :price=>"10", :row_total=>"10", :order=>order, :name=>product1.name)
+      order_item2 = FactoryGirl.create(:order_item, :product_id=>product2.id,
+                    :qty=>1, :price=>"10", :row_total=>"10", :order=>order, :name=>product2.name)
+      product_inv_wh1.reload
+      product_inv_wh2.reload
+      expect(product_inv_wh1.allocated_inv).to eq(6)
+      expect(product_inv_wh2.allocated_inv).to eq(6)
+      expect(product_inv_wh1.available_inv).to eq(24)
+      expect(product_inv_wh2.available_inv).to eq(24)
+      put :updateiteminorder, {:orderitem=>order_item1, :qty=>'2'}
+      expect(response.status).to eq(200)
+      result = JSON.parse(response.body)
+      product_inv_wh1.reload
+      product_inv_wh2.reload
+      expect(product_inv_wh1.allocated_inv).to eq(6)
+      expect(product_inv_wh2.allocated_inv).to eq(6)
+      expect(product_inv_wh1.available_inv).to eq(24)
+      expect(product_inv_wh2.available_inv).to eq(24)
+    end
+    
     # it "Changing order status from awaiting/service_issue to scanned and clicking on 'yes' option should update inventory counts from allocated to sold" do
     #   request.accept = "application/json"
     #   inv_wh = FactoryGirl.create(:inventory_warehouse)
