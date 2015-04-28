694cab658f7544bd7029d4b904cfaa3617a692c8
kalakar <ksahoo@navaratan.com>
2015-04-28 15:33:09 +0530

    added api key in intercom.rb

--- config/initializers/intercom.rb
+++ config/initializers/intercom.rb
@@ -13,7 +13,7 @@ IntercomRails.config do |config|
   # This is required for some Intercom rake tasks like importing your users;
   # you can generate one at https://app.intercom.io/apps/api_keys.
   #
-  # config.api_key = "..."
+  config.api_key = "cec70bb51615dc63a33ad4d579083a5ce9d078cf"
 
   # == Enabled Environments
   # Which environments is auto inclusion of the Javascript enabled for
