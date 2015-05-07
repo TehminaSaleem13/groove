0e8beee28dfcd96c1bf1ae539b89131a54aa9ee6
Kalakar <ksahoo@navaratan.com>
2015-05-07 18:29:58 +0530

    modified export_settings model to create csv file in different format

--- app/models/export_setting.rb
+++ app/models/export_setting.rb
@@ -72,58 +72,80 @@ class ExportSetting < ActiveRecord::Base
     result['messages'] = []
     start_time = nil
     end_time = nil
-    if true
-      if self.export_orders_option == 'on_same_day'
-        start_time = Time.zone.now.beginning_of_day
+    if self.export_orders_option == 'on_same_day'
+      start_time = Time.zone.now.beginning_of_day
+    else
+      unless self.last_exported.nil?
+        start_time = self.last_exported
       else
-        unless self.last_exported.nil?
-          start_time = self.last_exported
-        else
-          start_time = '2000-01-01 00:00:00'
-        end
+        start_time = '2000-01-01 00:00:00'
       end
-      if start_time.nil?
-        result['status'] = false
-        result['messages'].push('We need a start and an end time')
-      else
-        serials = OrderSerial.where(created_at: start_time..Time.now)
-        self.last_exported = Time.now
-        self.save
-        filename = 'groove-order-serials-'+Time.now.to_s+'.csv'
-
+    end
+    if start_time.nil?
+      result['status'] = false
+      result['messages'].push('We need a start and an end time')
+    else
+      orders = Order.where(scanned_on: start_time..Time.now)
+      scanpack_settings = ScanPackSetting.all.first
+      self.last_exported = Time.now
+      self.save
+      filename = 'groove-order-export-'+Time.now.to_s+'.csv'
+      unless self.order_export_type == 'do_not_include'
         row_map = {
-            :order_date =>'',
-            :order_number => '',
-            :barcode_with_serial => '',
-            :barcode =>'',
-            :serial =>'',
-            :primary_sku =>'',
-            :product_name=>'',
-            :packing_user =>'',
-            :order_item_count => '',
-            :scanned_date =>'',
-            :warehouse_name =>''
+          :order_date =>'',
+          :order_number => '',
+          :barcode_with_lot => '',
+          :barcode =>'',
+          :lot_number =>'',
+          :primary_sku =>'',
+          :product_name=>'',
+          :packing_user =>'',
+          :order_item_count => '',
+          :scanned_date =>'',
+          :warehouse_name =>''
         }
         CSV.open("#{Rails.root}/public/csv/#{filename}","w") do |csv|
           csv << row_map.keys
-          unless self.order_export_type == 'do_not_include'
-            serials.each do |serial|
-              if self.order_export_type == 'order_with_serial_lot'
-                unless serial.product.primary_barcode.nil?
-                  csv << push_single_row(serial, row_map)
+          orders.each do |order|
+            order_items = order.order_items
+            unless order_items.empty?
+              order_items.each do |order_item|
+                if self.order_export_type == 'order_with_serial_lot'
+                  unless order_item.product.primary_barcode.nil?
+                    csv << push_single_row(order_item, row_map, scanpack_settings)
+                  else
+                    next
+                  end
                 else
-                  next
+                  csv << push_single_row(order_item, row_map, scanpack_settings)
                 end
-              else
-                csv << push_single_row(serial, row_map)
               end
             end
           end
         end
-      end
-    else
-      result['status'] = false
-      result['messages'].push('You do not have enough permissions to view order serials')
+      else
+        row_map = {
+          :order_date =>'',
+          :order_number => '',
+          :packing_user =>'',
+          :scanned_date =>''
+        }
+        CSV.open("#{Rails.root}/public/csv/#{filename}","w") do |csv|
+          csv << row_map.keys
+          orders.each do |order|
+            single_row = row_map.dup
+            single_row[:order_number] = order.increment_id
+            single_row[:order_date] = order.order_placed_time
+            single_row[:scanned_date] = order.scanned_on
+            packing_user = nil
+            packing_user = User.find(order.packing_user_id) unless order.packing_user_id.blank?
+            unless packing_user.nil?
+              single_row[:packing_user] = packing_user.name + ' ('+packing_user.username+')'
+            end
+            csv << single_row.values
+          end
+        end
+      end  
     end
 
     unless result['status']
@@ -135,23 +157,28 @@ class ExportSetting < ActiveRecord::Base
     filename
   end
 
-  def push_single_row(serial, row_map)
+  def push_single_row(order_item, row_map, scanpack_settings)
     single_row = row_map.dup
-    single_row[:order_number] = serial.order.increment_id
-    single_row[:order_date] = serial.order.order_placed_time
-    single_row[:scanned_date] = serial.order.scanned_on
+    single_row[:order_number] = order_item.order.increment_id
+    single_row[:order_date] = order_item.order.order_placed_time
+    single_row[:scanned_date] = order_item.order.scanned_on
     packing_user = nil
-    packing_user = User.find(serial.order.packing_user_id) unless serial.order.packing_user_id.blank?
+    packing_user = User.find(order_item.order.packing_user_id) unless order_item.order.packing_user_id.blank?
     unless packing_user.nil?
       single_row[:packing_user] = packing_user.name + ' ('+packing_user.username+')'
-      single_row[:warehouse_name] =  serial.product.primary_warehouse.inventory_warehouse.name unless serial.product.primary_warehouse.nil? || serial.product.primary_warehouse.inventory_warehouse.nil?
+      single_row[:warehouse_name] =  order_item.product.primary_warehouse.inventory_warehouse.name unless order_item.product.primary_warehouse.nil? || order_item.product.primary_warehouse.inventory_warehouse.nil?
     end
-    single_row[:serial] = serial.serial
-    single_row[:product_name] = serial.product.name
-    single_row[:primary_sku] =  serial.product.primary_sku
-    single_row[:barcode] =  serial.product.primary_barcode
-    single_row[:barcode_with_serial] = serial.product.primary_barcode
-    single_row[:order_item_count] = serial.order.get_items_count
+    single_row[:barcode_with_lot] = order_item.product.primary_barcode
+    unless scanpack_settings.escape_string.nil?
+      barcode = order_item.product.primary_barcode
+      single_row[:barcode] = barcode.slice(0..(barcode.index(scanpack_settings.escape_string)-1)) unless barcode.index(scanpack_settings.escape_string).nil?
+      single_row[:lot_number] = barcode.slice(barcode.index(scanpack_settings.escape_string)..(barcode.length-1)) unless barcode.index(scanpack_settings.escape_string).nil?
+    end
+    # single_row[:barcode] =  order_item.product.primary_barcode
+    # single_row[:lot_number] = serial.serial
+    single_row[:product_name] = order_item.product.name
+    single_row[:primary_sku] =  order_item.product.primary_sku
+    single_row[:order_item_count] = order_item.order.get_items_count
 
     single_row.values
   end
