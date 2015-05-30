6abefbefa407d93c30985cfcd971932875996c4a
Shyam Visamsetty <svisamsetty@navaratan.com>
2015-05-31 00:24:55 +0530

    Design the navigation bar for dashboard GROOV-313 GROOV-448

--- app/assets/javascripts/directives/groovDashboard.js
+++ app/assets/javascripts/directives/groovDashboard.js
@@ -7,6 +7,28 @@ groovepacks_directives.directive('groovDashboard',['$window','$document','$sce',
       scope: {},
       link: function(scope,el,attrs) {
         scope.dashbord_detail_open = false;
+        scope.dash_tabs = [
+          {
+            "heading": "Home",
+            "templateUrl": "/assets/views/directives/test.html"
+          },
+          {
+            "heading": "Most Recent Exceptions",
+            "templateUrl": "/assets/views/directives/test.html"
+          },
+          {
+            "heading": "Exceptions by Frequency",
+            "templateUrl": "/assets/views/directives/test.html"
+          },
+          {
+            "heading": "Items by Exception rate",
+            "templateUrl": "/assets/views/directives/test.html"
+          },
+          {
+            "heading": "Leader Board",
+            "templateUrl": "/assets/views/directives/test.html"
+          }
+        ]
         scope.toggle_dashboard_detail = function() {
           $('#dashboard').toggleClass('pdash-open');
           scope.dashbord_detail_open = !scope.dashbord_detail_open;
