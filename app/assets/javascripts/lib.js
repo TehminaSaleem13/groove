41f6927668fcbae406b2f8d0f39b90dcd65bd07b
Shyam Visamsetty <svisamsetty@navaratan.com>
2015-05-31 18:33:24 +0530

    Added support for high charts, designed the tabs in the dashboard

--- app/assets/javascripts/lib.js
+++ app/assets/javascripts/lib.js
@@ -2,4 +2,6 @@
 //= require ./lib/ui-router
 //= require ./lib/angular-translate
 //= require ./lib/angular-translate-loader-static-files
+//= require ../../../vendor/assets/components/highcharts-release/highcharts
+//= require ../../../vendor/assets/components/highcharts-ng/dist/highcharts-ng
 //= require_tree ./lib
