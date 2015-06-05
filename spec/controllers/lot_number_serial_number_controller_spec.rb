1bd604d96b8dda1e6c98114fb2e15cc0d2fb822d
Kalakar <ksahoo@navaratan.com>
2015-06-05 20:51:59 +0530

    rspec test for inventory changes add_to_any_order

--- spec/controllers/lot_number_serial_number_controller_spec.rb
+++ spec/controllers/lot_number_serial_number_controller_spec.rb
@@ -285,6 +285,59 @@ describe ScanPackController do
 	    expect(order_item_serial_lot.order_serial.serial).to eq('productserial1')
 		end
 
+		it "updates inventory for the add_to_any_order enabled item scanned during order scan" do
+			request.accept = "application/json"
+			inv_wh = FactoryGirl.create(:inventory_warehouse)
+
+      store = FactoryGirl.create(:store, :inventory_warehouse_id => inv_wh.id)
+	    order = FactoryGirl.create(:order, :status=>'awaiting', :store=>store)
+
+	    product1 = FactoryGirl.create(:product)
+	    product_inv_wh1 = FactoryGirl.create(:product_inventory_warehouse, :product=> product1,
+                   :inventory_warehouse_id =>inv_wh.id, :available_inv => 25)
+	    product_sku1 = FactoryGirl.create(:product_sku, :product=> product1)
+	    product_barcode1 = FactoryGirl.create(:product_barcode, :product=> product1, :barcode=>'1236547890')
+	    order_item1 = FactoryGirl.create(:order_item, :product_id=>product1.id,
+	                  :qty=>4, :order=>order, :name=>product1.name)
+
+	    product2 = FactoryGirl.create(:product, :add_to_any_order=>true, :name=>"Apple iPhone 5Z")
+	    product_inv_wh2 = FactoryGirl.create(:product_inventory_warehouse, :product=> product2,
+                   :inventory_warehouse_id =>inv_wh.id, :available_inv => 25)
+	    product_sku2 = FactoryGirl.create(:product_sku, :product=> product2, :sku=>"IPHONE5Z")
+	    product_barcode2 = FactoryGirl.create(:product_barcode, :product=> product2, :barcode=>'3236547890')
+
+	    product3 = FactoryGirl.create(:product, :add_to_any_order=>true, :name=>"Apple iPhone 5T", :record_serial=>true)
+	    product_inv_wh3 = FactoryGirl.create(:product_inventory_warehouse, :product=> product3,
+                   :inventory_warehouse_id =>inv_wh.id, :available_inv => 25)
+	    product_sku3 = FactoryGirl.create(:product_sku, :product=> product3, :sku=>"IPHONE5T")
+	    product_barcode3 = FactoryGirl.create(:product_barcode, :product=> product3, :barcode=>'4236547890')
+
+	    get :scan_barcode, {:state=>'scanpack.rfp.default', :input => '1236547890', :id => order.id }
+	    expect(response.status).to eq(200)
+	    get :scan_barcode, {:state=>'scanpack.rfp.default', :input => '3236547890 - LOT1', :id => order.id }
+	    expect(response.status).to eq(200)
+	    get :scan_barcode, {:state=>'scanpack.rfp.default', :input => '3236547890 - LOT1', :id => order.id }
+	    expect(response.status).to eq(200)
+	    get :scan_barcode, {:state=>'scanpack.rfp.default', :input => '4236547890 - LOT2', :id => order.id }
+	    expect(response.status).to eq(200)
+	    data=extract_response(response)
+	    get :serial_scan, {:order_id=>order.id, :product_id => product3.id, :serial => 'productserial1', :product_lot_id => data["product_lot_id"], :barcode => data["barcode"], :order_item_id => data["order_item_id"] }
+	    expect(response.status).to eq(200)
+	    get :scan_barcode, {:state=>'scanpack.rfp.default', :input => '4236547890 - LOT2', :id => order.id }
+	    expect(response.status).to eq(200)
+	    data=extract_response(response)
+	    get :serial_scan, {:order_id=>order.id, :product_id => product3.id, :serial => 'productserial2', :product_lot_id => data["product_lot_id"], :barcode => data["barcode"], :order_item_id => data["order_item_id"] }
+	    expect(response.status).to eq(200)
+
+	    product_inv_wh1.reload()
+	    product_inv_wh2.reload()
+	    product_inv_wh3.reload()
+
+	    expect(product_inv_wh1.available_inv).to eq(21)
+	    expect(product_inv_wh2.available_inv).to eq(23)
+	    expect(product_inv_wh3.available_inv).to eq(23)
+		end
+
 		def extract_response(response)
 			result = JSON.parse(response.body)
 			data = Hash.new
