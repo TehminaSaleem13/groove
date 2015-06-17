1e105382a2bf0d7fdc34dbab18d7e875547d8ee7
Kalakar <ksahoo@navaratan.com>
2015-06-17 19:31:00 +0530

    restores products from cvs files

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -8,10 +8,9 @@ class SettingsController < ApplicationController
     if current_user.can? 'restore_backups'
         # Every entry on mapping[current_mapping][:map] should be in the format
       # csv_header => db_table_header
-      mapping = {
-          'products' => {
-              model: Product,
-              map: {
+      puts "params: " + params.inspect
+      if params[:method] == 'del_import_old'
+        product_map = {
                   'id' => 'id',
                   'store_product_id' => 'store_product_id',
                   'name' => 'name',
@@ -34,6 +33,45 @@ class SettingsController < ApplicationController
                   'weight' =>'weight',
                   'shipping_weight'=>'shipping_weight'
               }
+        default_warehouse_map = {
+                  'default_wh_avbl' => 'available_inv',
+                  'default_wh_loc_primary' =>'location_primary',
+                  'default_wh_loc_secondary' => 'location_secondary'
+              }
+      else
+        product_map = {
+                  'ID' => 'id',
+                  'Name' => 'name',
+                  'store_product_id' => 'store_product_id',
+                  'product_type' => 'product_type',
+                  'store_id' => 'store_id',
+                  'created_at' => 'created_at',
+                  'updated_at' => 'updated_at',
+                  'BinLocation 1' => 'inv_wh1',
+                  'status' => 'status',
+                  'spl_instructions_4_packer' => 'spl_instructions_4_packer',
+                  'spl_instructions_4_confirmation' => 'spl_instructions_4_confirmation',
+                  'Barcode 1' => 'barcode',
+                  'is_skippable' => 'is_skippable',
+                  'packing_placement' => 'packing_placement',
+                  'pack_time_adj' => 'pack_time_adj',
+                  'kit_parsing' => 'kit_parsing',
+                  'is_kit' => 'is_kit',
+                  'disable_conf_req' => 'disable_conf_req',
+                  'total_avail_ext' =>'total_avail_ext',
+                  'Weight' =>'weight',
+                  'shipping_weight'=>'shipping_weight'
+              }
+        default_warehouse_map = {
+                  'Quantity Avbl' => 'available_inv',
+                  'BinLocation 1' =>'location_primary',
+                  'BinLocation 2' => 'location_secondary'
+              }
+      end
+      mapping = {
+          'products' => {
+              model: Product,
+              map: product_map
           },
 
           'product_barcodes' => {
@@ -113,14 +151,7 @@ class SettingsController < ApplicationController
           }
       }
 
-      default_warehouse_map = {
-          'default_wh_avbl' => 'available_inv',
-          'default_wh_loc_primary' =>'location_primary',
-          'default_wh_loc_secondary' => 'location_secondary'
-      }
-
-
-      if params[:method].nil? || !['del_import','update_only'].include?(params[:method])
+      if params[:method].nil? || !['del_import_old','del_import_new'].include?(params[:method])
         @result['status'] = false
         @result.messages.push("No action selected")
       elsif params[:file].nil?
@@ -130,11 +161,11 @@ class SettingsController < ApplicationController
         require 'zip'
         require 'csv'
         # Clear tables if delete method is invoked
-        if params[:method] == 'del_import'
-          mapping.each do |single_map|
-            single_map[1][:model].delete_all
-          end
+        mapping.each do |single_map|
+          puts "single_map: " + single_map.inspect
+          single_map[1][:model].delete_all
         end
+
         products_to_check_later = []
         # Open uploaded zip file
         Zip::File.open(params[:file].path) do |zipfile|
@@ -145,47 +176,47 @@ class SettingsController < ApplicationController
             if mapping.key?(current_mapping)
               # Parse the file by it's data
               CSV.parse(zipfile.read(file.name),:headers=> true) do |csv_row|
+                puts "csv_row: " + csv_row.inspect
                 single_row = nil
                 create_new = false
                 # Create new row if deleted all else find and select by id for updating
-                if params[:method] == 'del_import'
-                  if current_mapping == 'product_barcodes'
-                    all_rows = mapping[current_mapping][:model].where(:barcode =>csv_row['barcode'].strip,:product_id=>csv_row['product_id'])
-                    if all_rows.length >0
-                      single_row = all_rows.first
-                    end
-                  elsif current_mapping == 'product_skus'
-                    all_rows = mapping[current_mapping][:model].where(:sku =>csv_row['sku'].strip,:product_id=>csv_row['product_id'])
-                    if all_rows.length >0
-                      single_row = all_rows.first
-                    end
-                  elsif current_mapping == 'product_cats'
-                    all_rows = mapping[current_mapping][:model].where(:category =>csv_row['category'].strip,:product_id=>csv_row['product_id'])
-                    if all_rows.length >0
-                      single_row = all_rows.first
-                    end
-                  elsif current_mapping == 'product_images'
-                    all_rows = mapping[current_mapping][:model].where(:image =>csv_row['image'],:product_id=>csv_row['product_id'])
-                    if all_rows.length > 0
-                      single_row = all_rows.first
-                    end
-                  elsif current_mapping == 'product_inventory_warehouses'
-                    all_rows = mapping[current_mapping][:model].where(:inventory_warehouse_id =>csv_row['inventory_warehouse_id'],:product_id=>csv_row['product_id'])
-                    if all_rows.length > 0
-                      single_row = all_rows.first
-                    end
+                # if params[:method] == 'del_import'
+                if current_mapping == 'product_barcodes'
+                  all_rows = mapping[current_mapping][:model].where(:barcode =>csv_row['barcode'].strip,:product_id=>csv_row['product_id'])
+                  if all_rows.length >0
+                    single_row = all_rows.first
                   end
-                  if single_row.nil?
-                    single_row = mapping[current_mapping][:model].new
-                    create_new = true
+                elsif current_mapping == 'product_skus'
+                  all_rows = mapping[current_mapping][:model].where(:sku =>csv_row['sku'].strip,:product_id=>csv_row['product_id'])
+                  if all_rows.length >0
+                    single_row = all_rows.first
                   end
-                else
-                  single_row = mapping[current_mapping][:model].find_by_id(csv_row['id'])
+                elsif current_mapping == 'product_cats'
+                  all_rows = mapping[current_mapping][:model].where(:category =>csv_row['category'].strip,:product_id=>csv_row['product_id'])
+                  if all_rows.length >0
+                    single_row = all_rows.first
+                  end
+                elsif current_mapping == 'product_images'
+                  all_rows = mapping[current_mapping][:model].where(:image =>csv_row['image'],:product_id=>csv_row['product_id'])
+                  if all_rows.length > 0
+                    single_row = all_rows.first
+                  end
+                elsif current_mapping == 'product_inventory_warehouses'
+                  all_rows = mapping[current_mapping][:model].where(:inventory_warehouse_id =>csv_row['inventory_warehouse_id'],:product_id=>csv_row['product_id'])
+                  if all_rows.length > 0
+                    single_row = all_rows.first
+                  end
+                end
+                if single_row.nil?
+                  single_row = mapping[current_mapping][:model].new
+                  create_new = true
                 end
 
                 unless single_row.nil?
                   # Now loop through all our defined mappings to check and update values
                   mapping[current_mapping][:map].each do |column|
+                    puts "column: " + column.inspect
+                    puts "csv_row[column[0]]:" + csv_row[column[0]].inspect
                     if current_mapping=='product_skus' && column[1] =='id' && !create_new
                       break;
                     end
@@ -228,9 +259,10 @@ class SettingsController < ApplicationController
                     unless csv_row['is_kit'].blank?
                       products_to_check_later << single_row
                     end
+                    single_row.save
+                    puts "update_product_status....................."
                     single_row.update_product_status
                   end
-
                 end
               end
             end
