b20b7bf6c176026ec774b0c9e862c0b8534777b9
Shyam Visamsetty <svisamsetty@navaratan.com>
2015-06-19 01:02:35 +0530

    add d3 chart with sample data GROOV-448

--- app/assets/javascripts/lib.js
+++ app/assets/javascripts/lib.js
@@ -2,11 +2,8 @@
 //= require ./lib/ui-router
 //= require ./lib/angular-translate
 //= require ./lib/angular-translate-loader-static-files
-//= require ../../../vendor/assets/components/highcharts-release/highcharts
-//= require ../../../vendor/assets/components/highcharts-ng/dist/highcharts-ng
-//= require ../../../vendor/assets/components/lodash/dist/lodash
 //= require ../../../vendor/assets/components/d3/d3
-//= require ../../../vendor/assets/components/crossfilter/crossfilter
-//= require ../../../vendor/assets/components/dcjs/dc
-//= require ../../../vendor/assets/components/angular-dc/dist/angular-dc
+//= require moment
+//= require ../../../vendor/assets/components/nvd3/nv.d3
+//= require ../../../vendor/assets/components/angularjs-nvd3-directives/dist/angularjs-nvd3-directives
 //= require_tree ./lib
