0e93a79d30203a84f50a3d67034f61991129c098
Kalakar <ksahoo@navaratan.com>
2015-08-05 16:34:38 +0530

    confirmation modal for deleting orders, products and tenant data

---
+++ app/assets/javascripts/groovepacks_admin/controllers/tools/tenantsDeleteModal.js
@@ -0,0 +1,29 @@
+groovepacks_admin_controllers.
+    controller('tenantsDeleteModal', [ '$scope', '$http', '$timeout', '$stateParams', '$location', '$state', '$cookies','$modal','$modalInstance','tenant_data','deletion_type','tenants',
+        function( $scope, $http, $timeout, $stateParams, $location, $state, $cookies,$modal,$modalInstance,tenant_data,deletion_type,tenants) {
+            //Definitions
+
+            var myscope = {};
+            /*
+             * Public methods
+             */
+
+            $scope.ok = function() {
+                $modalInstance.close("ok-button-click");
+                console.log(deletion_type);
+                tenants.single.delete($scope.tenants.single.basicinfo.id,deletion_type);
+            };
+            
+            $scope.cancel = function () {
+                $modalInstance.dismiss("cancel-button-click");
+            };
+
+            myscope.init = function() {
+                $scope.tenants = tenant_data;
+                $scope.tenants.deletion_type = deletion_type;
+                console.log($scope.tenants);
+            };
+
+            myscope.init();
+
+        }]);
