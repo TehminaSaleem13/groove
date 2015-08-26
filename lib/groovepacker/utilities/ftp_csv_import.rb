7962ab6811aa0fbde6794aff9fe9a2843e6fec29
Kalakar <ksahoo@navaratan.com>
2015-08-26 13:07:17 +0530

    catch errors and exceptions during import

--- lib/groovepacker/utilities/ftp_csv_import.rb
+++ lib/groovepacker/utilities/ftp_csv_import.rb
@@ -2,45 +2,33 @@ class FTPCsvImport
   require ('net/sftp')
 
   def self.retrieve_csv_file
-    # @host = '45.55.84.212'
-    # @user_name = 'deployer'
-    # @password = 'dsodevtest!'
-    # @port = 21
-    # @directory = 'csv/'
-    # login_to_ftp_server
-    # files = @sftp.chdir(@directory)
-    sftp = Net::SFTP.start('45.55.84.212', 'deployer', :password => 'dsodevtest!')
-    files = sftp.dir.glob('csv/', "*.csv").sort {|f| File.mtime(f)}
-    file = files.first.name
-    sftp.download!('csv/'+file, 'public/local.csv')
-    # @sftp.close
+    begin
+      sftp = Net::SFTP.start('45.55.84.212', 'deployer', :password => 'dsodevtest!')
+      files = sftp.dir.glob('csv/', "*.csv").sort {|f| File.mtime(f)}
+      file = files.first.name
+      sftp.download!('csv/'+file, 'public/local.csv')
+    rescue Exception => e
+      puts e.message
+    end
   end
 
   def self.update_csv_file
-    # login_to_ftp_server
-    # files = @ftp.chdir(@directory)
-    # @file = @ftp.glob("*.csv").max_by {|f| File.mtime(f)}
-    # @ftp.puttextfile('public/local.csv', @file)
-    sftp = Net::SFTP.start('45.55.84.212', 'deployer', :password => 'dsodevtest!')
-    files = sftp.dir.glob('csv/', "*.csv").sort {|f| File.mtime(f)}
-    file = files.first.name
-    new_file = ''
-    new_file = rename_file(file,new_file)
-    puts "new_file: " + new_file
-    sftp.rename('csv/'+file, 'csv/'+new_file)
-    # @ftp.close
+    begin
+      sftp = Net::SFTP.start('45.55.84.212', 'deployer', :password => 'dsodevtest!')
+      files = sftp.dir.glob('csv/', "*.csv").sort {|f| File.mtime(f)}
+      file = files.first.name
+      puts "file: " + file
+      new_file = ''
+      new_file = rename_file(file,new_file)
+      puts "new_file: " + new_file
+      sftp.rename!('csv/'+file, 'csv/'+new_file)
+      # files = sftp.dir.glob('csv/', "*.csv").sort {|f| File.mtime(f)}
+      # file = files.first.name
+      # puts "updated_file: " + file
+    rescue Exception => e
+      puts e.message
+    end
   end
-
-  # def self.login_to_ftp_server
-    # require ('net/sftp')
-    # require 'net/ftp'
-    # @ftp = Net::FTP.new(@host, @user_name, @password)
-    # @ftp.login
-    
-
-    # @sftp = Net::SFTP.start('45.55.84.212', 'deployer', :password => 'dsodevtest!')
-
-  # end
 
   def self.rename_file(file, new_file)
     substrings = file.split('.')
