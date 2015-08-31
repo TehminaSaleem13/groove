087bb3f2b1a6be933417eeff12e96193455989a1
Ashish Singh <aitashish173@gmail.com>
2015-08-31 06:39:51 +0530

    Added pry gem for debugging in console.
    Request routing to correct job.

--- lib/groovepacker/orders/bulk_actions.rb
+++ lib/groovepacker/orders/bulk_actions.rb
@@ -1,15 +1,25 @@
 module Groovepacker
-  module Products
+  module Orders
     class BulkActions
       include OrdersHelper
 
-      def status_update(tenant, params, bulk_action_id)
+      def status_update(tenant, params, bulk_actions_id)
         # Change db for tenant
         Apartment::Tenant.switch(tenant)
-        
-        list_selected_orders(params)
-        # List selected orders
 
+        bulk_action = GrooveBulkActions.find(bulk_actions_id)
+        
+        orders = list_selected_orders(params)
+
+        unless orders.nil?
+          # update the bulk action object with updating status
+
+          # Iterate over orders and check if products are in active state or not.
+          # If all products of an order are in active state then change order status.
+          # Save all the failed orders in an object or array.
+
+          # Update the BulkActionObject with current status.
+        end
 
       end
     end
