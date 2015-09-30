b72c23c4d7fb0155be10427134116de063f43fd1
Kalakar <ksahoo@navaratan.com>
2015-09-30 17:50:28 +0530

    spec tests for importing kits from csv file

--- spec/controllers/csv_import_controller_spec.rb
+++ spec/controllers/csv_import_controller_spec.rb
@@ -338,4 +338,28 @@ describe StoreSettingsController do
       expect(Product.where(:id=>product_id).size).to eq(0)
     end
   end
+  describe "POST 'kit import'" do
+    it "imports kit products from csv file" do
+      @inv_wh = FactoryGirl.create(:inventory_warehouse, :name=>'csv_inventory_warehouse')
+      @store = FactoryGirl.create(:store, :name=>'csv_store', :store_type=>'CSV', :inventory_warehouse=>@inv_wh)
+      expect(Product.all.count).to eq(0)
+      request.accept = "application/json"
+      get :csvImportData, {:type => 'kit', :id => @store.id}
+      expect(response.status).to eq(200)
+
+      request.accept = "application/json"
+      get :createUpdateStore, {:store_type => 'CSV', :id => @store.id, :kitfile => fixture_file_upload(Rails.root.join('/files/MT_Kits_03.csv'))}
+      expect(response.status).to eq(200)
+
+      doc = IO.read(Rails.root.join("spec/fixtures/files/MT_Kits_03_map"))
+      doc = eval(doc)
+
+      request.accept = "application/json"
+      post :csvDoImport, {:rows=>"2", :sep=>",", :other_sep=>"0", :delimiter=>"\"", :fix_width=>"0", :fixed_width=>"4", :import_action=>nil, :contains_unique_order_items=>false, :generate_barcode_from_sku=>false, :use_sku_as_product_name=>false, :order_date_time_format=>"MM/DD/YY TIME", :day_month_sequence=>"DD/MM", :map=>doc[:map][:map], :controller=>"store_settings", :action=>"csvDoImport", :store_id=> @store.id, :name=>doc[:name], :type=>'kit', :flag=>'file_upload'}
+      expect(response.status).to eq(200)
+      expect(Product.all.count).to eq(17)
+      expect(Product.where(:is_kit=>true).size).to eq(6)
+      expect(Product.where(:is_kit=>false).size).to eq(11)
+    end
+  end
 end
