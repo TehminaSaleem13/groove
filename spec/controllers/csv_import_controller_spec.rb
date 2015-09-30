94fcfbd99ffb2d38cbddbbd7c0984b236139fdd1
Kalakar <ksahoo@navaratan.com>
2015-09-30 16:52:32 +0530

    more tests for csv product import

--- spec/controllers/csv_import_controller_spec.rb
+++ spec/controllers/csv_import_controller_spec.rb
@@ -259,5 +259,83 @@ describe StoreSettingsController do
       expect(response.status).to eq(200)
       expect(Product.all.count).to eq(21)
     end
+    it "imports products from csv file with import option 3" do
+      @inv_wh = FactoryGirl.create(:inventory_warehouse, :name=>'csv_inventory_warehouse')
+      @store = FactoryGirl.create(:store, :name=>'csv_store', :store_type=>'CSV', :inventory_warehouse=>@inv_wh)
+      expect(Order.all.count).to eq(0)
+      expect(Product.all.count).to eq(0)
+      # import orders
+      request.accept = "application/json"
+      get :csvImportData, {:type => 'order', :id => @store.id}
+      expect(response.status).to eq(200)
+
+      request.accept = "application/json"
+      get :createUpdateStore, {:store_type => 'CSV', :id => @store.id, :orderfile => fixture_file_upload(Rails.root.join('/files/MT_Orders_04.csv'))}
+      expect(response.status).to eq(200)
+
+      doc = IO.read(Rails.root.join("spec/fixtures/files/MT_Orders_04_map"))
+      doc = eval(doc)
+
+      request.accept = "application/json"
+      post :csvDoImport, {:rows=>"2", :sep=>",", :other_sep=>"0", :delimiter=>"\"", :fix_width=>"0", :fixed_width=>"4", :import_action=>nil, :contains_unique_order_items=>false, :generate_barcode_from_sku=>false, :use_sku_as_product_name=>false, :order_date_time_format=>"MM/DD/YY TIME", :day_month_sequence=>"DD/MM", :map=>doc[:map][:map], :controller=>"store_settings", :action=>"csvDoImport", :store_id=> @store.id, :name=>doc[:name], :type=>'order', :flag=>'file_upload'}
+      expect(response.status).to eq(200)
+      expect(Order.all.count).to eq(9)
+      expect(Product.all.count).to eq(21)
+      # import products with option 2
+      request.accept = "application/json"
+      get :csvImportData, {:type => 'product', :id => @store.id}
+      expect(response.status).to eq(200)
+
+      request.accept = "application/json"
+      get :createUpdateStore, {:store_type => 'CSV', :id => @store.id, :productfile => fixture_file_upload(Rails.root.join('/files/MT_Products_04.csv'))}
+      expect(response.status).to eq(200)
+
+      doc = IO.read(Rails.root.join("spec/fixtures/files/MT_Products_04_map_option3"))
+      doc = eval(doc)
+      request.accept = "application/json"
+      post :csvDoImport, {:rows=>"2", :sep=>",", :other_sep=>"0", :delimiter=>"\"", :fix_width=>"0", :fixed_width=>"4", :contains_unique_order_items=>false, :generate_barcode_from_sku=>false, :use_sku_as_product_name=>false, :import_action=>doc[:map][:import_action], :map=>doc[:map][:map], :controller=>"store_settings", :action=>"csvDoImport", :store_id=> @store.id, :name=>doc[:name], :type=>'product', :flag=>'file_upload'}
+      expect(response.status).to eq(200)
+      expect(Product.all.count).to eq(41)
+    end
+    it "Delete a product during csv product import if the product name matches `[DELETE]`" do
+      @inv_wh = FactoryGirl.create(:inventory_warehouse, :name=>'csv_inventory_warehouse')
+      @store = FactoryGirl.create(:store, :name=>'csv_store', :store_type=>'CSV', :inventory_warehouse=>@inv_wh)
+      expect(Order.all.count).to eq(0)
+      expect(Product.all.count).to eq(0)
+      # import orders
+      request.accept = "application/json"
+      get :csvImportData, {:type => 'order', :id => @store.id}
+      expect(response.status).to eq(200)
+
+      request.accept = "application/json"
+      get :createUpdateStore, {:store_type => 'CSV', :id => @store.id, :orderfile => fixture_file_upload(Rails.root.join('/files/MT_Orders_04.csv'))}
+      expect(response.status).to eq(200)
+
+      doc = IO.read(Rails.root.join("spec/fixtures/files/MT_Orders_04_map"))
+      doc = eval(doc)
+
+      request.accept = "application/json"
+      post :csvDoImport, {:rows=>"2", :sep=>",", :other_sep=>"0", :delimiter=>"\"", :fix_width=>"0", :fixed_width=>"4", :import_action=>nil, :contains_unique_order_items=>false, :generate_barcode_from_sku=>false, :use_sku_as_product_name=>false, :order_date_time_format=>"MM/DD/YY TIME", :day_month_sequence=>"DD/MM", :map=>doc[:map][:map], :controller=>"store_settings", :action=>"csvDoImport", :store_id=> @store.id, :name=>doc[:name], :type=>'order', :flag=>'file_upload'}
+      expect(response.status).to eq(200)
+      expect(Order.all.count).to eq(9)
+      expect(Product.all.count).to eq(21)
+      product_id=ProductSku.where(:sku=>"R.S-CS.OFF").first.product.id
+      # import products with option 2
+      request.accept = "application/json"
+      get :csvImportData, {:type => 'product', :id => @store.id}
+      expect(response.status).to eq(200)
+
+      request.accept = "application/json"
+      get :createUpdateStore, {:store_type => 'CSV', :id => @store.id, :productfile => fixture_file_upload(Rails.root.join('/files/MT_Products_04_delete.csv'))}
+      expect(response.status).to eq(200)
+
+      doc = IO.read(Rails.root.join("spec/fixtures/files/MT_Products_04_map_option2"))
+      doc = eval(doc)
+      request.accept = "application/json"
+      post :csvDoImport, {:rows=>"2", :sep=>",", :other_sep=>"0", :delimiter=>"\"", :fix_width=>"0", :fixed_width=>"4", :contains_unique_order_items=>false, :generate_barcode_from_sku=>false, :use_sku_as_product_name=>false, :import_action=>doc[:map][:import_action], :map=>doc[:map][:map], :controller=>"store_settings", :action=>"csvDoImport", :store_id=> @store.id, :name=>doc[:name], :type=>'product', :flag=>'file_upload'}
+      expect(response.status).to eq(200)
+      expect(Product.all.count).to eq(20)
+      expect(Product.where(:id=>product_id).size).to eq(0)
+    end
   end
 end
