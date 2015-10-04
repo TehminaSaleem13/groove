46954dd5294adb1679558022740c5ed6fe24a053
Shyam Visamsetty <svisamsetty@navaratan.com>
2015-10-04 11:21:09 +0000

    restart_delayed_job.sh edited online with Bitbucket

--- restart_delayed_job.sh
+++ restart_delayed_job.sh
@@ -58,6 +58,14 @@ git stash
 git checkout ${ENV}
 git pull origin ${ENV}
 
+git submodule init
+git submodule update --recursive
+
+cd ~/groove/nodeserver/socket-server
+npm install
+
+cd ~/groove/nodeserver/oauth-server
+npm install
 
 RAILS_ENV=${ENV} bundle exec bundle install --deployment
 RAILS_ENV=${ENV} bundle exec script/delayed_job -n ${NUM_JOBS} start
