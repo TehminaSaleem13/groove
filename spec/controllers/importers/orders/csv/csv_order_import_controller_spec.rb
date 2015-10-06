c2d7cad1666ff09920c857f9490ce897676215e1
Kalakar <ksahoo@navaratan.com>
2015-10-06 21:40:22 +0530

    tests for corner cases in csv order import. more to be added

--- spec/controllers/importers/orders/csv/csv_order_import_controller_spec.rb
+++ spec/controllers/importers/orders/csv/csv_order_import_controller_spec.rb
@@ -179,6 +179,34 @@ describe StoresController do
         expect(Product.all.count).to eq(13)
         expect(Product.where(:is_intangible=>true).size).to eq(3)
       end
+      describe "Generate Barcode From SKU" do
+        it "Products will be imported for each order item of each order with Barcode value same as SKU" do
+          @inv_wh = FactoryGirl.create(:inventory_warehouse, :name=>'csv_inventory_warehouse')
+          @store = FactoryGirl.create(:store, :name=>'csv_store', :store_type=>'CSV', :inventory_warehouse=>@inv_wh)
+          expect(Order.all.count).to eq(0)
+          expect(Product.all.count).to eq(0)
+          request.accept = "application/json"
+          get :csv_import_data, {:type => 'order', :id => @store.id}
+          expect(response.status).to eq(200)
+
+          request.accept = "application/json"
+          get :create_update_store, {:store_type => 'CSV', :id => @store.id, :orderfile => fixture_file_upload(Rails.root.join('/files/Order_Import_Test.csv'))}
+          expect(response.status).to eq(200)
+
+          doc = IO.read(Rails.root.join("spec/fixtures/files/Unique_Orders_map"))
+          doc = eval(doc)
+
+          request.accept = "application/json"
+          post :csv_do_import, {:id => @store.id, :rows=>"2", :sep=>",", :other_sep=>"0", :delimiter=>"\"", :fix_width=>"0", :fixed_width=>"4", :import_action=>nil, :contains_unique_order_items=>true, :generate_barcode_from_sku=>true, :use_sku_as_product_name=>false, :order_date_time_format=>doc[:map][:order_date_time_format], :day_month_sequence=>"DD/MM", :map=>doc[:map][:map], :controller=>"stores", :action=>"csv_do_import", :store_id=> @store.id, :name=>doc[:name], :type=>'order', :flag=>'file_upload'}
+          expect(response.status).to eq(200)
+          expect(Order.all.count).to eq(4)
+          expect(Product.all.count).to eq(13)
+          products = Product.where(Product.arel_table[:base_sku].not_eq(nil))
+          products.each do |product|
+            expect(product.primary_barcode).to eq(product.primary_sku)
+          end
+        end
+      end
     end
   end
 end
