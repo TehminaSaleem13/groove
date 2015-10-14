b3c0a4e25f47f2480166298ae0e0257699fdc5db
Kapil <kchoudhary006@gmail.com>
2015-10-14 16:15:23 +0530

    Updated rspec for products import - If the product name in any record in the csv is blank, then the existing product name should remain unchanged - If a record in a csv does not have a barcode for an existing product, then the barcode must be ignored and other fields(product name, category etc.) must be modified.

--- spec/controllers/importers/products/csv/csv_product_import_controller_spec.rb
+++ spec/controllers/importers/products/csv/csv_product_import_controller_spec.rb
@@ -155,5 +155,82 @@ describe StoresController do
       expect(Product.all.count).to eq(20)
       expect(Product.where(:id=>product_id).size).to eq(0)
     end
+
+    it "If the product name in any record in the csv is blank, then existing product name should remain unchanged" do
+      # import orders
+      request.accept = "application/json"
+      get :csv_import_data, {:type => 'order', :id => @store.id}
+      expect(response.status).to eq(200)
+
+      request.accept = "application/json"
+      get :create_update_store, {:store_type => 'CSV', :status=> @store.status, :name => @store.name, :inventory_warehouse_id => @store.inventory_warehouse_id, :id => @store.id, :orderfile => fixture_file_upload(Rails.root.join('/files/MT_Orders_04.csv'))}
+      expect(response.status).to eq(200)
+
+      doc = IO.read(Rails.root.join("spec/fixtures/files/MT_Orders_04_map"))
+      doc = eval(doc)
+
+      request.accept = "application/json"
+      post :csv_do_import, {:id => @store.id, :rows=>"2", :sep=>",", :other_sep=>"0", :delimiter=>"\"", :fix_width=>"0", :fixed_width=>"4", :import_action=>nil, :contains_unique_order_items=>false, :generate_barcode_from_sku=>false, :use_sku_as_product_name=>false, :order_date_time_format=>"MM/DD/YY TIME", :day_month_sequence=>"DD/MM", :map=>doc[:map][:map], :controller=>"stores", :action=>"csv_do_import", :store_id=> @store.id, :name=>doc[:name], :type=>'order', :flag=>'file_upload'}
+      expect(response.status).to eq(200)
+      expect(Order.all.count).to eq(9)
+      expect(Product.all.count).to eq(21)
+      # import products with option 2
+      request.accept = "application/json"
+      get :csv_import_data, {:type => 'product', :id => @store.id}
+      expect(response.status).to eq(200)
+      products_names = Product.all.map(&:name)
+      request.accept = "application/json"
+      get :create_update_store, {:store_type => 'CSV', :status=> @store.status, :name => @store.name, :inventory_warehouse_id => @store.inventory_warehouse_id, :id => @store.id, :productfile => fixture_file_upload(Rails.root.join('/files/MT_Products_04_name_blank.csv'))}
+      expect(response.status).to eq(200)
+
+      doc = IO.read(Rails.root.join("spec/fixtures/files/MT_Products_04_map_name_blank"))
+      doc = eval(doc)
+      request.accept = "application/json"
+      post :csv_do_import, {:id => @store.id, :rows=>"2", :sep=>",", :other_sep=>"0", :delimiter=>"\"", :fix_width=>"0", :fixed_width=>"4", :contains_unique_order_items=>false, :generate_barcode_from_sku=>false, :use_sku_as_product_name=>false, :import_action=>doc[:map][:import_action], :map=>doc[:map][:map], :controller=>"stores", :action=>"csv_do_import", :store_id=> @store.id, :name=>doc[:name], :type=>'product', :flag=>'file_upload'}
+      expect(response.status).to eq(200)
+      expect(Product.all.count).to eq(21)
+      expect(Product.all.map(&:name)).to match_array(products_names)
+    end
+
+
+    it "If the product name in any record in the csv is blank, then existing product name should remain unchanged" do
+      # import orders
+      request.accept = "application/json"
+      get :csv_import_data, {:type => 'order', :id => @store.id}
+      expect(response.status).to eq(200)
+
+      request.accept = "application/json"
+      get :create_update_store, {:store_type => 'CSV', :status=> @store.status, :name => @store.name, :inventory_warehouse_id => @store.inventory_warehouse_id, :id => @store.id, :orderfile => fixture_file_upload(Rails.root.join('/files/MT_Orders_04.csv'))}
+      expect(response.status).to eq(200)
+
+      doc = IO.read(Rails.root.join("spec/fixtures/files/MT_Orders_04_map"))
+      doc = eval(doc)
+
+      request.accept = "application/json"
+      post :csv_do_import, {:id => @store.id, :rows=>"2", :sep=>",", :other_sep=>"0", :delimiter=>"\"", :fix_width=>"0", :fixed_width=>"4", :import_action=>nil, :contains_unique_order_items=>false, :generate_barcode_from_sku=>false, :use_sku_as_product_name=>false, :order_date_time_format=>"MM/DD/YY TIME", :day_month_sequence=>"DD/MM", :map=>doc[:map][:map], :controller=>"stores", :action=>"csv_do_import", :store_id=> @store.id, :name=>doc[:name], :type=>'order', :flag=>'file_upload'}
+      expect(response.status).to eq(200)
+      expect(Order.all.count).to eq(9)
+      expect(Product.all.count).to eq(21)
+      # import products with option 2
+      request.accept = "application/json"
+      get :csv_import_data, {:type => 'product', :id => @store.id}
+      expect(response.status).to eq(200)
+      products_names = Product.all.map(&:name)
+      request.accept = "application/json"
+      get :create_update_store, {:store_type => 'CSV', :status=> @store.status, :name => @store.name, :inventory_warehouse_id => @store.inventory_warehouse_id, :id => @store.id, :productfile => fixture_file_upload(Rails.root.join('/files/MT_Products_04_barcode_blank.csv'))}
+      expect(response.status).to eq(200)
+      product = Product.includes(:product_skus).where("product_skus.sku = 'R.S'").first
+      doc = IO.read(Rails.root.join("spec/fixtures/files/MT_Products_04_map_barcode_blank"))
+      doc = eval(doc)
+      request.accept = "application/json"
+      post :csv_do_import, {:id => @store.id, :rows=>"2", :sep=>",", :other_sep=>"0", :delimiter=>"\"", :fix_width=>"0", :fixed_width=>"4", :contains_unique_order_items=>false, :generate_barcode_from_sku=>false, :use_sku_as_product_name=>false, :import_action=>doc[:map][:import_action], :map=>doc[:map][:map], :controller=>"stores", :action=>"csv_do_import", :store_id=> @store.id, :name=>doc[:name], :type=>'product', :flag=>'file_upload'}
+      
+      expect(response.status).to eq(200)
+      updated_product = Product.includes(:product_skus).where("product_skus.sku = 'R.S'").first
+      expect(product.name).to_not eq(updated_product.name)
+      expect(product.weight).to_not eq(updated_product.weight)
+    end
+
+
   end
 end
