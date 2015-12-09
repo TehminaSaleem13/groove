5d86e3e3ebbd99802f4e67655b2ab05e49df148a
Kapil <kchoudhary006@gmail.com>
2015-12-09 17:16:05 +0530

    Pushing schema changess

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151119104627) do
+ActiveRecord::Schema.define(:version => 20151201041703) do
 
   create_table "access_restrictions", :force => true do |t|
     t.integer  "num_users",               :default => 0,     :null => false
