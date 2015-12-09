a503fc8fc3dff962bf5359d6e201a4acfc43f1ae
Ashish Singh <aitashish173@gmail.com>
2015-12-09 18:36:26 +0530

    created product scan service

---
+++ app/services/scan_pack/product_scan_service.rb
@@ -0,0 +1,13 @@
+module ScanPack
+  class ProductScanService < ScanPack::Base
+    def initialize
+    end
+
+    def run
+    end
+
+    def product_scan
+    end
+    
+  end # class end
+end #module end
