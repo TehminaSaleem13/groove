e63e56ac51bc2a3a2740dce1f3b57022593cddd7
Ashish Singh <aitashish173@gmail.com>
2015-12-23 14:11:36 +0530

    bug fix for GROOV-782

--- app/helpers/scan_pack_helper.rb
+++ app/helpers/scan_pack_helper.rb
@@ -525,12 +525,12 @@ module ScanPackHelper
   #   @generate_barcode.save
   # end
 
-  # Remove those order_items that are skippable when the scanned barcode
-  # is SKIP entered as the barcode.
-  def remove_skippable_product item
-    order_item = OrderItem.find(item['order_item_id'])
-    order = order_item.order
-    order.order_items.delete(order_item)
-    order.save
-  end
+  # # Remove those order_items that are skippable when the scanned barcode
+  # # is SKIP entered as the barcode.
+  # def remove_skippable_product item
+  #   order_item = OrderItem.find(item['order_item_id'])
+  #   order = order_item.order
+  #   order.order_items.delete(order_item)
+  #   order.save
+  # end
 end
