54aacdd4097d41728707ea1026a2141f00527385
Ashish Singh <aitashish173@gmail.com>
2015-12-28 12:27:35 +0530

    refactored scan recording service

---
+++ app/services/scan_pack/scan_recording_service.rb
@@ -0,0 +1,43 @@
+class ScanPack::ScanRecordingService
+  def initialize(args)
+    @current_user, @input, @state, @id = args
+    @result = {
+      "status"=>true,
+      "matched"=>false,
+      "error_messages"=>[],
+      "success_messages"=>[],
+      "notice_messages"=>[],
+      "data"=>{
+        'next_state' => 'scanpack.rfp.recording'
+      }
+    }
+    @order = Order.where(id: @id).last
+  end
+
+  def run
+    case true
+    when @input.blank?
+      set_error_messages("No tracking number is provided")
+    when @order.blank?
+      set_error_messages("Could not find order with id: "+id)
+    else
+      scan_recording
+    end
+    @result
+  end
+
+  def scan_recording
+    if @order.status == 'awaiting'
+      #allow tracking id to be saved without special permissions
+      @order.tracking_num = @input
+      @order.set_order_to_scanned_state(@current_user.username)
+      @result['data']['order_complete'] = true
+      @result['data']['next_state'] = 'scanpack.rfo'
+      #update inventory when inventory warehouses is implemented.
+      @order.save
+    else
+      set_error_messages("The order is not in awaiting state. Cannot scan the tracking number")
+    end
+  end
+
+end
