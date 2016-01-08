eafd8a1f09ea4ef704a298bd05dc87a4fe0837bb
Kapil <kchoudhary006@gmail.com>
2016-01-08 17:09:22 +0530

    ShippingEasy - code to delete cleared orders if not scanned

--- lib/groovepacker/shipping_easy/client.rb
+++ lib/groovepacker/shipping_easy/client.rb
@@ -12,30 +12,49 @@ module Groovepacker
 
       def orders(statuses, importing_time, import_item)
         page_index = 1
-        combined_response = {}
-        combined_response["orders"] = []
+        combined_response = {"orders" => []}
         filters = {page: page_index, per_page: 100, status: statuses, last_updated_at: importing_time-4.days}
+        filters = filters.merge(api_key_and_secret)
 
         unless import_item.import_type=='deep'
           last_import = @credential.last_imported_at rescue (importing_time - 4.days)
           filters[:last_updated_at] = last_import.to_s
         end
 
-        filters = filters.merge(api_key_and_secret)
-
         return combined_response if @api_key.blank? || @api_secret.blank?
         while page_index
-          response = ::ShippingEasy::Resources::Order.find_all(filters)
-          response["orders"] = response["orders"].select {|order| order["order_status"]!="drop_shipped" }
+          response = fetch_orders(filters)
           combined_response["orders"] += response["orders"]
           break if response["orders"].length < 100
           page_index += 1
         end
+        combined_response["cleared_orders_ids"] = get_cleared_orders_ids(combined_response["orders"])
+        combined_response["orders"] = remove_cleared_and_drop_shipped(combined_response["orders"])
+        combined_response["error"] = response["error"]
         combined_response
       end
 
       def api_key_and_secret
         {api_key: @api_key, api_secret: @api_secret}
+      end
+
+      def fetch_orders(filters)
+        begin
+          response = ::ShippingEasy::Resources::Order.find_all(filters)
+        rescue => e
+          error = JSON.parse(e.to_s)
+          error = error["errors"].first rescue {"message" => 'Something went wrong while fetching orders'}
+          response = {"orders" => [], "error" => error}
+        end
+        response
+      end
+
+      def remove_cleared_and_drop_shipped(orders)
+        orders.select {|order| ["drop_shipped", "cleared"].exclude?(order["order_status"]) }
+      end
+
+      def get_cleared_orders_ids(orders)
+        orders.map {|order| order["external_order_identifier"] if order["order_status"]=="cleared" }.compact
       end
 
     end
