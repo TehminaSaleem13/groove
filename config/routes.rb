5fefc2379e70c7b4130f1dffa11988790f9e9d2d
Kapil <kchoudhary006@gmail.com>
2016-01-09 20:49:26 +0530

    Added routes for print_action_barcode - GROOV-810

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Groovepacks::Application.routes.draw do
   get '/magento_rest/:store_id/get_access_token' => 'magento_rest#get_access_token'
   put '/magento_rest/:store_id/disconnect' => 'magento_rest#disconnect'
   get 'stores/export_active_products' => 'stores#export_active_products'
+  get '/settings/print_action_barcode/:id' => 'settings#print_action_barcode'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
