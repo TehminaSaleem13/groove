9ddb2c0cba753a1321632c2750969ad86a6763e3
Ashish Singh <aitashish173@gmail.com>
2016-01-15 14:41:18 +0530

    bug fix for typescan GROOVE-772

--- app/assets/javascripts/groovepacks/controllers/scanpack/rfp/scanPackRfpTypeScan.js
+++ app/assets/javascripts/groovepacks/controllers/scanpack/rfp/scanPackRfpTypeScan.js
@@ -18,7 +18,10 @@ groovepacks_controllers.controller('scanPackRfpTypeScan', ['$scope', '$modalInst
 
     $scope.update = function (reason) {
       if (reason == 'ok-enter-key') {
-        if (typeof($scope.code.count) != 'number' && $scope.code.count < 1 && $scope.code.count > $scope.order.next_item.qty) {
+        if (
+          typeof($scope.code.count) != 'number' || $scope.code.count < 1 ||
+          $scope.code.count > $scope.order.next_item.qty_remaining
+          ) {
           notification.notify("Wrong count has been entered. Please try again.");
           $modalInstance.dismiss("wrong-count");
         } else {
