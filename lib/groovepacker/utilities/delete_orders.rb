32636409499aaa6ff66d38f113a886a16247eaaf
Kalakar <ksahoo@navaratan.com>
2016-02-04 19:54:36 +0530

    refactored delete_orders file to improve code climate score

--- lib/groovepacker/utilities/delete_orders.rb
+++ lib/groovepacker/utilities/delete_orders.rb
@@ -10,22 +10,31 @@ class DeleteOrders
     tenants.each do |tenant|
       begin
         Apartment::Tenant.switch(tenant.name)
-        updated_time = (DateTime.now.utc - 91.days).beginning_of_day
-        orders = Order.where('updated_at < ?', updated_time)
-        next if orders.empty?
-        back_hash = []
-        orders.each do |order|
-          back_hash.push(build_hash(order.id))
-        end
-        file_name = Time.now.strftime('%d_%b_%Y_%I__%M_%p')
-        GroovS3.create_order_backup(tenant.name, file_name, back_hash.to_json)
-        orders.each do |order|
-          delete_order_data(order)
-        end
+        updated_time = (DateTime.now.utc - 30.days).beginning_of_day
+        @orders = Order.where('updated_at < ?', updated_time)
+        next if @orders.empty?
+        take_backup(tenant.name)
+        delete_orders
       rescue Exception => e
         puts e.message
         puts e.backtrace.join("\n")
       end
+    end
+  end
+
+  def take_backup(tenant)
+    back_hash = []
+    @orders.each do |order|
+      back_hash.push(build_hash(order.id))
+    end
+    puts back_hash.inspect
+    file_name = Time.now.strftime('%d_%b_%Y_%I__%M_%p')
+    GroovS3.create_order_backup(tenant, file_name, back_hash.to_json)
+  end
+
+  def delete_orders
+    @orders.each do |order|
+      delete_order_data(order)
     end
   end
 
@@ -72,8 +81,12 @@ class DeleteOrders
 
   def build_order_hash(order)
     order_columns = Order.column_names
-    order_columns.each do |name|
-      @record_hash[:order][name] = order[name]
+    build_single_hash(order_columns, order, 'order')
+  end
+
+  def build_single_hash(column_names, record, hash_key)
+    column_names.each do |name|
+      @record_hash[hash_key][name] = record[name]
     end
   end
 
@@ -81,9 +94,7 @@ class DeleteOrders
     @exception = order.order_exception
     if @exception
       exception_columns = OrderException.column_names
-      exception_columns.each do |name|
-        @record_hash[:order_exception][name] = @exception[name]
-      end
+      build_single_hash(exception_columns, @exception, 'order_exception')
     end
   end
 
@@ -91,9 +102,7 @@ class DeleteOrders
     @shipping = order.order_shipping
     if @shipping
       shipping_cloumns = OrderShipping.column_names
-      shipping_cloumns.each do |name|
-        @record_hash[:order_shipping][name] = @shipping[name]
-      end
+      build_single_hash(shipping_cloumns, @shipping, 'order_shipping')
     end
   end
 
@@ -101,79 +110,64 @@ class DeleteOrders
     @activities = order.order_activities
     return if @activities.empty?
     activity_columns = OrderActivity.column_names
-    @activities.each do |activity|
-      result = build_hash_common(activity, activity_columns)
-      @record_hash[:order_activities].push(result)
-    end
+    build_hash_common(@activities, activity_columns, 'order_activities')
   end
 
   def build_serial_hash(order)
     @serials = order.order_serials
     return if @serials.empty?
     serial_columns = OrderSerial.column_names
-    @serials.each do |serial|
-      result = build_hash_common(serial, serial_columns)
-      @record_hash[:order_serials].push(result)
-    end
+    build_hash_common(@serials, serial_columns, 'order_serials')
   end
 
   def build_order_item_hash(order_items)
     return if order_items.empty?
     item_columns = OrderItem.column_names
-    order_items.each do |item|
-      result = build_hash_common(item, item_columns)
-      @record_hash[:order_items].push(result)
-    end
+    build_hash_common(order_items, item_columns, 'order_items')
   end
 
   def build_oikp_hash(item)
     @item_kit_products = item.order_item_kit_products
     return if @item_kit_products.empty?
     ikp_columns = OrderItemKitProduct.column_names
-    @item_kit_products.each do |ikp|
-      result = build_hash_common(ikp, ikp_columns)
-      @record_hash[:order_item_kit_products].push(result)
-    end
+    build_hash_common(@item_kit_products, ikp_columns, 'order_item_kit_products')
   end
 
   def build_oiospl_hash(item)
     @iospl = item.order_item_order_serial_product_lots
     return if @iospl.empty?
     iospl_columns = OrderItemOrderSerialProductLot.column_names
-    @iospl.each do |i|
-      result = build_hash_common(i, iospl_columns)
-      @record_hash[:order_item_order_serial_product_lots].push(result)
-    end
+    build_hash_common(@iospl, iospl_columns, 'order_item_order_serial_product_lots')
   end
 
   def build_oist_hash(item)
     @item_scan_times = item.order_item_scan_times
     return if @item_scan_times.empty?
     ist_columns = OrderItemScanTime.column_names
-    @item_scan_times.each do |item|
-      result = build_hash_common(item, ist_columns)
-      @record_hash[:order_item_scan_times].push(result)
-    end
+    build_hash_common(@item_scan_times, ist_columns, 'order_item_scan_times')
   end
 
-  def build_hash_common(record, column_names)
+  def build_hash_common(items, column_names, hash_key)
+    items.each do |item|
       result = {}
       column_names.each do |name|
-        result[name] = record[name]
+        result[name] = item[name]
       end
+      @record_hash[hash_key].push(result)
+    end
   end
 
   def build_record_hash
     {
-      order: {},
-      order_activities: [],
-      order_exception: {},
-      order_shipping: {},
-      order_serials: [],
-      order_items: [],
-      order_item_kit_products: [],
-      order_item_order_serial_product_lots: [],
-      order_item_scan_times: []
+      "order"=> {},
+      "order_activities"=> [],
+      "order_exception"=> {},
+      "order_shipping"=> {},
+      "order_serials"=> [],
+      "order_items"=> [],
+      "order_item_kit_products"=> [],
+      "order_item_order_serial_product_lots"=> [],
+      "order_item_scan_times"=> []
     }
   end
 end
