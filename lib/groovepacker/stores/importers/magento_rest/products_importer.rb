43663aa1332ee5ec68cbb697a9aa0eb398804875
Kapil <kchoudhary006@gmail.com>
2016-02-09 18:52:53 +0530

    Add Magento Import Catalog Button : GROOV-816

--- lib/groovepacker/stores/importers/magento_rest/products_importer.rb
+++ lib/groovepacker/stores/importers/magento_rest/products_importer.rb
@@ -97,7 +97,7 @@ module Groovepacker
                 @productdb.save
                 make_product_intangible(@productdb)
                 @productdb.set_product_status
-                update_product_inventory(@productdb, product_attrs)
+                update_product_inventory(client, @productdb, product_attrs)
                 result_product_id = @productdb.id
               end
             rescue Exception => e
@@ -107,10 +107,12 @@ module Groovepacker
           end
 
 
-          def update_product_inventory(product, shopify_product_attrs)
+          def update_product_inventory(client, product, magento_product_attrs)
             inv_wh = product.product_inventory_warehousess.first
             inv_wh = product.product_inventory_warehousess.new if inv_wh.blank?
-            inv_wh.quantity_on_hand = shopify_product_attrs["stock_data"]["qty"].try(:to_i) + inv_wh.allocated_inv.to_i
+            stock_data = magento_product_attrs["stock_data"]
+            stock_data = client.stock_item(magento_product_attrs["entity_id"]) if stock_data.blank?
+            inv_wh.quantity_on_hand = stock_data["qty"].try(:to_i) + inv_wh.allocated_inv.to_i
             inv_wh.save
           end
         end
