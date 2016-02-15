f99acda53520bc3934cb981099b42c68f82bc2be
Kalakar <ksahoo@navaratan.com>
2016-02-15 17:38:58 +0530

    switch tenant to avoid could not find order with id error

--- lib/groovepacker/utilities/send_stat_stream.rb
+++ lib/groovepacker/utilities/send_stat_stream.rb
@@ -1,14 +1,14 @@
 class SendStatStream
 
   def build_send_stream(tenant, order_id)
-    stat_stream = build_stream(order_id)
+    stat_stream = build_stream(tenant, order_id)
     send_stream(tenant, stat_stream)
   end
 
-  def build_stream(order_id)
+  def build_stream(tenant, order_id)
     stat_stream_obj =
       Groovepacker::Dashboard::Stats::AnalyticStatStream.new()
-    stat_stream_obj.build_stream(order_id)
+    stat_stream_obj.get_order_stream(tenant, order_id)
   end
 
   def send_stream(tenant, stat_stream)
