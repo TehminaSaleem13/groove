9d9fc55d78ca05d413a0a173e3c4761fe7d23c62
Kalakar <ksahoo@navaratan.com>
2016-02-15 11:16:21 +0530

    send request to groovelytics server to create tenant through delayed_job

--- lib/groovepacker/utilities/create_tenant.rb
+++ lib/groovepacker/utilities/create_tenant.rb
@@ -3,12 +3,8 @@ class CreateTenant
     Apartment::Tenant.create(subscription.tenant_name)
     tenant = Tenant.create(name: subscription.tenant_name)
     subscription.tenant = tenant
-    begin
-      HTTParty.post("#{ENV["GROOV_ANALYTIC"]}/tenants",
-        query: { tenant_name: subscription.tenant_name })
-    rescue Exception => e
-      Rails.logger.error e.backtrace.join("\n")
-    end
+    self.delay(run_at: 1.seconds.from_now).create_groovelytics_tenant(subscription.tenant_name)
+    # self.create_groovelytics_tenant(subscription.tenant_name)
     Apartment::Tenant.switch(subscription.tenant_name)
     ApplyAccessRestrictions.new.delay(:run_at => 10.minutes.from_now, :queue => "apply_access_restrictions_#{subscription.tenant_name}").apply_access_restrictions(subscription.tenant_name, subscription.subscription_plan_id)
     Groovepacker::SeedTenant.new.seed(true,
@@ -20,5 +16,14 @@ class CreateTenant
     TransactionEmail.delay(run_at: 2.hours.from_now).send_email(subscription)
     TransactionEmail.welcome_email(subscription).deliver
     subscription.update_progress("email_sent")
+  end
+
+  def create_groovelytics_tenant(tenant_name)
+    begin
+      HTTParty.post("#{ENV["GROOV_ANALYTIC"]}/tenants",
+        query: { tenant_name: tenant_name })
+    rescue Exception => e
+      Rails.logger.error e.backtrace.join("\n")
+    end
   end
 end
