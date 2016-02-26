474ce8c2974cec47b255284b86f15b69c9fe92cb
Ashish Singh <aitashish173@gmail.com>
2016-02-26 19:07:26 +0530

    bug fix for product edit service

--- app/services/scan_pack/product_edit_conf_service.rb
+++ app/services/scan_pack/product_edit_conf_service.rb
@@ -35,7 +35,7 @@ class ScanPack::ProductEditConfService < ScanPack::Base
     @session[:product_edit_matched_for_current_user] = true
     @session[:product_edit_matched_for_products] = []
     @result['data']['inactive_or_new_products'].each do |inactive_new_product|
-      @session[:product_edit_matched_for_products].push(inactive_new_product.id)
+      @session[:product_edit_matched_for_products].push(inactive_new_product['id'])
     end
     @session[:product_edit_matched_for_order] = @single_order.id
   end
