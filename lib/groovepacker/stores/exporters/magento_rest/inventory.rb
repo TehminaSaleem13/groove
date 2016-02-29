fceff7159449e1fb053e06c0cd48e2665d5c5d7d
Kapil <kchoudhary006@gmail.com>
2016-02-29 12:25:35 +0530

    Refactored magento reset exporter

--- lib/groovepacker/stores/exporters/magento_rest/inventory.rb
+++ lib/groovepacker/stores/exporters/magento_rest/inventory.rb
@@ -7,19 +7,15 @@ module Groovepacker
 
           def push_inventories
             handler = self.get_handler
-            result = get_inv_exporter(handler).push_inventories
-          end
-
-          private
-            def get_inv_exporter(handler)
-              creds = handler[:credential]
-              if creds.store_version=='1.x'
-                exporter = Groovepacker::Stores::Exporters::MagentoRest::V1::Inventory.new(handler)
-                return exporter
-              end
+            creds = handler[:credential]
+            if creds.store_version=='1.x'
+              exporter = Groovepacker::Stores::Exporters::MagentoRest::V1::Inventory.new(handler)
+            else
               exporter = Groovepacker::Stores::Exporters::MagentoRest::V2::Inventory.new(handler)
             end
-
+            result = exporter.push_inventories
+          end
+          
         end
       end
     end
