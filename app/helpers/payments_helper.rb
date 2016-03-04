e8d708593448dca9e4603590c9ccf39e7dfb5f45
Kalakar <ksahoo@navaratan.com>
2016-03-04 17:58:33 +0530

    correct plan ids

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -178,14 +178,14 @@ module PaymentsHelper
       'annual-symphony' => 'annual-groove-symphony',
       'duo-60' => 'groove-duo-60',
       'trio-90' => 'groove-trio-90',
-      'quintet-120' => 'groove-quintet-120',
-      'quartet-150' => 'groove-quartet-150',
+      'quartet-120' => 'groove-quartet-120',
+      'quintet-150' => 'groove-quintet-150',
       'bigband-210' => 'groove-bigband-210',
       'symphony-300' => 'groove-symphony-300',
       'an-duo' => 'an-groove-duo',
       'an-trio' => 'an-groove-trio',
-      'an-quintet' => 'an-groove-quintet',
       'an-quartet' => 'an-groove-quartet',
+      'an-quintet' => 'an-groove-quintet',
       'an-bigband' => 'an-groove-bigband',
       'an-symphony' => 'an-groove-symphony'
     }
