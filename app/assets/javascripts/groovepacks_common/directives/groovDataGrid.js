82cdc20efee25898e7e2002c4e478eeba9e654f6
Ashish Singh <aitashish173@gmail.com>
2016-03-09 14:00:07 +0530

    UI Improvements for DATAGRID GROOV-928

--- app/assets/javascripts/groovepacks_common/directives/groovDataGrid.js
+++ app/assets/javascripts/groovepacks_common/directives/groovDataGrid.js
@@ -148,15 +148,15 @@ groovepacks_directives.directive('groovDataGrid', ['$timeout', '$http', '$sce', 
         if (typeof scope.editable[field][ind] == "undefined") {
           scope.editable[field][ind] = $sce.trustAsHtml(
             '<div ng-class="{\'grid-editable-field\': !editable_modal.status()}" ' + 
-                'style="width:' + scope.options.all_fields[field].col_length + 'rem"' + 
+                'style="width:' + ((scope.options.all_fields[field].col_length || scope.options.col_length) - 10) + 'rem" ' + 
                 //'ng-mouseover="row.show_pencil ? (row.show_pencil[field]=true) : (row.show_pencil={})"' +
                 //'ng-mouseleave="row.show_pencil ? (row.show_pencil[field]=false) : (row.show_pencil={})" ' + 
                 'groov-editable="options.editable" prop="{{field}}" ng-model="' +
                 scope.options.all_fields[field].model + '" identifier="' +
                 scope.options.identifier + '_list-' + field + '-' + ind + '">' +
-              '<a class="pull-right fa datagrid-pencil fa-pencil pointer" ' +
+              '<span class="pull-right fa datagrid-pencil fa-pencil pointer" ' +
                 'ng-show="row.show_pencil[field]" groov-click="compile(' + ind + ',\'' +field+ '\')">' +
-              '</a>' +
+              '</span>' +
               scope.options.all_fields[field].transclude +
             '</div>'
           );
