4aa4b5e70ba2f0e75ea36ed44b2ea974d0d321f2
Kalakar <ksahoo@navaratan.com>
2016-03-16 10:19:33 +0530

    config changes for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,12 @@ Groovepacks::Application.configure do
     :password => 'ckWnOifHhLOJRiqZQ-ZRKA',
     :openssl_verify_mode  => 'none'
   }
-  $redis = Redis.new(:host => 'groove-prod-1', :port=> 6379)
+  ENV['REDIS_HOST'] = 'groovelytics-redis'
+  ENV['REDIS_PASSWORD'] = 'WmHE1h2oRJqIW76AtsC1Eg_ZJXe$S7w*UpOpJ1OT_yBKoFQYR1r938Oc!2Ahv2wr'
+  ENV['REDIS_PORT'] = '7743'
+  $redis = Redis.new(:host => ENV['REDIS_HOST'], :port => ENV['REDIS_PORT'].to_i,
+    password: ENV['REDIS_PASSWORD'])
+  # $redis = Redis.new(:host => 'groove-prod-1', :port=> 6379)
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   ENV['AMAZON_MWS_ACCESS_KEY_ID'] = "AKIAJ4VZ2GY7HZUL277Q"
