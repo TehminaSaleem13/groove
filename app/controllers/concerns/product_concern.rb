c6e59066f72044494642e6b66f4fd940fb18a56c
Kapil <kchoudhary006@gmail.com>
2016-03-21 20:40:55 +0530

    Quick fix for errbit error on product update

--- app/controllers/concerns/product_concern.rb
+++ app/controllers/concerns/product_concern.rb
@@ -10,7 +10,7 @@ module ProductConcern
   
   private
     def gp_products_module
-      Groovepacker::Products::Products.new(result: @result, params_attrs: params, current_user: current_user)
+      Groovepacker::Products::Products.new(result: @result, params_attrs: params, current_user: current_user, session: session)
     end
 
     def init_result_obj
