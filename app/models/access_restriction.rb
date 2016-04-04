8579066aa531a6ec92887256a1e083707facd8ed
Kapil <kchoudhary006@gmail.com>
2016-04-04 12:01:33 +0530

    remove from campaignmonitor list if scanned 30 orders

--- app/models/access_restriction.rb
+++ app/models/access_restriction.rb
@@ -2,11 +2,11 @@ class AccessRestriction < ActiveRecord::Base
   attr_accessible :tenant_id, :num_users, :num_shipments, :num_import_sources, :total_scanned_shipments
   has_one :tenant
 
-  after_save :remove_from_new_customers_if_scanned_10
+  after_save :remove_from_new_customers_if_scanned_30
 
-  def remove_from_new_customers_if_scanned_10
+  def remove_from_new_customers_if_scanned_30
     if self.total_scanned_shipments_changed?
-      initialize_campaingmonitor.remove_subscriber_from_new_customers_list if self.total_scanned_shipments==10
+      initialize_campaingmonitor.remove_subscriber_from_new_customers_list if self.total_scanned_shipments==30
     end
   end
 
