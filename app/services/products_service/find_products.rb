186ac843e6d475289c874e9a5a4b167768cc05f4
Ashish Singh <aitashish173@gmail.com>
2016-04-04 16:22:52 +0530

    efactored Get Product Service GROOV-1007

--- app/services/products_service/find_products.rb
+++ app/services/products_service/find_products.rb
@@ -14,75 +14,17 @@ module ProductsService
 
     def call
       setup_query_params
-
-      #hack to bypass for now and enable client development
-      # sort_key = 'name' if sort_key == 'sku'
-
-      #todo status filters to be implemented
-
-      if @sort_key == 'sku'
-        products = Product.find_by_sql("SELECT products.* FROM products LEFT JOIN product_skus ON ("+
-                                         "products.id = product_skus.product_id ) "+@kit_query+
-                                         @status_filter_text+"GROUP BY product_id ORDER BY product_skus.sku "+@sort_order+@query_add)
-      elsif @sort_key == 'store_type'
-        products = Product.find_by_sql("SELECT products.* FROM products LEFT JOIN stores ON ("+
-                                         "products.store_id = stores.id ) "+@kit_query+
-                                         @status_filter_text+" ORDER BY stores.name "+@sort_order+@query_add)
-      elsif @sort_key == 'barcode'
-        products = Product.find_by_sql("SELECT products.* FROM products LEFT JOIN product_barcodes ON ("+
-                                         "products.id = product_barcodes.product_id ) "+@kit_query+
-                                         @status_filter_text+" ORDER BY product_barcodes.barcode "+@sort_order+@query_add)
-      elsif @sort_key == 'location_primary'
-        products = Product.find_by_sql("SELECT products.* FROM products LEFT JOIN product_inventory_warehouses ON ( "+
-                                         "products.id = product_inventory_warehouses.product_id ) "+ @kit_query+
-                                         @status_filter_text+" ORDER BY product_inventory_warehouses.location_primary "+@sort_order+@query_add)
-      elsif @sort_key == 'location_secondary'
-        products = Product.find_by_sql("SELECT products.* FROM products LEFT JOIN product_inventory_warehouses ON ( "+
-                                         "products.id = product_inventory_warehouses.product_id ) "+@kit_query+
-                                         @status_filter_text+" ORDER BY product_inventory_warehouses.location_secondary "+@sort_order+@query_add)
-      elsif @sort_key == 'location_tertiary'
-        products = Product.find_by_sql("SELECT products.* FROM products LEFT JOIN product_inventory_warehouses ON ( "+
-                                         "products.id = product_inventory_warehouses.product_id ) "+@kit_query+
-                                         @status_filter_text+" ORDER BY product_inventory_warehouses.location_tertiary "+@sort_order+@query_add)
-      elsif @sort_key == 'location_name'
-        products = Product.find_by_sql("SELECT products.* FROM products LEFT JOIN product_inventory_warehouses ON ( "+
-                                         "products.id = product_inventory_warehouses.product_id )  LEFT JOIN inventory_warehouses ON("+
-                                         "product_inventory_warehouses.inventory_warehouse_id = inventory_warehouses.id ) "+@kit_query+
-                                         @status_filter_text+" ORDER BY inventory_warehouses.name "+@sort_order+@query_add)
-      elsif @sort_key == 'available_inv'
-        products = Product.find_by_sql("SELECT products.* FROM products LEFT JOIN product_inventory_warehouses ON ( "+
-                                         "products.id = product_inventory_warehouses.product_id ) "+@kit_query+
-                                         @status_filter_text+" ORDER BY product_inventory_warehouses.available_inv "+@sort_order+@query_add)
-      elsif @sort_key == 'cat'
-        products = Product.find_by_sql("SELECT products.* FROM products LEFT JOIN product_cats ON ( "+
-                                         "products.id = product_cats.product_id ) "+@kit_query+
-                                         @status_filter_text+" ORDER BY product_cats.category "+@sort_order+@query_add)
+      # HACK: to bypass for now and enable client development
+      # @sort_key = 'name' if @sort_key == 'sku'
+      if @sort_key.in? expected_sort_keys
+        sorting_with_expected_sort_keys
       else
-        products = Product.order(@sort_key+" "+@sort_order)
-        unless @is_kit == -1
-          products = products.where(:is_kit => @is_kit.to_s)
-        end
-        unless @status_filter == 'all'
-          products = products.where(:status => @status_filter)
-        end
-        unless params[:select_all] || params[:inverted]
-          products = products.limit(limit).offset(@offset)
-        end
+        @products = Product.order(@sort_key + ' ' + @sort_order)
+        default_query
       end
 
-      if products.length == 0
-        products = Product.where(1)
-        unless @is_kit == -1
-          products = products.where(:is_kit => @is_kit.to_s)
-        end
-        unless status_filter == 'all'
-          products = products.where(:status => @status_filter)
-        end
-        unless params[:select_all] || params[:inverted]
-          products = products.limit(limit).offset(@offset)
-        end
-      end
-      products
+      query_if_no_products
+      @products
     end
 
     private
@@ -91,12 +33,17 @@ module ProductsService
       # Get passed in parameter variables if they are valid.
       set_sort_key
       set_sort_order
-      set_status_filter
       set_limit
       set_offset
-      set_sort_key
       set_is_kit
       set_status_filter
+    end
+
+    def expected_sort_keys
+      %w(
+        sku store_type barcode location_primary location_secondary
+        location_tertiary location_name available_inv cat
+      )
     end
 
     def supported_sort_keys
@@ -116,12 +63,12 @@ module ProductsService
 
     def set_limit
       p_limit = params[:limit]
-      @limit = !p_limit.nil? && p_limit.to_i > 0 ? p_limit.to_i : 10
+      @limit = p_limit.to_i > 0 ? p_limit.to_i : 10
     end
 
     def set_offset
       p_offset = params[:offset]
-      @offset = !p_offset.nil? && p_offset.to_i >= 0 ? p_offset.to_i : 0
+      @offset = p_offset.to_i >= 0 ? p_offset.to_i : 0
     end
 
     def set_sort_key
@@ -130,7 +77,7 @@ module ProductsService
     end
 
     def supported_sort_keys_contains(key)
-      key.present? && supported_sort_keys.include?(key.to_s)
+      supported_sort_keys.include?(key.to_s)
     end
 
     def set_sort_order
@@ -139,7 +86,7 @@ module ProductsService
     end
 
     def supported_order_keys_contains(key)
-      key.present? && supported_order_keys.include?(key.to_s)
+      supported_order_keys.include?(key.to_s)
     end
 
     def set_status_filter
@@ -152,7 +99,7 @@ module ProductsService
     end
 
     def supported_status_filters_contains(filter)
-      filter.present? && supported_status_filters.include?(filter.to_s)
+      supported_status_filters.include?(filter.to_s)
     end
 
     def set_is_kit
@@ -161,7 +108,7 @@ module ProductsService
     end
 
     def supported_kit_params_contains(kit_param)
-      kit_param.present? && supported_kit_params.include?(kit_param.to_s)
+      supported_kit_params.include?(kit_param.to_s)
     end
 
     def set_kit_query
@@ -178,6 +125,135 @@ module ProductsService
       return if status_filter == 'all'
       @status_filter_text = is_kit == '-1' ? ' WHERE ' : ' AND '
       @status_filter_text += " products.status='" + status_filter + "'"
+    end
+
+    def sorting_with_expected_sort_keys
+      query = send("sort_by_#{@sort_key}")
+      @products = Product.find_by_sql(query)
+    end
+
+    def sort_by_sku
+      %(\
+        SELECT products.* FROM products LEFT JOIN product_skus ON \
+        (products.id = product_skus.product_id ) #{@kit_query}\
+        #{@status_filter_text}GROUP BY product_id \
+        ORDER BY product_skus.sku #{@sort_order}#{@query_add}
+      )
+    end
+
+    def sort_by_store_type
+      %(\
+        SELECT products.* FROM products \
+        LEFT JOIN stores ON (products.store_id = stores.id ) \
+        #{@kit_query}#{@status_filter_text} \
+        ORDER BY stores.name #{@sort_order}#{@query_add}
+      )
+    end
+
+    def sort_by_barcode
+      %(\
+        SELECT products.* FROM products LEFT JOIN product_barcodes ON \
+        (products.id = product_barcodes.product_id ) \
+        #{@kit_query}#{@status_filter_text} \
+        ORDER BY product_barcodes.barcode #{@sort_order}#{@query_add}
+      )
+    end
+
+    def sort_by_location_primary
+      %(\
+        SELECT products.* FROM products \
+        LEFT JOIN product_inventory_warehouses ON \
+        ( products.id = product_inventory_warehouses.product_id ) \
+        #{@kit_query}#{@status_filter_text} \
+        ORDER BY product_inventory_warehouses.location_primary \
+        #{@sort_order}#{@query_add}
+      )
+    end
+
+    def sort_by_location_secondary
+      %(\
+        SELECT products.* FROM products \
+        LEFT JOIN product_inventory_warehouses ON \
+        ( products.id = product_inventory_warehouses.product_id ) \
+        #{@kit_query}#{@status_filter_text} \
+        ORDER BY product_inventory_warehouses.location_secondary \
+        #{@sort_order}#{@query_add}
+      )
+    end
+
+    def sort_by_location_tertiary
+      %(\
+        SELECT products.* FROM products \
+        LEFT JOIN product_inventory_warehouses ON \
+        ( products.id = product_inventory_warehouses.product_id ) \
+        #{@kit_query}#{@status_filter_text} \
+        ORDER BY product_inventory_warehouses.location_tertiary \
+        #{@sort_order}#{@query_add}
+      )
+    end
+
+    def sort_by_location_name
+      %(\
+        SELECT products.* FROM products \
+        LEFT JOIN product_inventory_warehouses ON \
+        ( products.id = product_inventory_warehouses.product_id )  \
+        LEFT JOIN inventory_warehouses ON \
+        (product_inventory_warehouses.inventory_warehouse_id = \
+        inventory_warehouses.id ) \
+        #{@kit_query}#{@status_filter_text} \
+        ORDER BY inventory_warehouses.name #{@sort_order}#{@query_add}
+      )
+    end
+
+    def sort_by_available_inv
+      %(\
+        SELECT products.* FROM products \
+        LEFT JOIN product_inventory_warehouses ON \
+        ( products.id = product_inventory_warehouses.product_id ) \
+        #{@kit_query}#{@status_filter_text} \
+        ORDER BY product_inventory_warehouses.available_inv \
+        #{@sort_order}#{@query_add}
+      )
+    end
+
+    def sort_by_cat
+      %(\
+        SELECT products.* FROM products \
+        LEFT JOIN product_cats ON \
+        ( products.id = product_cats.product_id ) \
+        #{@kit_query}#{@status_filter_text} \
+        ORDER BY product_cats.category #{@sort_order}#{@query_add}
+      )
+    end
+
+    def query_if_no_products
+      return unless @products.length == 0
+      @products = Product.where(1)
+      default_query
+    end
+
+    def default_query
+      filter_by_kit
+      filter_by_status
+      filter_by_limit_offset
+    end
+
+    def filter_by_kit
+      @products = @products.where(is_kit: @is_kit.to_s) unless @is_kit.eql?(-1)
+    end
+
+    def filter_by_status
+      return if @status_filter.eql?('all')
+      @products = @products.where(status: @status_filter)
+    end
+
+    def filter_by_limit_offset
+      return if select_all_and_inverted?
+      @products = @products.limit(limit).offset(@offset)
+    end
+
+    def select_all_and_inverted?
+      params[:select_all] || params[:inverted]
     end
   end
 end
