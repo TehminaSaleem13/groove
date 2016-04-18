b007246633b78af0d77522eee924008a3ea031ba
Ashish Singh <aitashish173@gmail.com>
2016-04-18 16:14:48 +0530

    Refactored Export setting model GROOV-1046

--- app/models/export_setting.rb
+++ app/models/export_setting.rb
@@ -18,59 +18,22 @@ class ExportSetting < ActiveRecord::Base
   end
 
   def should_export_orders_today
-    day = DateTime.now.strftime("%A")
-    result = false
-    if day=='Sunday' && self.send_export_email_on_sun
-      result = true
-    elsif day=='Monday' && self.send_export_email_on_mon
-      result = true
-    elsif day=='Tuesday' && self.send_export_email_on_tue
-      result = true
-    elsif day=='Wednesday' && self.send_export_email_on_wed
-      result = true
-    elsif day=='Thursday' && self.send_export_email_on_thu
-      result = true
-    elsif day=='Friday' && self.send_export_email_on_fri
-      result = true
-    elsif day=='Saturday' && self.send_export_email_on_sat
-      result = true
-    end
-    result
+    day = DateTime.now.strftime('%a')
+    # Returns True/False
+    send("send_export_email_on_#{day.downcase}")
   end
 
   def should_export_orders(date)
-    day = date.strftime("%A")
-    result = false
-    if day=='Sunday' && self.send_export_email_on_sun
-      result = true
-    elsif day=='Monday' && self.send_export_email_on_mon
-      result = true
-    elsif day=='Tuesday' && self.send_export_email_on_tue
-      result = true
-    elsif day=='Wednesday' && self.send_export_email_on_wed
-      result = true
-    elsif day=='Thursday' && self.send_export_email_on_thu
-      result = true
-    elsif day=='Friday' && self.send_export_email_on_fri
-      result = true
-    elsif day=='Saturday' && self.send_export_email_on_sat
-      result = true
-    end
-    result
+    day = date.strftime('%a')
+    # Returns True/False
+    send("send_export_email_on_#{day.downcase}")
   end
 
   def calculate_row_data(single_row, order_item)
     order = order_item.order
     update_single_row(single_row, order)
-    update_single_row_for_packing_user(order_item, order)
-    single_row[:barcode] = order_item.product.primary_barcode
-    if order_item.product.is_kit == 1
-      single_row[:kit_name] = order_item.product.name
-    else
-      single_row[:product_name] = order_item.product.name
-    end
-    single_row[:primary_sku] = order_item.product.primary_sku
-    single_row[:item_sale_price] = order_item.price
+    update_single_row_for_packing_user(single_row, order_item, order)
+    update_single_row_for_product_info(single_row, order_item)
     single_row
   end
 
@@ -102,7 +65,7 @@ class ExportSetting < ActiveRecord::Base
     ).destroy_all
   end
 
-  def update_single_row(order)
+  def update_single_row(single_row, order)
     single_row[:order_number] = order.increment_id
     single_row[:order_date] = order.order_placed_time
     single_row[:scanned_date] = order.scanned_on
@@ -114,11 +77,25 @@ class ExportSetting < ActiveRecord::Base
     single_row[:customer_name] = order.customer_name
   end
 
-  def update_single_row_for_packing_user(order_item, order)
+  def update_single_row_for_packing_user(single_row, order_item, order)
     packing_user = User.where(id: order.packing_user_id).first
     return if packing_user.blank?
     single_row[:packing_user] = "#{packing_user.name} (#{packing_user.username})"
     single_row[:warehouse_name] = order_item.product.primary_warehouse
                                   .try(:inventory_warehouse).try(:name)
+  end
+
+  def update_single_row_for_product_info(single_row, order_item)
+    product = order_item.product
+    single_row[:barcode] = product.primary_barcode
+
+    if product.is_kit == 1
+      single_row[:kit_name] = product.name
+    else
+      single_row[:product_name] = product.name
+    end
+
+    single_row[:primary_sku] = product.primary_sku
+    single_row[:item_sale_price] = order_item.price
   end
 end
