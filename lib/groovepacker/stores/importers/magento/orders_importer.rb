52980763337fff0ec0e7f7a137a40cc2774f117d
Kapil <kchoudhary006@gmail.com>
2016-05-02 15:12:35 +0530

    Magento no implicit conversion of Symbol into Integer: GROOV-1067

--- lib/groovepacker/stores/importers/magento/orders_importer.rb
+++ lib/groovepacker/stores/importers/magento/orders_importer.rb
@@ -46,115 +46,14 @@ module Groovepacker
                   import_item.current_order_imported_item = -1
                   import_item.to_import = result[:total_imported]
                   import_item.save
-
-                  response.body[:sales_order_list_response][:result][:item].each do |item|
-                    import_item.reload
-                    break if import_item.status == 'cancelled'
-                    import_item.current_increment_id = item[:increment_id]
-                    import_item.current_order_items = -1
-                    import_item.current_order_imported_item = -1
-                    import_item.save
-                    order_info = client.call(:sales_order_info,
-                                             message: {sessionId: session, orderIncrementId: item[:increment_id]})
-
-                    order_info = order_info.body[:sales_order_info_response][:result]
-                    if Order.where(:increment_id => item[:increment_id]).length == 0
-                      @order = Order.new
-                      @order.increment_id = item[:increment_id]
-                      @order.status = 'awaiting'
-                      @order.order_placed_time = item[:created_at]
-                      #@order.storename = item[:store_name]
-                      @order.store = credential.store
-                      line_items = order_info[:items]
-                      if line_items[:item].is_a?(Hash)
-                        import_item.current_order_items = 1
-                        import_item.current_order_imported_item = 0
-                        import_item.save
-                        product_id = nil
-                        
-                        if line_items[:item][:product_type] == 'simple'
-                          @order_item = OrderItem.new
-                          @order_item.price = line_items[:item][:price]
-                          @order_item.qty = line_items[:item][:qty_ordered]
-                          @order_item.row_total= line_items[:item][:row_total]
-                          @order_item.name = line_items[:item][:name] || "Un-named Magento Product"
-                          @order_item.sku = line_items[:item][:sku]
-                          if ProductSku.where(:sku => @order_item.sku).length == 0
-                            #import other product details
-                            product_id = Groovepacker::Stores::Importers::Magento::
-                                ProductsImporter.new(handler).import_single({ product_id: line_items[:item][:product_id] })
-                          else
-                            product_id = ProductSku.where(:sku => @order_item.sku).first.product_id
-                          end
-                          @order_item.product_id = product_id
-                          @order.order_items << @order_item
-                        else
-                          if ProductSku.where(:sku => line_items[:item][:sku]).length == 0
-                            Groovepacker::Stores::Importers::Magento::
-                                ProductsImporter.new(handler).import_single({ product_id: line_items[:item][:product_id] })
-                          end
-                        end
-                        
-                        import_item.current_order_imported_item = 1
-                        import_item.save
-                      else
-                        import_item.current_order_items = line_items[:item].length
-                        import_item.current_order_imported_item = 0
-                        import_item.save
-                        line_items[:item].each do |line_item|
-                          if line_item[:product_type] == 'simple'
-                            @order_item = OrderItem.new
-                            @order_item.price = line_item[:price]
-                            @order_item.qty = line_item[:qty_ordered]
-                            @order_item.row_total= line_item[:row_total]
-                            @order_item.name = line_item[:name] || "Un-named Magento Product"
-                            @order_item.sku = line_item[:sku]
-                            if ProductSku.where(:sku => @order_item.sku).length == 0
-                              product_id = Groovepacker::Stores::Importers::Magento::
-                                  ProductsImporter.new(handler).import_single({ product_id: line_item[:product_id] })
-                            else
-                              product_id = ProductSku.where(:sku => @order_item.sku).first.product_id
-                            end
-                            @order_item.product_id = product_id
-                            @order.order_items << @order_item
-                          else
-                            if ProductSku.where(:sku => line_item[:sku]).length == 0
-                              Groovepacker::Stores::Importers::Magento::
-                                  ProductsImporter.new(handler).import_single({ product_id: line_item[:product_id] })
-                            end
-                          end
-                          import_item.current_order_imported_item = import_item.current_order_imported_item + 1
-                          import_item.save
-                        end
-                      end
-
-                      #if product does not exist import product using product.info
-                      @order.address_1 = order_info[:shipping_address][:street]
-                      @order.city = order_info[:shipping_address][:city]
-                      @order.country = order_info[:shipping_address][:country_id]
-                      @order.postcode = order_info[:shipping_address][:postcode]
-                      @order.email = item[:customer_email]
-                      @order.lastname = order_info[:shipping_address][:lastname]
-                      @order.firstname = order_info[:shipping_address][:firstname]
-                      @order.state = order_info[:shipping_address][:region]
-                      if @order.save
-                        if !@order.addnewitems
-                          result[:status] &= false
-                          result[:messages].push('Problem adding new items')
-                        end
-                        @order.addactivity("Order Import", credential.store.name+" Import")
-                        @order.order_items.each do |item|
-                          @order.addactivity("Item with SKU: "+item.sku+" Added", credential.store.name+" Import")
-                        end
-                        @order.set_order_status
-                        result[:success_imported] = result[:success_imported] + 1
-                        import_item.success_imported = result[:success_imported]
-                        import_item.save
-                      end
-                    else
-                      result[:previous_imported] = result[:previous_imported] + 1
-                      import_item.previous_imported = result[:previous_imported]
-                      import_item.save
+                  if response.body[:sales_order_list_response][:result][:item].class.to_s.include?("Hash")
+                    item = response.body[:sales_order_list_response][:result][:item]
+                    import_single_order(item, import_item, client, response, credential, session, result)
+                  else
+                    response.body[:sales_order_list_response][:result][:item].each do |item|
+                      import_item.reload
+                      break if import_item.status == 'cancelled'
+                      import_single_order(item, import_item, client, response, credential, session, result)
                     end
                   end
                 end
@@ -180,6 +79,116 @@ module Groovepacker
           def import_single(hash)
             {}
           end
+
+          def import_single_order(item, import_item, client, response, credential, session, result)
+            import_item.current_increment_id = item[:increment_id]
+            import_item.current_order_items = -1
+            import_item.current_order_imported_item = -1
+            import_item.save
+            order_info = client.call(:sales_order_info,
+                                     message: {sessionId: session, orderIncrementId: item[:increment_id]})
+
+            order_info = order_info.body[:sales_order_info_response][:result]
+            if Order.where(:increment_id => item[:increment_id]).length == 0
+              @order = Order.new
+              @order.increment_id = item[:increment_id]
+              @order.status = 'awaiting'
+              @order.order_placed_time = item[:created_at]
+              #@order.storename = item[:store_name]
+              @order.store = credential.store
+              line_items = order_info[:items]
+              if line_items[:item].is_a?(Hash)
+                import_item.current_order_items = 1
+                import_item.current_order_imported_item = 0
+                import_item.save
+                product_id = nil
+                
+                if line_items[:item][:product_type] == 'simple'
+                  @order_item = OrderItem.new
+                  @order_item.price = line_items[:item][:price]
+                  @order_item.qty = line_items[:item][:qty_ordered]
+                  @order_item.row_total= line_items[:item][:row_total]
+                  @order_item.name = line_items[:item][:name] || "Un-named Magento Product"
+                  @order_item.sku = line_items[:item][:sku]
+                  if ProductSku.where(:sku => @order_item.sku).length == 0
+                    #import other product details
+                    product_id = Groovepacker::Stores::Importers::Magento::
+                        ProductsImporter.new(handler).import_single({ product_id: line_items[:item][:product_id] })
+                  else
+                    product_id = ProductSku.where(:sku => @order_item.sku).first.product_id
+                  end
+                  @order_item.product_id = product_id
+                  @order.order_items << @order_item
+                else
+                  if ProductSku.where(:sku => line_items[:item][:sku]).length == 0
+                    Groovepacker::Stores::Importers::Magento::
+                        ProductsImporter.new(handler).import_single({ product_id: line_items[:item][:product_id] })
+                  end
+                end
+                
+                import_item.current_order_imported_item = 1
+                import_item.save
+              else
+                import_item.current_order_items = line_items[:item].length
+                import_item.current_order_imported_item = 0
+                import_item.save
+                line_items[:item].each do |line_item|
+                  if line_item[:product_type] == 'simple'
+                    @order_item = OrderItem.new
+                    @order_item.price = line_item[:price]
+                    @order_item.qty = line_item[:qty_ordered]
+                    @order_item.row_total= line_item[:row_total]
+                    @order_item.name = line_item[:name] || "Un-named Magento Product"
+                    @order_item.sku = line_item[:sku]
+                    if ProductSku.where(:sku => @order_item.sku).length == 0
+                      product_id = Groovepacker::Stores::Importers::Magento::
+                          ProductsImporter.new(handler).import_single({ product_id: line_item[:product_id] })
+                    else
+                      product_id = ProductSku.where(:sku => @order_item.sku).first.product_id
+                    end
+                    @order_item.product_id = product_id
+                    @order.order_items << @order_item
+                  else
+                    if ProductSku.where(:sku => line_item[:sku]).length == 0
+                      Groovepacker::Stores::Importers::Magento::
+                          ProductsImporter.new(handler).import_single({ product_id: line_item[:product_id] })
+                    end
+                  end
+                  import_item.current_order_imported_item = import_item.current_order_imported_item + 1
+                  import_item.save
+                end
+              end
+
+              #if product does not exist import product using product.info
+              @order.address_1 = order_info[:shipping_address][:street]
+              @order.city = order_info[:shipping_address][:city]
+              @order.country = order_info[:shipping_address][:country_id]
+              @order.postcode = order_info[:shipping_address][:postcode]
+              @order.email = item[:customer_email]
+              @order.lastname = order_info[:shipping_address][:lastname]
+              @order.firstname = order_info[:shipping_address][:firstname]
+              @order.state = order_info[:shipping_address][:region]
+              if @order.save
+                if !@order.addnewitems
+                  result[:status] &= false
+                  result[:messages].push('Problem adding new items')
+                end
+                @order.addactivity("Order Import", credential.store.name+" Import")
+                @order.order_items.each do |item|
+                  @order.addactivity("Item with SKU: "+item.sku+" Added", credential.store.name+" Import")
+                end
+                @order.set_order_status
+                result[:success_imported] = result[:success_imported] + 1
+                import_item.success_imported = result[:success_imported]
+                import_item.save
+              end
+            else
+              result[:previous_imported] = result[:previous_imported] + 1
+              import_item.previous_imported = result[:previous_imported]
+              import_item.save
+            end
+          end
+
         end
       end
     end
