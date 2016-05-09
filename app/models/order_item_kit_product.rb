ad88c9531761df270236d8815da9de178756e954
Kalakar <ksahoo@navaratan.com>
2016-05-09 13:25:23 +0530

    test cases for changes in order_item_kit_products.rb

--- app/models/order_item_kit_product.rb
+++ app/models/order_item_kit_product.rb
@@ -14,45 +14,14 @@ class OrderItemKitProduct < ActiveRecord::Base
   PARTIALLY_SCANNED_STATUS = 'partially_scanned'
 
   def process_item(clicked, username, typein_count=1)
-    # order_item_unscanned = false
-    # order_unscanned = false
     total_qty = calculate_total_item
-    # if self.order_item.product.kit_parsing == 'depends'
-    #   total_qty = self.order_item.kit_split_qty
-    # else
-    #   total_qty = self.order_item.qty
-    # end
     total_product_kit_skus = total_qty * self.product_kit_skus.qty
     if self.scanned_qty < total_product_kit_skus
       self.scanned_qty += typein_count
       set_clicked_qty(clicked, username, typein_count)
-      # if clicked
-      #   self.clicked_qty = self.clicked_qty + typein_count
-      #   self.order_item.order.addactivity("Item with SKU: " +
-      #                                       self.product_kit_skus.option_product.primary_sku + " has been click scanned", username)
-      # end
       set_scanned_status(total_product_kit_skus)
-      # if self.scanned_qty == total_product_kit_skus
-      #   self.scanned_status = SCANNED_STATUS
-      # else
-      #   self.scanned_status = PARTIALLY_SCANNED_STATUS
-      # end
       self.save
       calculate_scan_time(typein_count, username)
-      # scan_time = self.order_item_kit_product_scan_times.create(
-      #   scan_start: self.order_item.order.last_suggested_at,
-      #   scan_end: DateTime.now)
-      # if typein_count > 0
-      #   avg_time = avg_time_per_item(username)
-      #   if avg_time
-      #     self.order_item.order.total_scan_time += (avg_time * typein_count).to_i
-      #   else
-      #     self.order_item.order.total_scan_time += (scan_time.scan_end - scan_time.scan_start).to_i * typein_count
-      #   end
-      # else
-      #   self.order_item.order.total_scan_time = self.order_item.order.total_scan_time +
-      #     (scan_time.scan_end - scan_time.scan_start).to_i
-      # end
       self.order_item.order.total_scan_count += typein_count
       self.order_item.order.save
 
@@ -60,56 +29,9 @@ class OrderItemKitProduct < ActiveRecord::Base
       # for this calculate minimum of order items
       #update order item status
       min = set_min_value
-      # product_kit_skus_qty = self.order_item.order_item_kit_products.first.product_kit_skus.qty
-      # if product_kit_skus_qty != 0
-      #   min = self.order_item.order_item_kit_products.first.scanned_qty /
-      #     product_kit_skus_qty
-      # else
-      #   min = 0
-      # end
-      # if self.order_item.order_item_kit_products.first.product_kit_skus.qty != 0
-      #   min = self.order_item.order_item_kit_products.first.scanned_qty /
-      #     self.order_item.order_item_kit_products.first.product_kit_skus.qty
-      # else
-      #   min = 0
-      # end
-
-      # self.order_item.order_item_kit_products.each do |kit_product|
-      #   temp = kit_product.scanned_qty / kit_product.product_kit_skus.qty
-      #   min = temp if (temp) < min
-      # end
-
       set_order_item_scanned_qty(min)
-
-      # if self.order_item.product.kit_parsing == 'depends'
-      #   self.order_item.kit_split_scanned_qty = min
-      #   self.order_item.scanned_qty = self.order_item.single_scanned_qty +
-      #     self.order_item.kit_split_scanned_qty
-      # else
-      #   self.order_item.scanned_qty = min
-      # end
-
       set_order_item_scanned_statys
-
-      # if self.order_item.scanned_qty != self.order_item.qty
-      #   self.order_item.scanned_status = PARTIALLY_SCANNED_STATUS
-      # else
-      #   self.order_item.scanned_status = SCANNED_STATUS
-      # end
       self.order_item.save
-
-      #update order status
-      # self.order_item.order.order_items.each do |order_item|
-      #   if order_item.scanned_status != SCANNED_STATUS
-      #     order_unscanned = true
-      #   end
-      # end
-      # if order_unscanned
-      #   self.order_item.order.status = 'awaiting'
-      # else
-      #       self.order_item.order.set_order_to_scanned_state
-      # end
-      # self.order_item.order.save
     end
   end
 
@@ -169,6 +91,7 @@ class OrderItemKitProduct < ActiveRecord::Base
     min = 0
     min = self.order_item.order_item_kit_products.first.scanned_qty / product_kit_skus_qty if product_kit_skus_qty != 0
     self.order_item.order_item_kit_products.each do |kit_product|
+      kit_product.reload
       temp = kit_product.scanned_qty / kit_product.product_kit_skus.qty
       min = temp if (temp) < min
     end
