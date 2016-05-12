fb0c751d630095bae3ffadf2848586a07b95a173
Kalakar <ksahoo@navaratan.com>
2016-05-12 20:37:01 +0530

    code refactoring for files in dashboard/stats folder

--- lib/groovepacker/dashboard/stats/leader_board_stats.rb
+++ lib/groovepacker/dashboard/stats/leader_board_stats.rb
@@ -1,85 +1,85 @@
-module Groovepacker
-  module Dashboard
-    module Stats
-      class LeaderBoardStats
-        def initialize
-        end
+# module Groovepacker
+#   module Dashboard
+#     module Stats
+#       class LeaderBoardStats
+#         def initialize
+#         end
 
-        def compute_leader_board
-          1..12.times do |index|
-            compute_leader_board_for_order_item_count(index+1)
-          end
-        end
+#         def compute_leader_board
+#           1..12.times do |index|
+#             compute_leader_board_for_order_item_count(index+1)
+#           end
+#         end
 
-        def compute_leader_board_for_order_item_count(order_item_count)
-          begin
-            order = Order.find_by_sql("SELECT * from orders where timediff(scanned_on, scan_start_time) = (SELECT MIN(timediff(scanned_on, scan_start_time)) " +
-                                        "FROM orders where orders.id IN(SELECT orders.id from orders INNER JOIN " +
-                                        "order_items ON orders.id = order_items.order_id AND " +
-                                        "orders.scan_start_time != 'NULL' GROUP BY order_items.order_id " +
-                                        "HAVING COUNT(*) = '"+ (order_item_count).to_s+"'))")
-            leader_board = LeaderBoard.where(order_item_count: order_item_count)
-            if leader_board.empty?
-              if order.first.nil?
-                LeaderBoard.create(order_item_count: (order_item_count),
-                                   order_id: nil, scan_time: nil)
-              else
-                order = order.first
-                LeaderBoard.create(order_item_count: (order_item_count),
-                                   order_id: order.id, scan_time: (order.scanned_on - order.scan_start_time))
-              end
-            else
-              leader_board = leader_board.first
-              if !order.first.nil?
-                order= order.first
-                if leader_board.scan_time.nil? || (leader_board.scan_time > (order.scanned_on - order.scan_start_time))
-                  leader_board.order_id = order.id
-                  leader_board.scan_time = order.scanned_on - order.scan_start_time
-                  leader_board.order_item_count = order_item_count
-                  leader_board.save
-                end
-              else
-                leader_board.order_id = nil
-                leader_board.scan_time = nil
-                leader_board.save
-              end
-            end
-          rescue Exception => e
-            puts e.message
-            puts e.backtrace.join("\n")
-          end
-        end
+#         def compute_leader_board_for_order_item_count(order_item_count)
+#           begin
+#             order = Order.find_by_sql("SELECT * from orders where timediff(scanned_on, scan_start_time) = (SELECT MIN(timediff(scanned_on, scan_start_time)) " +
+#                                         "FROM orders where orders.id IN(SELECT orders.id from orders INNER JOIN " +
+#                                         "order_items ON orders.id = order_items.order_id AND " +
+#                                         "orders.scan_start_time != 'NULL' GROUP BY order_items.order_id " +
+#                                         "HAVING COUNT(*) = '"+ (order_item_count).to_s+"'))")
+#             leader_board = LeaderBoard.where(order_item_count: order_item_count)
+#             if leader_board.empty?
+#               if order.first.nil?
+#                 LeaderBoard.create(order_item_count: (order_item_count),
+#                                    order_id: nil, scan_time: nil)
+#               else
+#                 order = order.first
+#                 LeaderBoard.create(order_item_count: (order_item_count),
+#                                    order_id: order.id, scan_time: (order.scanned_on - order.scan_start_time))
+#               end
+#             else
+#               leader_board = leader_board.first
+#               if !order.first.nil?
+#                 order= order.first
+#                 if leader_board.scan_time.nil? || (leader_board.scan_time > (order.scanned_on - order.scan_start_time))
+#                   leader_board.order_id = order.id
+#                   leader_board.scan_time = order.scanned_on - order.scan_start_time
+#                   leader_board.order_item_count = order_item_count
+#                   leader_board.save
+#                 end
+#               else
+#                 leader_board.order_id = nil
+#                 leader_board.scan_time = nil
+#                 leader_board.save
+#               end
+#             end
+#           rescue Exception => e
+#             puts e.message
+#             puts e.backtrace.join("\n")
+#           end
+#         end
 
-        def list
-          results = []
-          LeaderBoard.all.each do |row|
-            leader_stat = {}
-            leader_stat[:order_items_count] = row.order_item_count
-            if !row.order.nil?
-              leader_stat[:user_name] = User.find(row.order.packing_user_id).name
-              leader_stat[:record_date] = row.order.scanned_on
-              leader_stat[:increment_id] = row.order.increment_id
-              leader_stat[:packing_time] = format_packing_time(row.scan_time)
-            else
-              leader_stat[:user_name] = "-"
-              leader_stat[:record_date] = "-"
-              leader_stat[:increment_id] = "-"
-              leader_stat[:packing_time] = "-"
-            end
-            results << leader_stat
-          end
+#         def list
+#           results = []
+#           LeaderBoard.all.each do |row|
+#             leader_stat = {}
+#             leader_stat[:order_items_count] = row.order_item_count
+#             if !row.order.nil?
+#               leader_stat[:user_name] = User.find(row.order.packing_user_id).name
+#               leader_stat[:record_date] = row.order.scanned_on
+#               leader_stat[:increment_id] = row.order.increment_id
+#               leader_stat[:packing_time] = format_packing_time(row.scan_time)
+#             else
+#               leader_stat[:user_name] = "-"
+#               leader_stat[:record_date] = "-"
+#               leader_stat[:increment_id] = "-"
+#               leader_stat[:packing_time] = "-"
+#             end
+#             results << leader_stat
+#           end
 
-          results
-        end
+#           results
+#         end
 
-        private
+#         private
 
-        def format_packing_time(scan_time)
-          min = scan_time / 60
-          sec = scan_time % 60
-          [min.to_s.rjust(2, '0'), sec.to_s.rjust(2, '0')].join(":")
-        end
-      end
-    end
-  end
-end
+#         def format_packing_time(scan_time)
+#           min = scan_time / 60
+#           sec = scan_time % 60
+#           [min.to_s.rjust(2, '0'), sec.to_s.rjust(2, '0')].join(":")
+#         end
+#       end
+#     end
+#   end
+# end
--- lib/groovepacker/dashboard/stats/shipment_stats.rb
+++ lib/groovepacker/dashboard/stats/shipment_stats.rb
@@ -5,66 +5,58 @@ module Groovepacker
 
         def get_shipment_stats(name, avg_data=false)
           shipping_result = {}
-          shipping_result['shipped_current'] = 0
-          shipping_result['shipped_last'] = 0
-          shipping_result['max_allowed'] = 0
-          tenant = Tenant.where(name: name).first unless Tenant.where(name: name).empty?
+          @tenant = Tenant.where(name: name).first
           current_tenant = Apartment::Tenant.current_tenant
-          unless tenant.nil?
+          if @tenant
             begin
-              @access_restrictions = nil
-              Apartment::Tenant.switch(tenant.name)
-              # shipping_result['shipped_last6'] = []
+              Apartment::Tenant.switch(@tenant.name)
               @access_restrictions = AccessRestriction.order("created_at")
               shipping_result['shipped_last6'] = []
               unless @access_restrictions.empty?
-                data_length = @access_restrictions.length
-                shipping_result['shipped_current'] = @access_restrictions[data_length - 1].total_scanned_shipments
-                shipping_result['shipped_last'] = @access_restrictions[data_length - 2].total_scanned_shipments if data_length > 1
-                shipping_result['max_allowed'] = @access_restrictions[data_length - 1].num_shipments
-                shipping_result['max_users'] = @access_restrictions[data_length-1].num_users
-                shipping_result['max_import_sources'] = @access_restrictions[data_length-1].num_import_sources
-                shipping_result['allow_bc_inv_push'] = @access_restrictions[data_length-1].allow_bc_inv_push
-                shipping_result['allow_mg_rest_inv_push'] = @access_restrictions[data_length-1].allow_mg_rest_inv_push
-                shipping_result['allow_shopify_inv_push'] = @access_restrictions[data_length-1].allow_shopify_inv_push
-                shipping_result['allow_teapplix_inv_push'] = @access_restrictions[data_length-1].allow_teapplix_inv_push
+                get_shipping_result(shipping_result)
+                # data_length = @access_restrictions.length
+                # latest_access_data = @access_restrictions[data_length - 1]
+
+                # accepted_data.keys.each do |key|
+                #   if key == 'shipped_last'
+                #     shipping_result[key] = @access_restrictions[data_length - 2].accepted_data[key]
+                #   else
+                #     shipping_result[key] = latest_access_data.accepted_data[key]
+                #   end
+                # end
+                # shipping_result['shipped_current'] = latest_access_data.total_scanned_shipments
+                # shipping_result['shipped_last'] = @access_restrictions[data_length - 2].total_scanned_shipments if data_length > 1
+                # shipping_result['max_allowed'] = latest_access_data.num_shipments
+                # shipping_result['max_users'] = latest_access_data.num_users
+                # shipping_result['max_import_sources'] = latest_access_data.num_import_sources
+                # shipping_result['allow_bc_inv_push'] = latest_access_data.allow_bc_inv_push
+                # shipping_result['allow_mg_rest_inv_push'] = latest_access_data.allow_mg_rest_inv_push
+                # shipping_result['allow_shopify_inv_push'] = latest_access_data.allow_shopify_inv_push
+                # shipping_result['allow_teapplix_inv_push'] = latest_access_data.allow_teapplix_inv_push
                 if avg_data
                   shipping_result['average_shipped'] = get_avg_shipped('current')
                   shipping_result['average_shipped_last'] = get_avg_shipped('last')
                 end
-                for i in 2..7 do
-                  if data_length-i >= 0
-                    shipped = {}
-                    shipped['shipping_duration'] = (@access_restrictions[data_length-i].created_at - 1.month).strftime("%d %b") + " - " + @access_restrictions[data_length-i].created_at.strftime("%d %b")
-                    shipped['shipped_count'] = @access_restrictions[data_length-i].total_scanned_shipments.to_s
-                    shipping_result['shipped_last6'] << shipped
-                  else
-                    shipping_result['shipped_last6'] << {'shipping_duration' => '-', 'shipped_count' => '-'}
-                  end
-                end
+                get_old_shipped_count(shipping_result)
+                # for i in 2..7 do
+                #   if data_length-i >= 0
+                #     shipped = {}
+                #     access_data = @access_restrictions[data_length-i]
+                #     shipped['shipping_duration'] = (access_data.created_at - 1.month).strftime("%d %b") + " - " + access_data.created_at.strftime("%d %b")
+                #     shipped['shipped_count'] = access_data.total_scanned_shipments.to_s
+                #     shipping_result['shipped_last6'] << shipped
+                #   else
+                #     shipping_result['shipped_last6'] << {'shipping_duration' => '-', 'shipped_count' => '-'}
+                #   end
+                # end
               else
-                shipping_result['shipped_current'] = 0
-                shipping_result['shipped_last'] = 0
-                shipping_result['max_allowed'] = 0
-                shipping_result['max_users'] = 0
-                shipping_result['max_import_sources'] = 0
-                shipping_result['average_shipped'] = 0
-                shipping_result['average_shipped_last'] = 0
-                shipping_result['shipped_last6'] = [{'shipping_duration' => '-', 'shipped_count' => '-'}]
+                raise
               end
             rescue
-              shipping_result['shipped_current'] = 0
-              shipping_result['shipped_last'] = 0
-              shipping_result['max_allowed'] = 0
-              shipping_result['max_users'] = 0
-              shipping_result['max_import_sources'] = 0
-              shipping_result['average_shipped'] = 0
-              shipping_result['average_shipped_last'] = 0
-              shipping_result['shipped_last6'] = [{'shipping_duration' => '-', 'shipped_count' => '-'}]
+              shipping_result = default_result
             end
           end
           Apartment::Tenant.switch(current_tenant)
-          puts "shipping_result['shipped_last6']: " + shipping_result['shipped_last6'].inspect
           shipping_result
         end
 
@@ -89,6 +81,72 @@ module Groovepacker
             return @access_restrictions.last.total_scanned_shipments
           elsif @access_restrictions.length > 1 && month == "last"
             return @access_restrictions[@access_restrictions.length - 2].total_scanned_shipments
+          end
+        end
+
+        private
+
+        def default_result
+          {
+            'shipped_current' => 0,
+            'shipped_last' => 0,
+            'max_allowed' => 0,
+            'max_users' => 0,
+            'max_import_sources' => 0,
+            'average_shipped' => 0,
+            'average_shipped_last' => 0,
+            'shipped_last6' => [
+              {
+                'shipping_duration' => '-',
+                'shipped_count' => '-'
+              }
+            ]
+          }
+        end
+
+        def accepted_data
+          {
+            'shipped_current' => 'total_scanned_shipments',
+            'shipped_last' => 'total_scanned_shipments',
+            'max_allowed' => 'num_shipments',
+            'max_users' => 'num_users',
+            'max_import_sources' => 'num_import_sources',
+            'allow_bc_inv_push' => 'allow_bc_inv_push',
+            'allow_mg_rest_inv_push' => 'allow_mg_rest_inv_push',
+            'allow_shopify_inv_push' => 'allow_shopify_inv_push',
+            'allow_teapplix_inv_push' => 'allow_teapplix_inv_push'
+          }
+        end
+
+        def get_shipping_result(shipping_result)
+          data_length = @access_restrictions.length
+          latest_access_data = @access_restrictions[data_length - 1]
+
+          accepted_data.keys.each do |key|
+            if key == 'shipped_last'
+              shipping_result[key] = @access_restrictions[data_length - 2].accepted_data[key]
+            else
+              shipping_result[key] = latest_access_data.accepted_data[key]
+            end
+          end
+        end
+
+        def get_old_shipped_count(shipping_result)
+          data_length = @access_restrictions.length
+          (2..7).each do |i|
+            if data_length - i >= 0
+              shipped = {}
+              access_data = @access_restrictions[data_length - i]
+              shipped['shipping_duration'] =
+                (access_data.created_at - 1.month).strftime("%d %b") +
+                " - " + access_data.created_at.strftime("%d %b")
+              shipped['shipped_count'] = access_data.total_scanned_shipments.to_s
+              shipping_result['shipped_last6'] << shipped
+            else
+              shipping_result['shipped_last6'] << { 'shipping_duration' => '-',
+                                                    'shipped_count' => '-'
+                                                  }
+            end
           end
         end
       end
