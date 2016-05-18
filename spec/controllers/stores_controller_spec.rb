391368494758423a97c6e3ed2a58c44afb5a2d4d
Kapil <kchoudhary006@gmail.com>
2016-05-18 14:04:41 +0530

    Added test cases for create_update_ftp_credentials and connect_and_retrieve

--- spec/controllers/stores_controller_spec.rb
+++ spec/controllers/stores_controller_spec.rb
@@ -79,7 +79,7 @@ describe StoresController do
     it "updates an ShippingEasy store" do
       @inv_wh = FactoryGirl.create(:inventory_warehouse, :name=>'shipping_easy_inventory_warehouse')
       @store = FactoryGirl.create(:store, :name=>'shipping_easy_store', :inventory_warehouse=>@inv_wh, :status => false)
-      @big_commerce_credential = FactoryGirl.create(:shipping_easy_credential, :store_id=>@store.id, :api_key=>'xxxxxxxxxxxxxxxx', :api_secret=>'yyyyyyyyyyyyyyyyyyyyyyyyyyy', :import_ready_for_shipment => true, :import_shipped => true, :gen_barcode_from_sku => true)
+      @shipping_easy_credential = FactoryGirl.create(:shipping_easy_credential, :store_id=>@store.id, :api_key=>'xxxxxxxxxxxxxxxx', :api_secret=>'yyyyyyyyyyyyyyyyyyyyyyyyyyy', :import_ready_for_shipment => true, :import_shipped => true, :gen_barcode_from_sku => true)
       request.accept = "application/json"
       @user.role.update_attribute(:add_edit_stores, true)
       post :create_update_store, { :store_type => 'ShippingEasy', :id => @store.id, :status => true }
@@ -93,6 +93,24 @@ describe StoresController do
       expect(store.shipping_easy_credential).to be_present
     end
   end
+
+  describe "Get 'stores'" do
+    it "It fetches all stores excluding system stores" do
+      @inv_wh = FactoryGirl.create(:inventory_warehouse, :name=>'shipping_easy_inventory_warehouse')
+      @user.role.update_attribute(:add_edit_stores, true)
+      @store = FactoryGirl.create(:store, :store_type => 'ShippingEasy', :name=>'Shipping Easy Test Store', :inventory_warehouse=>@inv_wh, :status => false)
+      FactoryGirl.create(:shipping_easy_credential, :store_id=>@store.id, :api_key=>'xxxxxxxxxxxxxxxx', :api_secret=>'yyyyyyyyyyyyyyyyyyyyyyyyyyy', :import_ready_for_shipment => true, :import_shipped => true, :gen_barcode_from_sku => true)
+      @store1 = FactoryGirl.create(:store, :store_type => 'Teapplix', :name=>'Teapplix Test Store', :inventory_warehouse=>@inv_wh, :status => true)
+      FactoryGirl.create(:teapplix_credential, :store_id=>@store1.id, :account_name=>'xxxxxxxx', :username=>'yyyyyyyyyyy', :password => 'password', :import_shipped => true, :import_open_orders => false)
+      request.accept = "application/json"
+      get :index, {}
+      expect(response.status).to eq(200)
+      result = JSON.parse(response.body)
+      expect(result.length).to eq(2)
+    end
+  end
+
+
   
   # describe "GET 'getactivestores'" do
   #   it "Should not return any active stores" do
@@ -141,14 +159,38 @@ describe StoresController do
     it "Should create or update ftp credentials for CSV type store" do
       @inv_wh = FactoryGirl.create(:inventory_warehouse, :name=>'csv_store_inv_warehouse')
       @store = FactoryGirl.create(:store, :name=>'csv_store', :store_type => 'CSV', :inventory_warehouse=>@inv_wh)
+      FactoryGirl.create(:ftp_credential, :username => "TEST", :password => "TEST", :store_id => @store.id)
       request.accept = "application/json"
-      post :create_update_ftp_credentials, {:id => @store.id, :username => "TEST", :password => "TEST"}
+      post :create_update_ftp_credentials, {:id => @store.id, :username => "TEST1", :password => "TEST1"}
       expect(response.status).to eq(200)
       result = JSON.parse(response.body)
       expect(result['status']).to eq(true)
       @ftp_credential = @store.ftp_credential
-      expect(@ftp_credential.password).to eq("TEST")
-      expect(@ftp_credential.username).to eq("TEST")
+      expect(@ftp_credential.password).to eq("TEST1")
+      expect(@ftp_credential.username).to eq("TEST1")
+    end
+
+    it "Should connect and retrieve using ftp credentials of a store" do
+      @inv_wh = FactoryGirl.create(:inventory_warehouse, :name=>'csv_store_inv_warehouse')
+      @store = FactoryGirl.create(:store, :name=>'csv_store', :store_type => 'CSV', :inventory_warehouse=>@inv_wh)
+      FactoryGirl.create(:ftp_credential, :host => "ftp.groovepacker.com/orders", :username => "test@groovepacker.com", :password => "1gpacker", :store_id => @store.id)
+      request.accept = "application/json"
+      get :connect_and_retrieve, {:id => @store.id}
+      expect(response.status).to eq(200)
+      result = JSON.parse(response.body)
+      expect(result['connection']['status']).to eq(false)
+      expect(result['connection']['error_messages']).to_not be_blank
+      expect(result['connection']['success_messages']).to be_blank
+
+      @store1 = FactoryGirl.create(:store, :name=>'csv_store1', :store_type => 'CSV', :inventory_warehouse=>@inv_wh)
+      FactoryGirl.create(:ftp_credential, :host => "ftp.groovepacker.com/orders", :username => "test@groovepacker.com", :password => "1gpacker!", :store_id => @store1.id)
+      request.accept = "application/json"
+      get :connect_and_retrieve, {:id => @store1.id}
+      expect(response.status).to eq(200)
+      result = JSON.parse(response.body)
+      expect(result['connection']['status']).to eq(true)
+      expect(result['connection']['error_messages']).to be_blank
+      expect(result['connection']['success_messages']).to_not be_blank
     end
   end
 end
