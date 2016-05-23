4640dcaac0c2112879e7a17c250bf5dab0a4bfcd
Kapil <kchoudhary006@gmail.com>
2016-05-23 18:14:06 +0530

    on demand import: GROOV1138

--- lib/groovepacker/stores/importers/orders_importer.rb
+++ lib/groovepacker/stores/importers/orders_importer.rb
@@ -15,9 +15,10 @@ module Groovepacker
           order_no = hash[:order_no]
           stores = Store.where("status=? and store_type NOT IN (?)", true, ['CSV', 'system'])
           s_stores = stores.where("store_type='Shipstation API 2'")
+          o_stores = stores.where("store_type='BigCommerce'")
           #o_stores = stores.where("store_type!='Shipstation API 2'")
-          #[s_stores, o_stores].each { |st| run_for_each_store(st, order_no) }
-          run_for_each_store(s_stores, order_no)
+          [s_stores, o_stores].each { |st| run_for_each_store(st, order_no) }
+          #run_for_each_store(s_stores, order_no)
         end
 
         private
