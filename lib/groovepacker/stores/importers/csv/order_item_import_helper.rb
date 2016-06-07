ff85669a521a6d51e45db2bc750053ee60c9bf09
Kalakar <ksahoo@navaratan.com>
2016-06-07 14:42:43 +0530

    mass insert order items

--- lib/groovepacker/stores/importers/csv/order_item_import_helper.rb
+++ lib/groovepacker/stores/importers/csv/order_item_import_helper.rb
@@ -10,30 +10,33 @@ module Groovepacker
           end
 
           def create_new_order_item(single_row, product, single_sku, order)
-            order_item = order.order_items.build
-            #order_item = OrderItem.new
+            # order_item = order.order_items.build
+            order_item = OrderItem.new
             order_item.product = product
-            #order_item.order = order
+            order_item.order = order
             order_item.sku = single_sku.strip
             order_item.qty = 0
             order_item.price = 0.0
-            initialize_or_update_order_item(single_row, order_item)
+            order_item = initialize_or_update_order_item(single_row, order_item)
             order_item
           end
 
-          def create_update_order_item(single_row, product, single_sku, order)
+          def create_update_order_item(single_row, product, single_sku, order, created_order_items)
             order_items = OrderItem.where(
               product_id: product.id,
               order_id: order.id)
+            order_items = created_order_items.select { |e| e.order_id == order.id && e.product_id == product.id && e.sku == single_sku.strip } if !created_order_items.empty? && order_items.empty?
             if order_items.empty?
               order_item = create_new_order_item(single_row, product, single_sku, order)
             else
-              order_item = order_items.first
-              order_item = initialize_or_update_order_item(single_row, order_item)
+              item = order_items.first
+              order_item = initialize_or_update_order_item(single_row, item)
+              created_order_items.delete(item)
             end
             @product_helper.import_image(product, single_row, true)
             
-            order_item
+            created_order_items << order_item
+            return created_order_items
           end
 
           def initialize_or_update_order_item(single_row, order_item)
@@ -48,7 +51,7 @@ module Groovepacker
                   @helper.get_row_data(single_row, 'item_sale_price')
               end
             end
-            order_item.save
+            # order_item.save
             return order_item
           end
         end
