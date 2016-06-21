d1af0eb22591f918c0b4890879a0ec855a89cbb8
Kapil <kchoudhary006@gmail.com>
2016-06-21 16:17:47 +0530

    Code for import progress feature-countdown until next update: GROOV-1247

--- app/assets/javascripts/groovepacks/controllers/appCtrl.js
+++ app/assets/javascripts/groovepacks/controllers/appCtrl.js
@@ -6,9 +6,26 @@ groovepacks_controllers.
       $scope.se_deep_import_days = 1;
       $scope.ss_deep_import_days = 1;
       $scope.tp_deep_import_days = 1;
+      $scope.import_progress = 10;
+      $scope.countdown_progressbar = 0;
+      $scope.import_item_id = 0
       $scope.add_popup_summary_on_load = true;
       $scope.$on("user-data-reloaded", function () {
         $scope.current_user = auth;
+      });
+
+      groovIO.on('countdown_update', function (progress_info) {
+        if($scope.import_item_id==0) $scope.import_item_id = progress_info.id;
+        if($scope.import_item_id!=progress_info.id) {
+          $scope.import_progress = 10 
+          $scope.countdown_progressbar = 0;
+        }
+        $scope.import_progress = $scope.import_progress - 1
+        $scope.countdown_progressbar = (10 - $scope.import_progress)*10
+        if (($scope.import_progress == 0) ) {
+          $scope.import_progress = 10 
+          $scope.countdown_progressbar = 0;
+        }
       });
 
       groovIO.on('import_status_update', function (message) {
@@ -195,6 +212,7 @@ groovepacks_controllers.
                 '<td style="width:62%;padding:3px;">' +
                   '<progressbar type="{{store.progress.type}}" value="store.progress.value"> {{store.progress.message| limitTo: 75}}</progressbar>' +
                   // '<progressbar ng-show="store.progress_product.show" type="{{store.progress_product.type}}" value="store.progress_product.value">{{store.progress_product.message | limitTo: 56}}</progressbar>' +
+                  '<progressbar ng-show="store.progress_product.show" type="{{store.progress_product.type}}" value="{{countdown_progressbar}}" style="text-align: center;">{{import_progress | limitTo: 56}}</progressbar>' +
                 '</td>' +
                 '<td style="text-align:right;width:38%;padding:3px;">' +
                   '<div class="btn-group">' +
