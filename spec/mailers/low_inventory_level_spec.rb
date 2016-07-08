dd0c46e5ab39e2595de0510c3fc5d55f0b006a15
Shubham Panchkaran <shubham.panchkaran@navaratan.com>
2016-07-08 17:29:01 +0530

    Added test cases for low inventory level mailer

--- spec/mailers/low_inventory_level_spec.rb
+++ spec/mailers/low_inventory_level_spec.rb
@@ -2,7 +2,19 @@ require 'rails_helper'
 
 RSpec.describe LowInventoryLevel do
 	before(:each) do
-		@inv_wh = FactoryGirl.create(:inventory_warehouse, is_default: true)
+		inv_wh = FactoryGirl.create(:inventory_warehouse, is_default: true)
+		store = FactoryGirl.create(:store, :inventory_warehouse_id => inv_wh.id)
+
+		@product = FactoryGirl.create(:product, :name=>'Apple iPhone 5C')
+		product_sku = FactoryGirl.create(:product_sku, :product=> @product, :sku=>'IPHONE5C')
+		product_barcode = FactoryGirl.create(:product_barcode, :product=> @product, :barcode=>'1234567891')
+
+		@p_alias = FactoryGirl.create(:product, :name=>'Apple iPhone 5S')
+		p_alias_sku = FactoryGirl.create(:product_sku, :product=> @p_alias, :sku=>'IPHONE5S')
+		p_alias_barcode = FactoryGirl.create(:product_barcode, :product=> @p_alias, :barcode=>'1234567892')
+
+		Delayed::Worker.delay_jobs = false
+		tenant = FactoryGirl.create(:tenant, :name=>"sitetest")
 	end
 
 	describe 'low inventry level mailer' do
@@ -11,28 +23,53 @@ RSpec.describe LowInventoryLevel do
 			let(:mail) { LowInventoryLevel.notify(GeneralSetting.all.first, Apartment::Tenant.current).deliver }
 
 			it 'Should notify user when inventry level goes low through email' do
-				
-				tenant = FactoryGirl.create(:tenant, :name=>"sitetest")
-
-				general_setting = FactoryGirl.create(:general_setting, inventory_tracking: true, 
-					low_inventory_alert_email: true, low_inventory_email_address: "test@example.com", 
-					hold_orders_due_to_inventory: nil, conf_req_on_notes_to_packer: "never", 
-					send_email_for_packer_notes: "never", email_address_for_packer_notes: nil, 
-					default_low_inventory_alert_limit: 1, send_email_on_mon: true, send_email_on_tue: true, 
-					send_email_on_wed: true, send_email_on_thurs: true, send_email_on_fri: true, 
-					send_email_on_sat: true, send_email_on_sun: true, time_to_send_email: "2016-07-06 10:53:00")
+				general_setting = FactoryGirl.create :low_inventory_alert_settings
 
 				mail.subject.should eq("GroovePacker Low Inventory Alert")
 				mail.from.should eq(["app@groovepacker.com"])
 				mail.to.should eq([GeneralSetting.all.first.low_inventory_email_address])
 
-        last_delivery = ActionMailer::Base.deliveries.last
-        last_delivery.body.should include "This email was sent to let you know that the following items are currently at or below their low inventory"
-        # expect(LowInventoryLevel).to receive(:notify).once
-      end
+				last_delivery = ActionMailer::Base.deliveries.last
+				expect(ActionMailer::Base.deliveries.count).to eq(2)
+				last_delivery.body.should include "This email was sent to let you know that the following items are currently at or below their low inventory"
+			end
 
-    end
+			it 'Should notify user when inventry level goes low through email including products' do
+				general_setting = FactoryGirl.create :low_inventory_alert_settings
 
-  end
+				mail.subject.should eq("GroovePacker Low Inventory Alert")
+				mail.from.should eq(["app@groovepacker.com"])
+				mail.to.should eq([GeneralSetting.all.first.low_inventory_email_address])
+
+				last_delivery = ActionMailer::Base.deliveries.last
+				expect(ActionMailer::Base.deliveries.count).to eq(2)
+				last_delivery.body.should include "This email was sent to let you know that the following items are currently at or below their low inventory"
+				last_delivery.body.should include "Apple iPhone 5C"
+				last_delivery.body.should include "Apple iPhone 5S"
+			end
+
+			it 'Should notify user when inventry level goes lower then default limit through email including products' do
+				general_setting = FactoryGirl.create( :low_inventory_alert_settings, :default_low_inventory_alert_limit => 10 )
+
+				mail.subject.should eq("GroovePacker Low Inventory Alert")
+				mail.from.should eq(["app@groovepacker.com"])
+				mail.to.should eq([GeneralSetting.all.first.low_inventory_email_address])
+
+				last_delivery = ActionMailer::Base.deliveries.last
+				expect(ActionMailer::Base.deliveries.count).to eq(2)
+				last_delivery.body.should include "This email was sent to let you know that the following items are currently at or below their low inventory"
+				last_delivery.body.should include "Apple iPhone 5C"
+				last_delivery.body.should include "Apple iPhone 5S"
+			end
+
+			it 'Should not send mail when low inventory level email set to false' do
+				general_setting = FactoryGirl.create( :low_inventory_alert_settings, :low_inventory_alert_email=>false )
+
+				last_delivery = ActionMailer::Base.deliveries.count
+				expect(last_delivery).to eq(0)
+			end
+
+		end
+	end
 
 end
