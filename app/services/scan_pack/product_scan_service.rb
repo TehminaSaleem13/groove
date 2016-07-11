683e2ba2b247b23874b8157dd9b88e278f704ff2
Kapil <kchoudhary006@gmail.com>
2016-07-11 15:22:24 +0530

    changes in addactivity for barcode scan

--- app/services/scan_pack/product_scan_service.rb
+++ app/services/scan_pack/product_scan_service.rb
@@ -130,11 +130,15 @@ module ScanPack
         sku_for_activity = @order_item.product.primary_sku rescue nil
         order_item_sku = sku_for_activity.split(' ')[0] rescue nil
         type_in_count = @typein_count
-        if action_keyword.present? && order_item_sku.present? && action_keyword.include?("click") && action_keyword.include?(order_item_sku) && @typein_count > 1
-          type_in_count = @typein_count.to_i + 1
-          last_activity.action  += " for a Type-In count"
-          last_activity.save
-        end    
+        if action_keyword.present? && order_item_sku.present? && action_keyword.include?(order_item_sku) && @typein_count > 1
+          if action_keyword.include?("click")
+            type_in_count = @typein_count.to_i + 1
+            last_activity.action  += " for a Type-In count"
+            last_activity.save
+          elsif action_keyword.include?("barcode:")
+            type_in_count = @typein_count.to_i + 1
+          end
+        end
         @single_order.addactivity("Type-In count of #{type_in_count} entered for product #{sku_for_activity}", @current_user.username) if @typein_count > 1
         do_if_barcode_found
       else
