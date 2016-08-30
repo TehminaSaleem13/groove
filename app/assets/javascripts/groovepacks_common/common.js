c1a3e690a4de7e2439eb0c65cc5183a68b6749ce
Shubham Panchkaran <shubham.panchkaran@navaratan.com>
2016-08-30 21:28:50 +0530

    removed angular js code from api

--- app/assets/javascripts/groovepacks_common/common.js
+++ app/assets/javascripts/groovepacks_common/common.js
@@ -1,10 +1,4 @@
 //
-//= require ./lib
 //= require ./popover
 //= require ./moment
-//= require ./angular-moment
-//= require ./filters
-//= require ./services
-//= require ./directives
-//= require ./controllers
 //= require ./subscriptions
