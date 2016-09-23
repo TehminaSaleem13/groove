cc05cec52d7b512183fd615651ffd59d2f963eee
Ashish Singh <aitashish173@gmail.com>
2016-09-23 23:55:17 +0530

    Updated go code for delete orders

--- lib/groovepacker/utilities/go/delete_orders.go
+++ lib/groovepacker/utilities/go/delete_orders.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	_ "strconv"
+	"strings"
 	"time"
 )
 
@@ -27,11 +28,12 @@ type Tenant struct {
 var (
 	ENV            CONFIG
 	IntTenant      string
-	IntDeleteCount string
+	IntDeleteCount int
 )
 
 func main() {
 
+	start_time := time.Now()
 	intialize()
 
 	// Setup Connection with default DB
@@ -40,9 +42,17 @@ func main() {
 	defer db.Close()
 	tenants := collect_tenant_names(db)
 
-	for _, tenant := range tenants {
-		perform_deletion(tenant)
+	for i, tenant := range tenants {
+		msg := perform_deletion(tenant)
+		log.Println("---------- Operation ", i, " STARTED ---------")
+		for m := range msg {
+			log.Println(m)
+		}
+		log.Println("----------------------------------------------")
 	}
+
+	end_time := time.Since(start_time)
+	log.Println("Completed In (", end_time, ")")
 }
 
 func intialize() {
@@ -59,7 +69,7 @@ func intialize() {
 	// Intial Variables
 	if len(os.Args) > 2 {
 		IntTenant = os.Args[1]
-		IntDeleteCount = os.Args[2]
+		IntDeleteCount = 100 //os.Args[2]
 	}
 
 	fmt.Println(IntTenant, IntDeleteCount)
@@ -128,22 +138,41 @@ func time_zone(location string) *time.Location {
 	return zone
 }
 
-func perform_deletion(tenant Tenant) {
-	db := sql_open(tenant.Name)
-	defer db.Close()
+func perform_deletion(tenant Tenant) <-chan string {
+	msg := make(chan string)
 
-	orders_ids := orders_ninety_days_ago_ids(db)
+	go func() {
+		msg <- "Performing Deletion for Tenant DB -> " + tenant.Name
+		defer close(msg)
 
-	if len(IntDeleteCount) > 0 {
-		orders_ids = append(orders_ids, orders_custom_days_ids(db)...)
-	}
+		db := sql_open(tenant.Name)
+		defer db.Close()
 
-	//delete_orders_days := tenant.OrdersDeleteDays
-	log.Println(orders_ids)
+		order_ids := orders_ninety_days_ago_ids(db)
+		delete_order_days := tenant.OrdersDeleteDays
+
+		if delete_order_days > 0 {
+			order_ids = append(order_ids, orders_custom_days_ids(delete_order_days, db)...)
+			order_ids = removeDuplicates(order_ids)
+		}
+
+		if len(order_ids) == 0 {
+			msg <- "No Orders Found !!!"
+			return
+		}
+
+		//if IntDeleteCount > 0 {
+		//	order_ids = order_ids[:IntDeleteCount]
+		//}
+
+		msg <- delete_orders(order_ids, db, &msg)
+	}()
+
+	return msg
 }
 
-func orders_ninety_days_ago_ids(db *sql.DB) []int {
-	var orders_ids []int
+func orders_ninety_days_ago_ids(db *sql.DB) []string {
+	var order_ids []string
 
 	rows, err := sq.
 		Select("id").
@@ -158,18 +187,142 @@ func orders_ninety_days_ago_ids(db *sql.DB) []int {
 	}
 
 	for rows.Next() {
-		var id int
+		var id string
 		err := rows.Scan(&id)
 		if err != nil {
 			log.Fatal(err)
 		}
-		orders_ids = append(orders_ids, id)
+		order_ids = append(order_ids, id)
 	}
 
-	return orders_ids
+	return order_ids
 }
 
-func orders_custom_days_ids(db *sql.DB) []int {
-	var orders_ids []int
-	return orders_ids
+func orders_custom_days_ids(delete_order_days int, db *sql.DB) []string {
+	var order_ids []string
+
+	rows, err := sq.
+		Select("id").
+		From("orders").
+		Where("updated_at < ? && (status = ? || status = ?)",
+			time.Now().AddDate(0, 0, -delete_order_days).In(time_zone("Asia/Kolkata")).Format("2006-01-02 15:04:05"),
+			"awaiting", "onhold").
+		RunWith(db).Query()
+
+	defer rows.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var id string
+		err := rows.Scan(&id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		order_ids = append(order_ids, id)
+	}
+
+	return order_ids
+}
+
+func removeDuplicates(a []string) []string {
+	result := []string{}
+	seen := map[string]string{}
+	for _, val := range a {
+		if _, ok := seen[val]; !ok {
+			result = append(result, val)
+			seen[val] = val
+		}
+	}
+	return result
+}
+
+func delete_orders(order_ids []string, db *sql.DB, msg *chan string) string {
+	for i, j := 0, len(order_ids); i < j; {
+		tmp := j - i
+		if tmp > 500 {
+			tmp = i + 500
+		} else {
+			tmp += i
+		}
+		o_ids := order_ids[i:tmp]
+		log.Println("Deleting Orders from ", i, "to", tmp)
+		//Delete Items
+		delete_items(o_ids, db)
+
+		// Delete Order Data
+		table_names := []string{"order_activities",
+			"order_exceptions",
+			"order_serials",
+			"order_shippings"}
+
+		for _, table_name := range table_names {
+			query := sq.
+				Delete(table_name).
+				Where("order_id IN (" + strings.Join(o_ids, ",") + ")").
+				RunWith(db)
+			log.Println(query.ToSql())
+			log.Println(query.Exec())
+		}
+
+		query := sq.
+			Delete("orders").
+			Where("id IN (" + strings.Join(o_ids, ",") + ")").
+			RunWith(db)
+		log.Println(query.ToSql())
+		log.Println(query.Exec())
+		i += 500
+	}
+
+	return "Orders Deleted"
+}
+
+func delete_items(order_ids []string, db *sql.DB) {
+	var order_item_ids []string
+
+	if len(order_ids) == 0 {
+		log.Println("No Order Found")
+		return
+	}
+
+	query := sq.
+		Select("id").
+		From("order_items").
+		Where("order_id IN (" + strings.Join(order_ids, ",") + ")").
+		RunWith(db)
+	log.Println(query.ToSql())
+
+	rows, err := query.Query()
+	defer rows.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var id string
+		err := rows.Scan(&id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		order_item_ids = append(order_item_ids, id)
+	}
+
+	if len(order_item_ids) == 0 {
+		log.Println("No Order Items Found !!!")
+		return
+	}
+
+	table_names := []string{"order_item_kit_products",
+		"order_item_order_serial_product_lots",
+		"order_item_scan_times"}
+
+	for _, table_name := range table_names {
+		query := sq.
+			Delete(table_name).
+			Where("order_item_id IN (" + strings.Join(order_item_ids, ",") + ")").
+			RunWith(db)
+		log.Println(query.ToSql())
+		log.Println(query.Exec())
+	}
 }
