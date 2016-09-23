6708ba63cd17c807fd134ad342bf30e5436ce906
Ashish Singh <aitashish173@gmail.com>
2016-09-23 13:30:52 +0530

    Collecting order ids ffrom data base for 90days

--- lib/groovepacker/utilities/go/delete_orders.go
+++ lib/groovepacker/utilities/go/delete_orders.go
@@ -1,119 +1,175 @@
 package main
 
 import (
-  "database/sql"
-  _ "github.com/go-sql-driver/mysql"
-  "github.com/joho/godotenv"
-  "os"
-  "log"
-  _ "strconv"
-  "fmt"
+	"database/sql"
+	"fmt"
+	sq "github.com/Masterminds/squirrel"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	_ "strconv"
+	"time"
 )
 
 type CONFIG struct {
-  DB_HOST string
-  DB_NAME string
-  DB_USER string
-  DB_PASS string
+	DB_HOST string
+	DB_NAME string
+	DB_USER string
+	DB_PASS string
 }
 
-var(
-  ENV CONFIG
-  Tenant string
-  DeleteCount string
+type Tenant struct {
+	Name             string
+	OrdersDeleteDays int
+}
+
+var (
+	ENV            CONFIG
+	IntTenant      string
+	IntDeleteCount string
 )
 
 func main() {
-  
-  intialize()
-  
-  // Setup Connection with default DB
-  // To get all the tenant names
-  db := sql_open(ENV.DB_NAME)
-  defer db.Close()
-  tenant_names := collect_tenant_names(db)
 
-  for _, name := range tenant_names {
-    go perform_deletion(name)
-  }
+	intialize()
+
+	// Setup Connection with default DB
+	// To get all the tenant names
+	db := sql_open(ENV.DB_NAME)
+	defer db.Close()
+	tenants := collect_tenant_names(db)
+
+	for _, tenant := range tenants {
+		perform_deletion(tenant)
+	}
 }
 
 func intialize() {
-  _ = godotenv.Load("../../../../.env")
+	_ = godotenv.Load("../../../../.env")
 
-  // DB Config
-  ENV.DB_HOST = "tcp(" + os.Getenv("DB_HOST") + ":3306)"
-  ENV.DB_NAME = "groovepacks_development"
-  ENV.DB_USER = os.Getenv("DB_USERNAME")
-  ENV.DB_PASS = os.Getenv("DB_PASSWORD")
+	// DB Config
+	ENV.DB_HOST = "tcp(" + os.Getenv("DB_HOST") + ":3306)"
+	ENV.DB_NAME = "groovepacks_development"
+	ENV.DB_USER = os.Getenv("DB_USERNAME")
+	ENV.DB_PASS = os.Getenv("DB_PASSWORD")
 
-  fmt.Println(ENV)
+	fmt.Println(ENV)
 
-  // Intial Variables
-  if len(os.Args) > 2 {
-    Tenant = os.Args[1]
-    DeleteCount = os.Args[2]
-  }
+	// Intial Variables
+	if len(os.Args) > 2 {
+		IntTenant = os.Args[1]
+		IntDeleteCount = os.Args[2]
+	}
 
-  fmt.Println(Tenant, DeleteCount)
+	fmt.Println(IntTenant, IntDeleteCount)
 }
 
-func sql_open(db_name string) *sql.DB{
-  // user:password@host/DB?charset=utf8
-  dsn := ENV.DB_USER + ":" + ENV.DB_PASS +
-         "@" + ENV.DB_HOST + "/" + db_name +
-         "?charset=utf8"
-  db, err := sql.Open("mysql", dsn)
-  if err != nil {
-    log.Fatal(err)
-  }
+func sql_open(db_name string) *sql.DB {
+	// user:password@host/DB?charset=utf8
+	dsn := ENV.DB_USER + ":" + ENV.DB_PASS +
+		"@" + ENV.DB_HOST + "/" + db_name +
+		"?charset=utf8"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  err = db.Ping()
-  if err != nil {
-    log.Fatal(err)
-  }
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  return db
+	return db
 }
 
-  //
-func collect_tenant_names(db *sql.DB) []string {
-  var query string
-  
-  if len(Tenant) > 0 {
-    query = "SELECT name FROM `tenants` WHERE name = \"" + Tenant + "\""
-  }else{
-    query = "SELECT name FROM `tenants` ORDER BY name"
-  }
-  
-  rows, err := db.Query(query)
-  // Close query
-  defer rows.Close()
+//
+func collect_tenant_names(db *sql.DB) []Tenant {
+	var query string
 
-  if err != nil {
-    log.Fatal(err)
-  }
-  
-  tenant_names := make([]string, 0)
-  var name string
+	if len(IntTenant) > 0 {
+		query = "SELECT name, orders_delete_days FROM `tenants` WHERE name = \"" + IntTenant + "\""
+	} else {
+		query = "SELECT name, orders_delete_days FROM `tenants` ORDER BY name"
+	}
 
-  for rows.Next() {
-    err := rows.Scan(&name)
-    if err != nil {
-      log.Fatal(err)
-    }
-    tenant_names := append(tenant_names, name)
-    log.Println(tenant_names)
-  }
-  
-  err = rows.Err()
-  if err != nil {
-    log.Fatal(err)
-  }
+	rows, err := db.Query(query)
+	// Close query
+	defer rows.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  return tenant_names
+	tenants := make([]Tenant, 0)
+
+	for rows.Next() {
+		tenant := new(Tenant)
+		err := rows.Scan(&tenant.Name, &tenant.OrdersDeleteDays)
+		if err != nil {
+			log.Fatal(err)
+		}
+		tenants = append(tenants, *tenant)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(tenants)
+	return tenants
 }
 
-func perform_deletion(tenant_name string) {
-  db := sql_open(ENV.DB_NAME)
+func time_zone(location string) *time.Location {
+	zone, err := time.LoadLocation(location)
+	if err != nil {
+		fmt.Println("err: ", err.Error())
+	}
+	return zone
+}
+
+func perform_deletion(tenant Tenant) {
+	db := sql_open(tenant.Name)
+	defer db.Close()
+
+	orders_ids := orders_ninety_days_ago_ids(db)
+
+	if len(IntDeleteCount) > 0 {
+		orders_ids = append(orders_ids, orders_custom_days_ids(db)...)
+	}
+
+	//delete_orders_days := tenant.OrdersDeleteDays
+	log.Println(orders_ids)
+}
+
+func orders_ninety_days_ago_ids(db *sql.DB) []int {
+	var orders_ids []int
+
+	rows, err := sq.
+		Select("id").
+		From("orders").
+		Where("updated_at < ?",
+			time.Now().AddDate(0, 0, -90).In(time_zone("Asia/Kolkata")).Format("2006-01-02 15:04:05")).
+		RunWith(db).Query()
+
+	defer rows.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var id int
+		err := rows.Scan(&id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		orders_ids = append(orders_ids, id)
+	}
+
+	return orders_ids
+}
+
+func orders_custom_days_ids(db *sql.DB) []int {
+	var orders_ids []int
+	return orders_ids
 }
