ab785cd579a771d0dee6f7de434b7071c3d8f23d
Kapil <kchoudhary006@gmail.com>
2016-09-27 19:39:49 +0530

    Added code to get more info in HTTParty request failure for GL app

--- app/mailers/groovelytics_mailer.rb
+++ app/mailers/groovelytics_mailer.rb
@@ -1,8 +1,10 @@
 class GroovelyticsMailer < ActionMailer::Base
 	default from: "app@groovepacker.com"
 
-	def groovelytics_request_failed(tenant)
-		subject = "HTTParty post request to groovelytics server from #{tenant} failed"
-		mail to: ENV["FAILED_IMPORT_NOTIFICATION_EMAILS"], subject: subject
+	def groovelytics_request_failed(tenant, error_raised_manualy, error)
+      @error_raised_manualy = error_raised_manualy
+      @error = error
+      subject = "HTTParty post request to groovelytics server from #{tenant} failed"
+      mail to: ENV["FAILED_IMPORT_NOTIFICATION_EMAILS"], subject: subject
 	end
 end
