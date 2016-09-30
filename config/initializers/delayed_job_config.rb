c20330df69f2fe0871770af1a8d0b38b0aae4a0d
Kapil <kchoudhary006@gmail.com>
2016-09-30 19:54:55 +0530

    changes delayed max run time to 150 minutes

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,7 +1,7 @@
 Delayed::Worker.max_attempts = 5
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.max_run_time = 2.hours
+Delayed::Worker.max_run_time = 150.minutes
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
 
 Delayed::Backend::ActiveRecord::Job.class_eval do
