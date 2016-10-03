7e66a9aef3df0590ca7ba156904f18c672f417eb
Kapil <kchoudhary006@gmail.com>
2016-10-03 18:38:09 +0530

    Fixed order scan by tracking no issue

--- app/services/scan_pack/order_scan_service.rb
+++ app/services/scan_pack/order_scan_service.rb
@@ -48,7 +48,7 @@ module ScanPack
     def collect_orders
       find_order
       if @orders.empty? && @scanpack_settings.scan_by_tracking_number
-        @orders = Order.where("tracking_num IN (?, '#{@input}tracking_num#{@input}') ", @input)
+        @orders = Order.where('tracking_num = ? or ? LIKE CONCAT("%",tracking_num,"%") ',@input, @input)
       end
       @single_order = @orders.first
     end
