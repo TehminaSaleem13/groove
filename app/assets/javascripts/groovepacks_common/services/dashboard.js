474a016e65c7d4893ad8320b135cd82e22afad76
Kapil <kchoudhary006@gmail.com>
2016-10-18 19:53:18 +0530

    fixes for dashboard avg point: GROOV-1199

--- app/assets/javascripts/groovepacks_common/services/dashboard.js
+++ app/assets/javascripts/groovepacks_common/services/dashboard.js
@@ -1,14 +1,3 @@
-groovepacks_controllers.controller('DashboardCtrl', function ($scope, $modal, $log, $http) {
-  $scope.ok = function(avg, data_point){
-    url = "/dashboard/update_to_avg_datapoint.json?avg=" + avg + "&&val=" + data_point;
-    $http.post(url);
-    $scope.modal_d.close();
-  };
-  $scope.cancel = function(){
-    $scope.modal_d.close();
-  };
-});
-
 groovepacks_services.factory('dashboard', ['$http', 'notification', 'auth', '$modal', function ($http, notification, auth, $modal) {
   var get_default = function () {
     return {
