42671c8c2ecd3379c5b3e3d277210f9b5afa4c9a
Kapil <kchoudhary006@gmail.com>
2016-11-15 19:56:12 +0530

    revert notification js changes

--- app/assets/javascripts/groovepacks_common/services/notification.js
+++ app/assets/javascripts/groovepacks_common/services/notification.js
@@ -23,11 +23,11 @@ groovepacks_services.factory("notification", ['$timeout', '$rootScope', '$window
     );
   };
 
-  var notify = function (msgs, type, close=0) {
-    notification_id = localStorage.getItem('notification_id');
-    if (close == "2"){
-      delete_notif(notification_id)
-    }
+  var notify = function (msgs, type) {
+    // notification_id = localStorage.getItem('notification_id');
+    // if (close == "2"){
+    //   delete_notif(notification_id)
+    // }
     if (typeof type != "number" || typeof notif_types[type] == "undefined") {
       type = notif_types["default"];
     }
@@ -44,12 +44,12 @@ groovepacks_services.factory("notification", ['$timeout', '$rootScope', '$window
           }
         }
         id++;
-        notifications[id] = {show: true, alert: alert, msg: msgs[i]};
-        if (close!=1) {
+        // notifications[id] = {show: true, alert: alert, msg: msgs[i]};
+        // if (close!=1) {
           queue_remove(id);
-        } else {
-          localStorage.setItem('notification_id', id);
-        }
+        // } else {
+        //   localStorage.setItem('notification_id', id);
+        // }
       }
     }else { // if more than 5 notifications group them.
       var message = msgs[0];
