8bff44a976899a2495baa987d76c4b8e03ada521
Kapil <kchoudhary006@gmail.com>
2016-11-25 12:52:49 +0530

    added mailer for failed export email: GROOV-1507

--- app/mailers/export_order.rb
+++ app/mailers/export_order.rb
@@ -5,31 +5,41 @@ class ExportOrder < ActionMailer::Base
     Apartment::Tenant.switch(tenant)
     export_settings = ExportSetting.first
     
-    @counts = get_order_counts(export_settings) if export_settings.export_orders_option == 'on_same_day'
-    filename = export_settings.export_data(tenant)
-    @tenant_name = tenant    
-    url = GroovS3.find_export_csv(tenant, filename)
-    # file_locatin = "#{Rails.root}/public/csv/#{filename}"
-    @csv_data = []
-    file_data = Net::HTTP.get(URI.parse(url)) rescue []
-    file_data = file_data.split("\n")
-    file_data.each {|row| @csv_data << row.split(",")}
-    # @csv_data = Net::HTTP.get(URI.parse(url)) rescue []
-    @csv_data.try(:first).try(:each_with_index) do |value, index|
-      case value
-      when 'order_number'
-        @order_number = index
-      when 'scanned_by_status_change'
-        @scanned_by_status_change = index
+    begin
+      @counts = get_order_counts(export_settings) if export_settings.export_orders_option == 'on_same_day'
+      filename = export_settings.export_data(tenant)
+      @tenant_name = tenant    
+      url = GroovS3.find_export_csv(tenant, filename)
+      # file_locatin = "#{Rails.root}/public/csv/#{filename}"
+      @csv_data = []
+      file_data = Net::HTTP.get(URI.parse(url)) rescue []
+      file_data = file_data.split("\n")
+      file_data.each {|row| @csv_data << row.split(",")}
+      # @csv_data = Net::HTTP.get(URI.parse(url)) rescue []
+      @csv_data.try(:first).try(:each_with_index) do |value, index|
+        case value
+        when 'order_number'
+          @order_number = index
+        when 'scanned_by_status_change'
+          @scanned_by_status_change = index
+        end
       end
+      attachments["#{filename}"] = Net::HTTP.get(URI.parse(url)) rescue nil
+      mail to: export_settings.order_export_email,
+           subject: "GroovePacker Order Export Report"
+      #import_orders_obj = ImportOrders.new
+      #import_orders_obj.reschedule_job('export_order', tenant)
+      #File.delete(file_locatin) rescue nil
+    rescue => e
+      ExportOrder.failed_export(e).deliver      
     end
+  end
 
-    attachments["#{filename}"] = Net::HTTP.get(URI.parse(url)) rescue nil
-    mail to: export_settings.order_export_email,
-         subject: "GroovePacker Order Export Report"
-    #import_orders_obj = ImportOrders.new
-    #import_orders_obj.reschedule_job('export_order', tenant)
-    #File.delete(file_locatin) rescue nil
+  def failed_export(exception)
+    export_settings = ExportSetting.first
+    maunual_or_auto = export_settings.manual_export ? "Manual" : "Auto" 
+    @exception = exception
+    mail to: export_settings.order_export_email, subject: "[#{Apartment::Tenant.current}] [#{Rails.env}] #{maunual_or_auto} Order Export Failed"
   end
 
   def get_order_counts(export_settings)
