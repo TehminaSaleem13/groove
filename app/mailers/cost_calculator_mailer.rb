9a341a09daedd0dfe5e115522b84faff479addc5
Kapil <kchoudhary006@gmail.com>
2016-12-17 14:50:02 +0530

    cost calc quick fix

--- app/mailers/cost_calculator_mailer.rb
+++ app/mailers/cost_calculator_mailer.rb
@@ -7,7 +7,7 @@ class CostCalculatorMailer < ActionMailer::Base
   	follow_up_email = params[:follow_up_email]
   	recipients = [params[:recipient_one], params[:recipient_two], params[:recipient_three]]	
   	@object_url = "admin.#{ENV['HOST_NAME']}/cost_calculator?" + URI.encode(params.except!("controller", "action", "cost_calculator", "recipient_one", "recipient_two" , "recipient_three", "recipient_four", "follow_up_email").to_query)
-  	subject = "Fulfillment error cost calculator / Sent by #{params["recipient_name"]}"
+  	subject = params["recipient_name"].present? ? "Fulfillment error cost calculator / Sent by #{params["recipient_name"]}" : "Fulfillment error cost calculator"
     mail to: recipients, subject: subject if !(recipients.all? &:blank?)
   end
 end
