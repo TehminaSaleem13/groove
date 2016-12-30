143bed005e02d456e0bca05fac3059f386335e46
Ashish Singh <aitashish173@gmail.com>
2016-12-30 11:08:29 +0530

    order search by tracking num with status check only for awaiting and serviceissue

--- app/services/scan_pack/order_scan_service.rb
+++ app/services/scan_pack/order_scan_service.rb
@@ -48,7 +48,12 @@ module ScanPack
     def collect_orders
       find_order
       if @orders.empty? && @scanpack_settings.scan_by_tracking_number
-        @orders = Order.where('tracking_num = ? or ? LIKE CONCAT("%",tracking_num,"%") ',@input, @input)
+        @orders =
+          Order
+          .where(
+            'status IN ("awaiting","serviceissue") AND (tracking_num = ? or ? LIKE CONCAT("%",tracking_num,"%"))',
+            @input, @input
+          )
       end
       @single_order = @orders.first
     end
