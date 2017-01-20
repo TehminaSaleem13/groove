0e32098df616515b366209f9a6e7ba03ec541c5f
Kapil <kchoudhary006@gmail.com>
2017-01-20 15:42:25 +0530

    some more refectoring

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -64,52 +64,54 @@ class StoresController < ApplicationController
     render json: result
   end
 
-  def init_update_store_data
-    init_store_data
-  end
+  # def init_update_store_data
+  #   init_store_data
+  # end
 
   def create_update_store
     @result = {"status"=>true, "store_id"=>0, "csv_import"=>false, "messages"=>[]}
-    if current_user.can? 'add_edit_stores'
-      if params[:id].nil?
-        if Store.can_create_new?
-          @store = Store.new
-          init_update_store_data
-          # ftp_credential = FtpCredential.create(use_ftp_import: false, store_id: @store.id) if params[:store_type] == 'CSV'
-          params[:id] = @store.id
-        else
-          @result['status'] = false
-          @result['messages'] = "You have reached the maximum limit of number of stores for your subscription."
-        end
-      end
-      update_create_store unless params[:id].blank?
-    end
+    create_store if current_user.can? 'add_edit_stores'
+      # if params[:id].nil?
+      #   if Store.can_create_new?
+      #     @store = Store.new
+      #     init_store_data
+      #     # init_update_store_data
+      #     # ftp_credential = FtpCredential.create(use_ftp_import: false, store_id: @store.id) if params[:store_type] == 'CSV'
+      #     params[:id] = @store.id
+      #   else
+      #     @result['status'] = false
+      #     @result['messages'] = "You have reached the maximum limit of number of stores for your subscription."
+      #   end
+      # end
+      # update_create_store unless params[:id].blank?
+    # end
     render json: @result
   end
 
-  def update_create_store
-    @store ||= Store.find(params[:id])
-    FtpCredential.create(use_ftp_import: false, store_id: @store.id) if params[:store_type] == 'CSV' && @store.ftp_credential.nil?
-    create_and_update_store
-    @result["store_id"] = @store.id if !@store.nil? && @store.id.present? rescue nil
-  end
+  # def update_create_store
+  #   @store ||= Store.find(params[:id])
+  #   FtpCredential.create(use_ftp_import: false, store_id: @store.id) if params[:store_type] == 'CSV' && @store.ftp_credential.nil?
+  #   create_and_update_store
+  #   @result["store_id"] = @store.id if !@store.nil? && @store.id.present? rescue nil
+  # end
 
-  def create_and_update_store
-    if params[:store_type].nil?
-      @result['status'] = false
-      @result['messages'].push('Please select a store type to create a store')
-    else
-      init_update_store_data
-    end
-    if @result['status']
-      params[:import_images] = false if params[:import_images].nil?
-      params[:import_products] = false if params[:import_products].nil? 
-      @result = check_store_type
-    else
-      @result['status'] = false
-      @result['messages'].push("Current user does not have permission to create or edit a store")
-    end
-  end
+  # def create_and_update_store
+  #   if params[:store_type].nil?
+  #     @result['status'] = false
+  #     @result['messages'].push('Please select a store type to create a store')
+  #   else
+  #     init_store_data
+  #     # init_update_store_data
+  #   end
+  #   if @result['status']
+  #     params[:import_images] = false if params[:import_images].nil?
+  #     params[:import_products] = false if params[:import_products].nil? 
+  #     @result = check_store_type
+  #   else
+  #     @result['status'] = false
+  #     @result['messages'].push("Current user does not have permission to create or edit a store")
+  #   end
+  # end
 
   def csv_map_data
     result = {'product' => CsvMap.find_all_by_kind('product'), 'order' => CsvMap.find_all_by_kind('order'), 'kit' => CsvMap.find_all_by_kind('kit')}
@@ -172,55 +174,8 @@ class StoresController < ApplicationController
     check_store
     check_store_status
     csv_store_map_data if @result['status']
-    if @result['status']
-      data = {:flag=>params[:flag], :type=>params[:type], :fix_width=>params[:fix_width], :fixed_width=>params[:fixed_width], :sep=>params[:sep], :delimiter=>params[:delimiter], :rows=>params[:rows], :map=>params[:map], :store_id=>params[:store_id], :import_action=>params[:import_action], :contains_unique_order_items=>params[:contains_unique_order_items], :generate_barcode_from_sku=>params[:generate_barcode_from_sku], :use_sku_as_product_name=>params[:use_sku_as_product_name], :order_placed_at=>params[:order_placed_at],  :order_date_time_format=>params[:order_date_time_format], :day_month_sequence=>params[:day_month_sequence]}    
-      # Comment everything after this line till next comment (i.e. the entire if block) when everything is moved to bulk actions
-      if params[:type] == 'order'
-        order_csv_import(data)
-        # if OrderImportSummary.where(status: 'in_progress').empty? 
-        #   bulk_actions = Groovepacker::Orders::BulkActions.new
-        #   bulk_actions.delay(:run_at => 1.seconds.from_now).import_csv_orders(Apartment::Tenant.current_tenant, @store.id, data.to_s, current_user.id)
-        #   # bulk_actions.import_csv_orders(Apartment::Tenant.current_tenant, @store.id, data.to_s, current_user.id)
-        # else
-        #   @result['status'] = false
-        #   @result['messages'].push("Import is in progress. Try after it is complete")
-        # end
-      elsif params[:type] == 'kit'
-        groove_bulk_actions = GrooveBulkActions.new(:identifier=>'csv_import', :activity=>'kit')
-        # groove_bulk_actions.identifier = 'csv_import'
-        # groove_bulk_actions.activity = 'kit'
-        groove_bulk_actions.save
-        data[:bulk_action_id] = groove_bulk_actions.id
-        import_csv = ImportCsv.new
-        import_csv.delay(:run_at => 1.seconds.from_now).import Apartment::Tenant.current, data.to_s
-        # import_csv.import(Apartment::Tenant.current, data.to_s)
-      elsif params[:type] == 'product'
-        product_import = CsvProductImport.find_by_store_id(@store.id)
-        product_import = CsvProductImport.create(store_id: @store.id) if product_import.nil?
-        # if product_import.nil?
-        #   product_import = CsvProductImport.new
-        #   product_import.store_id = @store.id
-        #   product_import.save
-        # end
-        import_csv = ImportCsv.new
-        delayed_job = import_csv.delay(:run_at => 1.seconds.from_now).import Apartment::Tenant.current, data.to_s
-        # delayed_job = import_csv.import(Apartment::Tenant.current, data.to_s)
-        product_import.update_attributes(delayed_job_id: delayed_job.id, total: 0, success: 0, cancel: false, status: 'scheduled')
-      end
-      # Comment everything before this line till previous comment (i.e. the entire if block) when everything is moved to bulk actions
-    end
+    csv_import if @result['status']
     render json: @result
-  end
-
-  def order_csv_import(data)
-    if OrderImportSummary.where(status: 'in_progress').empty? 
-      bulk_actions = Groovepacker::Orders::BulkActions.new
-      bulk_actions.delay(:run_at => 1.seconds.from_now).import_csv_orders(Apartment::Tenant.current_tenant, @store.id, data.to_s, current_user.id)
-      # bulk_actions.import_csv_orders(Apartment::Tenant.current_tenant, @store.id, data.to_s, current_user.id)
-    else
-      @result['status'] = false
-      @result['messages'].push("Import is in progress. Try after it is complete")
-    end
   end
 
   def csv_product_import_cancel
@@ -242,17 +197,18 @@ class StoresController < ApplicationController
 
   def change_store_status
     @result = {"status"=>true, "messages"=>[]}
-    if current_user.can? 'add_edit_stores'
-      params['_json'].each do |store|
-        @store = Store.find(store["id"])
-        @store.status = store["status"]
-        @result['status'] = false if !@store.save
-      end
-      OrderImportSummary.first.emit_data_to_user unless OrderImportSummary.first.nil?
-    else
-      @result["status"] = false
-      @result["messages"].push('User does not have permissions to change store status')
-    end
+    update_store_status
+    # if current_user.can? 'add_edit_stores'
+    #   params['_json'].each do |store|
+    #     @store = Store.find(store["id"])
+    #     @store.status = store["status"]
+    #     @result['status'] = false if !@store.save
+    #   end
+    #   OrderImportSummary.first.emit_data_to_user unless OrderImportSummary.first.nil?
+    # else
+    #   @result["status"] = false
+    #   @result["messages"].push('User does not have permissions to change store status')
+    # end
     render json: @result
   end
 
@@ -337,29 +293,30 @@ class StoresController < ApplicationController
     @store = EbayCredentials.where(:store_id => params[:id])
 
     if !@store.nil? && @store.length > 0
-      @store = @store.first
-      req = Net::HTTP::Post.new(url.path)
-      req.add_field("X-EBAY-API-REQUEST-CONTENT-TYPE", 'text/xml')
-      req.add_field("X-EBAY-API-COMPATIBILITY-LEVEL", "675")
-      req.add_field("X-EBAY-API-DEV-NAME", ENV['EBAY_DEV_ID'])
-      req.add_field("X-EBAY-API-APP-NAME", ENV['EBAY_APP_ID'])
-      req.add_field("X-EBAY-API-CERT-NAME", ENV['EBAY_CERT_ID'])
-      req.add_field("X-EBAY-API-SITEID", 0)
-      req.add_field("X-EBAY-API-CALL-NAME", "FetchToken")
-      req.body ='<?xml version="1.0" encoding="utf-8"?>'+ '<FetchTokenRequest xmlns="urn:ebay:apis:eBLBaseComponents">'+ '<SessionID>'+session[:ebay_session_id]+'</SessionID>' + '</FetchTokenRequest>'
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      res = http.start do |http_runner|
-        http_runner.request(req)
-      end
-      ebaytoken_resp = MultiXml.parse(res.body)
-      @result['response'] = ebaytoken_resp
-      if ebaytoken_resp['FetchTokenResponse']['Ack'] == 'Success'
-        @store.auth_token = ebaytoken_resp['FetchTokenResponse']['eBayAuthToken']
-        @store.productauth_token = ebaytoken_resp['FetchTokenResponse']['eBayAuthToken']
-        @store.ebay_auth_expiration = ebaytoken_resp['FetchTokenResponse']['HardExpirationTime']
-        @result['status'] = true if @store.save
-      end
+      ebay_token_update(url)
+      # @store = @store.first
+      # req = Net::HTTP::Post.new(url.path)
+      # req.add_field("X-EBAY-API-REQUEST-CONTENT-TYPE", 'text/xml')
+      # req.add_field("X-EBAY-API-COMPATIBILITY-LEVEL", "675")
+      # req.add_field("X-EBAY-API-DEV-NAME", ENV['EBAY_DEV_ID'])
+      # req.add_field("X-EBAY-API-APP-NAME", ENV['EBAY_APP_ID'])
+      # req.add_field("X-EBAY-API-CERT-NAME", ENV['EBAY_CERT_ID'])
+      # req.add_field("X-EBAY-API-SITEID", 0)
+      # req.add_field("X-EBAY-API-CALL-NAME", "FetchToken")
+      # req.body ='<?xml version="1.0" encoding="utf-8"?>'+ '<FetchTokenRequest xmlns="urn:ebay:apis:eBLBaseComponents">'+ '<SessionID>'+session[:ebay_session_id]+'</SessionID>' + '</FetchTokenRequest>'
+      # http = Net::HTTP.new(url.host, url.port)
+      # http.use_ssl = true
+      # res = http.start do |http_runner|
+      #   http_runner.request(req)
+      # end
+      # ebaytoken_resp = MultiXml.parse(res.body)
+      # @result['response'] = ebaytoken_resp
+      # if ebaytoken_resp['FetchTokenResponse']['Ack'] == 'Success'
+      #   @store.auth_token = ebaytoken_resp['FetchTokenResponse']['eBayAuthToken']
+      #   @store.productauth_token = ebaytoken_resp['FetchTokenResponse']['eBayAuthToken']
+      #   @store.ebay_auth_expiration = ebaytoken_resp['FetchTokenResponse']['HardExpirationTime']
+      #   @result['status'] = true if @store.save
+      # end
     else
       @result['status'] = false;
     end
@@ -506,22 +463,7 @@ class StoresController < ApplicationController
 
   def update_store_list
     @result = {"status"=>true}
-    @store = Store.find(params[:id])
-    if @store.nil?
-      @result['status'] = false
-      @result['message'] = "Either the store is not present or you don't have permissions to update"
-    else
-      unless params[:var].eql?('status')
-        @result['status'] = false
-        @result['message'] = "Unkown Field"
-        return @result
-      end
-      @store.status = params[:value] if params[:var] == 'status'
-      if @result['status'] && !@store.save
-        @result['status'] = false
-        @result['message'] = "Could not save store info"
-      end
-    end
+    store_list_update
     render json: @result
   end
 end
