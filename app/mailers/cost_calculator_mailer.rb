36a4ad15f666c19f443a5f41262f141a7782b280
Kapil <kchoudhary006@gmail.com>
2017-04-08 10:47:03 +0530

    added groovepacker email

--- app/mailers/cost_calculator_mailer.rb
+++ app/mailers/cost_calculator_mailer.rb
@@ -5,7 +5,7 @@ class CostCalculatorMailer < ActionMailer::Base
   	@reciepient_name = params["recipient_name"]
   	@body = params["email_text"].gsub("6904.5", params[:monthly_shipping])
   	follow_up_email = params[:follow_up_email]
-  	recipients = [params[:recipient_one], params[:recipient_two], params[:recipient_three]]	
+  	recipients = [params[:recipient_one], params[:recipient_two], params[:recipient_three], "groovepacker+calculator@gmail.com"]	
   	@object_url = "admin.#{ENV['HOST_NAME']}/cost_calculator?" + URI.encode(params.except!("controller", "action", "cost_calculator", "recipient_one", "recipient_two" , "recipient_three", "recipient_four", "follow_up_email").to_query)
   	subject = params["recipient_name"].present? ? "Fulfillment error cost calculator / Sent by #{params["recipient_name"]}" : "Fulfillment error cost calculator"
     mail to: recipients, subject: subject if !(recipients.all? &:blank?)
