f8a9deb9f8ff65df664bafd47232f600413d9287
Kapil <kchoudhary006@gmail.com>
2017-07-05 14:25:37 +0530

    before save item check

--- app/models/order.rb
+++ app/models/order.rb
@@ -21,6 +21,7 @@ class Order < ActiveRecord::Base
   has_and_belongs_to_many :order_tags
   after_update :update_inventory_levels_for_items
   before_save :perform_pre_save_checks
+  before_save :unique_order_items
   after_save :process_unprocessed_orders
   after_save :update_tracking_num_value
   after_save :delete_if_order_exist
@@ -1007,5 +1008,9 @@ class Order < ActiveRecord::Base
   def delete_if_order_exist
     orders = Order.where(increment_id: increment_id)
     self.destroy if orders.count > 1
+  end
+
+  def unique_order_items
+    self.order_items = self.order_items.uniq_by {|obj| obj.product_id} 
   end
 end
