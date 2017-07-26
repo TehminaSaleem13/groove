58980757c6f364937215daf0d476c6341e9234f3
Kapil <kchoudhary006@gmail.com>
2017-07-26 14:17:01 +0530

    on demand, multi delayed job changes

--- app/services/scan_pack/scan_barcode_service.rb
+++ app/services/scan_pack/scan_barcode_service.rb
@@ -115,9 +115,14 @@ module ScanPack
         current_tenant = Apartment::Tenant.current
         order_no_input = @params["input"]
         order_importer = Groovepacker::Stores::Importers::OrdersImporter.new(nil)
-        order_importer.delay(:run_at => 1.seconds.from_now).search_and_import_single_order(tenant: current_tenant, order_no: order_no_input)
-        #order_importer.search_and_import_single_order(tenant: current_tenant, order_no: order_no_input)
-        @result["notice_messages"]="It does not look like that order has been imported into GroovePacker. We'll attempt to import it in the background and you can continue scanning other orders while it imports."
+        job = Delayed::Job.find_by_queue(order_no_input)
+        if job.blank? || job.failed_at.present?
+          order_importer.delay(:run_at => 1.seconds.from_now, :queue => order_no_input).search_and_import_single_order(tenant: current_tenant, order_no: order_no_input)
+          #order_importer.search_and_import_single_order(tenant: current_tenant, order_no: order_no_input)
+          @result["notice_messages"]="It does not look like that order has been imported into GroovePacker. We'll attempt to import it in the background and you can continue scanning other orders while it imports."
+        else
+          @result["notice_messages"]="Still checking on this order."
+        end
       end
     end
   end
