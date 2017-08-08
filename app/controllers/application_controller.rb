962c6c8f6a71f0c819c0fd17ee9bc44d71f76587
Kapil <kchoudhary006@gmail.com>
2017-08-08 11:59:54 +0530

    logout issue changeas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
 
   def groovepacker_authorize!
     auth_header = request.headers["Authorization"]
-    if !auth_header.nil? && auth_header.include?("Bearer")
+    if auth_header.nil?
+      doorkeeper_authorize!
+    elsif !auth_header.nil? && auth_header.include?("Bearer")
       doorkeeper_authorize!
       @current_user = User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
       stored_session = JSON.generate({'tenant' => Apartment::Tenant.current, 'user_id' => @current_user.id, 'username' => @current_user.username})
