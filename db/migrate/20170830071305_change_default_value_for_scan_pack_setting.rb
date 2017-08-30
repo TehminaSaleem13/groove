ae9ab86adbf0b3369fec6f88c70363fb1e6eaf19
Kapil <kchoudhary006@gmail.com>
2017-08-30 13:45:39 +0530

    GROOV-1711 : Quick ui fix in settings

---
+++ db/migrate/20170830071305_change_default_value_for_scan_pack_setting.rb
@@ -0,0 +1,9 @@
+class ChangeDefaultValueForScanPackSetting < ActiveRecord::Migration
+  def up
+  	change_column :scan_pack_settings, :string_removal_enabled, :boolean, :default => false
+  end
+
+  def down
+  	change_column :scan_pack_settings, :string_removal_enabled, :boolean, :default => true
+  end
+end
