2f327ddc5664e4bf4a108ce6b963f1e1ee585fb3
Kapil <kchoudhary006@gmail.com>
2017-09-18 12:50:07 +0530

    Revert CSV changes

--- lib/groovepacker/stores/importers/csv/orders_importer.rb
+++ lib/groovepacker/stores/importers/csv/orders_importer.rb
@@ -125,23 +125,26 @@ module Groovepacker
           end
 
           def check_single_row_order_item(order, items_array, order_items_ar, index, current_inc_id, order_map, result)
-            record_hash = []
-            new_array = []
-            order_items_ar.each { |row| record_hash << @helper.get_row_data(row, 'increment_id') }
-            final_record.each { |row| new_array << row if @helper.get_row_data(row, 'increment_id').delete(' ').include?(record_hash[0].delete(' ')) }
-            order_items_ar = new_array
+            # record_hash = []
+            # new_array = []
+            # order_items_ar.each { |row| record_hash << @helper.get_row_data(row, 'increment_id') }
+            # final_record.each { |row| new_array << row if @helper.get_row_data(row, 'increment_id').delete(' ').include?(record_hash[0].delete(' ')) }
+            # order_items_ar = new_array
             qty = items_array.flatten.reject { |c| c.is_a?(String) }
             if order.order_items.count == order_items_ar.count && qty.sum == order.order_items.map(&:qty).sum
               log = {}
             #if order.order_items.count == items_array.count 
               #order_item = order.order_items.where(:sku => row[0]).first
               #order_item.update_attribute(:qty, row[1]) if order_item.qty != row[1]
-              if ["unitedmedco", "sunlessinc", "janinetait", "brokencoast"].include?(Apartment::Tenant.current)
+              # if ["unitedmedco", "sunlessinc", "janinetait", "brokencoast"].include?(Apartment::Tenant.current)
                 on_demand_logger = Logger.new("#{Rails.root}/log/qty_csv_import_1_#{Apartment::Tenant.current}.log")
                 on_demand_logger.info("1=========================================1")
                 log[current_inc_id] = order_items_ar.flatten.join(",")
                 on_demand_logger.info(log) 
-              end
+                file_response = File.read("#{Rails.root}/log/qty_csv_import_1_#{Apartment::Tenant.current}.log")
+                
+              # end
+              binding.pry
             else
               log = {}
               order.destroy
