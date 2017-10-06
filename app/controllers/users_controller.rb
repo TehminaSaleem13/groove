728c66d2351dd7ec52ac604b6bef2b3806f20f1a
Kapil <kchoudhary006@gmail.com>
2017-10-06 17:26:18 +0530

    some changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -414,7 +414,7 @@ class UsersController < ApplicationController
       if user.email.blank?
         admin_user = User.find_by_email(admin_email)
         result[:msg] = "A password recovery link has been sent to #{admin_user.username} at #{admin_email}"
-        email = admin_email
+        email = admin_user.send_reset_password_instructions
       else
         result[:msg] = "A password reset link has been emailed to the address associated with your user account: #{user.email}"
         email = user.send_reset_password_instructions
