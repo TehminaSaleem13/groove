2829a2cc39e29c317972c5cdc61974aac33c0996
Ashish Singh <aitashish173@gmail.com>
2017-10-17 19:27:58 +0530

    adding auth config to elixir service

--- app/services/elixir_api/Processor/csv/base.rb
+++ app/services/elixir_api/Processor/csv/base.rb
@@ -1,7 +1,38 @@
 module ElixirApi
   module Processor
     module CSV
+      # Keep generic methods for the Elixir services here
       class Base < ServiceInit
+        IMPORT_ROUTES = {
+          'order' => '/route/for/order/import'
+        }.freeze
+
+        def host_url(tenant)
+          "http://#{tenant}.#{ENV['HOST_NAME']}.com"
+        end
+
+        def auth_params
+          {
+            'route' => '/auth/v1/login',
+            'username' => ENV['ADMIN_USERNAME'],
+            'password' => ENV['ADMIN_PASSWORD']
+          }
+        end
+
+        def generate_mapping(map)
+          map.each_with_object({}) do |map_single, mapping|
+            map_single_first_value = map_single[1].present? &&
+                                     map_single[1]['value']
+
+            next mapping unless map_single_first_value &&
+                                map_single_first_value != 'none'
+
+            mapping[map_single_first_value] = {
+              position: map_single[0].to_i,
+              action:  map_single[1][:action] || 'skip'
+            }
+          end
+        end
       end
     end
   end
