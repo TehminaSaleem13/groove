679190e4f81b07058a75d9d256a448b173fc2a2b
Ashish Singh <aitashish173@gmail.com>
2017-10-20 22:18:18 +0530

    buf fix

--- app/services/elixir_api/Processor/csv/base.rb
+++ app/services/elixir_api/Processor/csv/base.rb
@@ -8,7 +8,7 @@ module ElixirApi
         }.freeze
 
         def host_url(tenant)
-          "http://#{tenant}.#{ENV['HOST_NAME']}.com"
+          "http://#{tenant}.#{ENV['HOST_NAME']}"
         end
 
         def auth_params
