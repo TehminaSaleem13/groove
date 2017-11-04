84a1bc584d127b328c18ceedb62d7dd45c9ce2cd
Kapil <kchoudhary006@gmail.com>
2017-11-04 16:05:54 +0530

    update changes for gem and error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     if !auth_header.nil? && auth_header.include?("Bearer")
       doorkeeper_authorize!
       @current_user = User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-      stored_session = JSON.generate({'tenant' => Apartment::Tenant.current, 'user_id' => @current_user.id, 'username' => @current_user.username})
+      stored_session = JSON.generate({'tenant' => Apartment::Tenant.current, 'user_id' => @current_user.try(:id), 'username' => @current_user.try(:username)})
       $redis.hset('groovehacks:session', auth_header.gsub("Bearer ", ""), stored_session)
     else
       puts auth_header.inspect
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
     #store session to redis
     if current_user
       user = current_user
-      user.last_sign_in_at=DateTime.now
+      user.last_sign_in_at = DateTime.now
       user.save
       save_bc_auth_if_present
       # an unique MD5 key
