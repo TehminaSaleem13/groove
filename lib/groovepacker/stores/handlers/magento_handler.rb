327c90e9b543855ab8d6f2059402227a32f57f13
Kapil <kchoudhary006@gmail.com>
2017-12-14 11:41:49 +0530

    added loop and new api for magento

--- lib/groovepacker/stores/handlers/magento_handler.rb
+++ lib/groovepacker/stores/handlers/magento_handler.rb
@@ -3,25 +3,25 @@ module Groovepacker
     module Handlers
       class MagentoHandler < Handler
         def build_handle
-          magento_credential =
-            MagentoCredentials.where(:store_id => self.store.id).first unless MagentoCredentials.where(:store_id => self.store.id).empty?
+          magento_credential = MagentoCredentials.where(:store_id => self.store.id).first unless MagentoCredentials.where(:store_id => self.store.id).empty?
           client = nil
           session = nil
-
+          tenant = Apartment::Tenant.current
           if !magento_credential.nil?
-            client =
-              Savon.client(
-                wsdl: magento_credential.host+"/index.php/api/v2_soap/index/wsdl/1"
-              )
-
-            response = client.call(:login,
-                                   message: {
-                                     apiUser: magento_credential.username,
-                                     apikey: magento_credential.api_key})
-
-            session = response.body[:login_response][:login_return]
+            3.times do
+              if ["shopakira", "mftstamps"].include?(tenant)
+                client = Savon.client( wsdl: magento_credential.host+"/api/v2_soap/?wsdl=1")
+              else
+                client = Savon.client( wsdl: magento_credential.host+"/index.php/api/v2_soap/index/wsdl/1")
+              end
+              response = client.call(:login, message: {apiUser: magento_credential.username, apikey: magento_credential.api_key})
+              begin
+                session = response.body[:login_response][:login_return]
+                break
+              rescue
+              end
+            end
           end
-
           self.make_handle(magento_credential, {handle: client, session: session})
         end
 
