0110d2e2738e0a6fd3fc1dbba9a7fe1b7ba26b31
Kapil <kchoudhary006@gmail.com>
2018-04-03 14:44:52 +0530

    update changes with 24 * 7 rake

--- app/models/general_setting.rb
+++ app/models/general_setting.rb
@@ -21,7 +21,8 @@ class GeneralSetting < ActiveRecord::Base
                   :tracking_error_info_not_found, :custom_field_one,
                   :custom_field_two, :export_csv_email,
                   :show_primary_bin_loc_in_barcodeslip, :html_print,
-                  :time_zone, :auto_detect, :schedule_import_mode, :master_switch, :idle_timeout, :hex_barcode
+                  :time_zone, :auto_detect, :schedule_import_mode, :master_switch, :idle_timeout, :hex_barcode,
+                  :from_import, :to_import
   # validates_format_of :email_address_for_packer_notes, with: Devise.email_regexp, allow_blank: true
   validates :email_address_for_packer_notes, :format => { :with => /(\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})(,\s*([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,}))*\z)/i }, :allow_blank => true
   after_save :send_low_inventory_alert_email
---
+++ db/migrate/20180402091742_add_column_to_gen_setting.rb
@@ -0,0 +1,6 @@
+class AddColumnToGenSetting < ActiveRecord::Migration
+  def change
+  	add_column :general_settings, :from_import, :datetime, :default => '2000-01-01 00:00:00'
+    add_column :general_settings, :to_import, :datetime, :default => '2000-01-01 00:00:00'
+  end
+end
