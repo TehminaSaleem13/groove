71b87fafce05599cb1662df54a48131516a1c4a4
Kapil <kchoudhary006@gmail.com>
2018-04-05 17:02:15 +0530

    changes with magento

--- lib/groovepacker/stores/handlers/magento_handler.rb
+++ lib/groovepacker/stores/handlers/magento_handler.rb
@@ -7,13 +7,14 @@ module Groovepacker
           client = nil
           session = nil
           if !magento_credential.nil?
-            3.times do
+            5.times do
               if magento_credential.updated_patch
+                # client = Savon.client( wsdl: magento_credential.host+"/api/soap/?wsdl=1")
                 client = Savon.client( wsdl: magento_credential.host+"/api/v2_soap/?wsdl=1")
               else
                 client = Savon.client( wsdl: magento_credential.host+"/index.php/api/v2_soap/index/wsdl/1")
               end
-              response = client.call(:login, message: {apiUser: magento_credential.username, apikey: magento_credential.api_key})
+              response = client.call(:login, message: {apiUser: magento_credential.username, apikey: magento_credential.api_key}) rescue nil
               begin
                 session = response.body[:login_response][:login_return]
                 break
