f7b7d2d8991b50da8af510fa365b6b0c88dd693c
Kapil <kchoudhary006@gmail.com>
2018-06-05 11:31:08 +0530

    added env in delayedjob queue mailer

--- app/mailers/delayed_job_mailer.rb
+++ app/mailers/delayed_job_mailer.rb
@@ -3,7 +3,7 @@ class DelayedJobMailer < ActionMailer::Base
 
   def waiting_jobs(jobs)
     @jobs = jobs
-    subject = "Groovepacker - List of the jobs waiting for worker to be assigned"
+    subject = "Groovepacker [#{Rails.env}] - List of the jobs waiting for worker to be assigned"
     mail to: ENV["FAILED_IMPORT_NOTIFICATION_EMAILS"], subject: subject
   end
 
