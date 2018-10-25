03edf9d64a84194054a1df8076ea8a9d0644db9e
kapil <kchoudhary006@gmail.com>
2018-10-25 12:40:52 +0530

    allow RTS switch

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,7 @@ Groovepacks::Application.routes.draw do
   get '/tenants/update_import_mode' => 'tenants#update_import_mode'
   get '/tenants/update_fba' => 'tenants#update_fba'
   get '/tenants/update_api_call' => 'tenants#update_api_call'
+  get '/tenants/update_allow_rts' => 'tenants#update_allow_rts'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
