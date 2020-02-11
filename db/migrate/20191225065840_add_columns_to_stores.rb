e328073e727d15f26a4084940e897ccc7ab8b036
Kapil <kchoudhary006@gmail.com>
2020-02-11 14:45:35 +0530

    GROOV-2011 fix

--- db/migrate/20191225065840_add_columns_to_stores.rb
+++ db/migrate/20191225065840_add_columns_to_stores.rb
@@ -1,6 +1,6 @@
 class AddColumnsToStores < ActiveRecord::Migration
   def change
-  	add_column :stores, :quick_fix, :boolean, :default => false
+    add_column :stores, :quick_fix, :boolean, :default => false
     add_column :stores, :troubleshooter_option, :boolean, :default => false
   end
 end
