58e7e2ffa60d9840261cf75dfa54d8fbd8a8d57b
Kapil <kchoudhary006@gmail.com>
2020-07-07 20:23:12 +0530

    Shopify API Update

--- lib/groovepacker/shopify_ruby/client.rb
+++ lib/groovepacker/shopify_ruby/client.rb
@@ -2,18 +2,29 @@ module Groovepacker
   module ShopifyRuby
     class Client < Base
       def orders
-        page_index = 1
+        # page_index = 1
         combined_response = {}
         combined_response["orders"] = []
         last_import = shopify_credential.last_imported_at.utc.in_time_zone('Eastern Time (US & Canada)').to_datetime.to_s rescue (DateTime.now.utc.in_time_zone('Eastern Time (US & Canada)').to_datetime - 10.days).to_s
         fulfillment_status = shopify_credential.get_status
-        while page_index
-          query = {"page" => page_index, "updated_at_min" => last_import, "limit" => 250}.as_json
-          response = HTTParty.get("https://#{shopify_credential.shop_name}.myshopify.com/admin/orders?status=#{shopify_credential.shopify_status}&fulfillment_status=#{fulfillment_status}",query: query,headers: {"X-Shopify-Access-Token" => shopify_credential.access_token,"Content-Type" => "application/json","Accept" => "application/json"})
-          page_index = page_index + 1
+        # while page_index
+        #   query = {"page" => page_index, "updated_at_min" => last_import, "limit" => 250}.as_json
+        #   response = HTTParty.get("https://#{shopify_credential.shop_name}.myshopify.com/admin/orders?status=#{shopify_credential.shopify_status}&fulfillment_status=#{fulfillment_status}",query: query,headers: {"X-Shopify-Access-Token" => shopify_credential.access_token,"Content-Type" => "application/json","Accept" => "application/json"})
+        #   page_index = page_index + 1
+        #   combined_response["orders"] << response["orders"]
+        #   break if (response["orders"].blank? || response["orders"].count < 250)
+        # end
+
+        query = {"updated_at_min" => last_import, "limit" => 250}.as_json
+        response = HTTParty.get("https://#{shopify_credential.shop_name}.myshopify.com/admin/api/2019-10/orders?status=#{shopify_credential.shopify_status}&fulfillment_status=#{fulfillment_status}",query: query,headers: {"X-Shopify-Access-Token" => shopify_credential.access_token,"Content-Type" => "application/json","Accept" => "application/json"})
+        combined_response["orders"] << response["orders"]
+
+        while response.headers['link'].present? && (response.headers['link'].include? 'next')
+          new_link = response.headers['link'].split(',').last.split(';').first.strip.chop.reverse.chop.reverse
+          response = HTTParty.get(new_link, headers: {"X-Shopify-Access-Token" => shopify_credential.access_token,"Content-Type" => "application/json","Accept" => "application/json"})
           combined_response["orders"] << response["orders"]
-          break if (response["orders"].blank? || response["orders"].count < 250)
         end
+
         combined_response["orders"] = combined_response["orders"].flatten
         combined_response
       end
@@ -22,21 +33,43 @@ module Groovepacker
         page_index = 1
         combined_response = {}
         combined_response["products"] = []
-        while page_index
-          puts "======================Fetching Page #{page_index}======================"
-          query_opts = {"page" => page_index, "limit" => 100}.as_json
-          add_url = product_import_type == 'new_updated' && shopify_credential.product_last_import ? formatted_product_import_date(shopify_credential.product_last_import) : ''
-          response = HTTParty.get("https://#{shopify_credential.shop_name}.myshopify.com/admin/products#{add_url}", 
-                                    query: query_opts, 
-                                    headers: {
-                                    "X-Shopify-Access-Token" => shopify_credential.access_token,
-                                    "Content-Type" => "application/json",
-                                    "Accept" => "application/json"
-                                  })
+        # while page_index
+        #   puts "======================Fetching Page #{page_index}======================"
+        #   query_opts = {"page" => page_index, "limit" => 100}.as_json
+        #   add_url = product_import_type == 'new_updated' && shopify_credential.product_last_import ? formatted_product_import_date(shopify_credential.product_last_import) : ''
+        #   response = HTTParty.get("https://#{shopify_credential.shop_name}.myshopify.com/admin/products#{add_url}", 
+        #                             query: query_opts, 
+        #                             headers: {
+        #                             "X-Shopify-Access-Token" => shopify_credential.access_token,
+        #                             "Content-Type" => "application/json",
+        #                             "Accept" => "application/json"
+        #                           })
+        #   page_index = page_index + 1
+        #   combined_response["products"] << response["products"]
+        #   combined_response["products"] = combined_response["products"].flatten
+        #   break if (response["products"].blank? || response["products"].count < 100)
+        # end
+        puts "======================Fetching Page #{page_index}======================"
+        query_opts = { "limit" => 100 }.as_json
+        add_url = product_import_type == 'new_updated' && shopify_credential.product_last_import ? "?updated_at_min=#{shopify_credential.product_last_import.strftime("%Y-%m-%d %H:%M:%S").gsub(' ', '%20')}" : ''
+
+        response = HTTParty.get("https://#{shopify_credential.shop_name}.myshopify.com/admin/api/2019-10/products#{add_url}", 
+                                  query: query_opts, 
+                                  headers: {
+                                  "X-Shopify-Access-Token" => shopify_credential.access_token,
+                                  "Content-Type" => "application/json",
+                                  "Accept" => "application/json"
+                                })
+        combined_response["products"] << response["products"]
+        combined_response["products"] = combined_response["products"].flatten
+
+        while response.headers['link'].present? && (response.headers['link'].include? 'next')
           page_index = page_index + 1
+          puts "======================Fetching Page #{page_index}======================"
+          new_link = response.headers['link'].split(',').last.split(';').first.strip.chop.reverse.chop.reverse
+          response = HTTParty.get(new_link, headers: {"X-Shopify-Access-Token" => shopify_credential.access_token,"Content-Type" => "application/json","Accept" => "application/json"})
           combined_response["products"] << response["products"]
           combined_response["products"] = combined_response["products"].flatten
-          break if (response["products"].blank? || response["products"].count < 100)
         end
         combined_response
       end
