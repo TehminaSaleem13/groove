2d6accb9cb43dcfc29efb1847ee7ce825474d9a0
Kapil <kchoudhary006@gmail.com>
2020-07-21 10:18:30 +0530

    GROOV-2197

--- lib/groovepacker/stores/stores.rb
+++ lib/groovepacker/stores/stores.rb
@@ -195,8 +195,8 @@ module Groovepacker
 				params = @params
 		    @shopify = ShopifyCredential.find_by_store_id(@store.id)
 		    begin
-		      params[:shop_name] = nil if params[:shop_name] == 'null'
-		      params[:shop_name] = params[:shop_name].gsub(/[,()'".]+\z/,'') if params[:shop_name] != 'null'
+					params[:shop_name] = nil if params[:shop_name] == 'null'
+		      params[:shop_name] = params[:shop_name].gsub(/[,()'".]+\z/,'') if params[:shop_name] != 'null' && params[:shop_name].present?
 		      if @shopify.nil?
 		        @store.shopify_credential = ShopifyCredential.new(shop_name: params[:shop_name])
 		        new_record = true
