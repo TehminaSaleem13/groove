bfb4bf5d77c17ef1f89c675d4b2633ffc16c97b2
kapil <kchoudhary006@gmail.com>
2021-06-04 11:23:41 +0530

    DJ Update

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -4,12 +4,12 @@ Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_run_time = 240.minutes
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
 
-Delayed::Backend::ActiveRecord::Job.class_eval do
-  if ::ActiveRecord::VERSION::MAJOR < 4 || defined?(::ActiveRecord::MassAssignmentSecurity)
-    # attr_accessible :priority, :run_at, :queue, :payload_object,
-    #                 :failed_at, :locked_at, :locked_by, :handler,
-    #                 :last_error, :attempts
-  end
-end
+# Delayed::Backend::ActiveRecord::Job.class_eval do
+#   if ::ActiveRecord::VERSION::MAJOR < 4 || defined?(::ActiveRecord::MassAssignmentSecurity)
+#     # attr_accessible :priority, :run_at, :queue, :payload_object,
+#     #                 :failed_at, :locked_at, :locked_by, :handler,
+#     #                 :last_error, :attempts
+#   end
+# end
 
-Delayed::Backend::ActiveRecord.configuration.reserve_sql_strategy = :default_sql
+# Delayed::Backend::ActiveRecord.configuration.reserve_sql_strategy = :default_sql
