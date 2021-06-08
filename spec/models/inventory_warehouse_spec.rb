2bd74d5022e569d9d2e21726cb5e2baaa6dc6caa
kapil <kchoudhary006@gmail.com>
2021-06-08 16:58:31 +0530

    Rspec

--- spec/models/inventory_warehouse_spec.rb
+++ spec/models/inventory_warehouse_spec.rb
@@ -35,5 +35,14 @@ RSpec.describe InventoryWarehouse, type: :model do
       inventory_warehouse = InventoryWarehouse.new(name: 'Primary_inventory')
       inventory_warehouse.should be_valid
     end
+
+    it 'Check Update Fields' do
+      user = User.create(username: "kapil2", active: true, other: nil, name: "kapil2", confirmation_code: "8460", role_id: 4, view_dashboard: "packer_dashboard", is_deleted: false, reset_token: nil, email: "kapiltest@yomail.com", last_name: nil, custom_field_one: nil, custom_field_two: nil, dashboard_switch: false,warehouse_postcode: nil)
+      inv_wh = FactoryBot.create(:inventory_warehouse, name: 'csv_inventory_warehouse', is_default: 1)
+      user_inventory_permissions = UserInventoryPermission.new(user_id: user.id, inventory_warehouse_id: inv_wh.id, see: false, edit: true)
+      user_inventory_permissions.run_callbacks :save 
+      user_inventory_permissions.check_update_fields
+      expect(user_inventory_permissions.see).to eq(true)
+    end
   end
 end
