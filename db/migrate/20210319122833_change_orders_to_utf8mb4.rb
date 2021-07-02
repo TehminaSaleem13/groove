707e5854ed296b7c08006e83f907f7a48e21c785
kapil <kchoudhary006@gmail.com>
2021-07-02 12:15:01 +0530

    Utf migration fix

--- db/migrate/20210319122833_change_orders_to_utf8mb4.rb
+++ db/migrate/20210319122833_change_orders_to_utf8mb4.rb
@@ -1,11 +1,5 @@
 class ChangeOrdersToUtf8mb4 < ActiveRecord::Migration[5.1]
-  def up
-    execute "ALTER TABLE orders CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
-    execute "ALTER TABLE orders MODIFY customer_comments VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"
-  end
-  
-  def down
-    execute "ALTER TABLE orders CONVERT TO CHARACTER SET utf8 COLLATE utf8_bin"
-    execute "ALTER TABLE orders MODIFY customer_comments VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin"
+  def change
+    execute "ALTER TABLE orders MODIFY customer_comments VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
   end
 end
