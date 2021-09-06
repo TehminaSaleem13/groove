fc82d822423f227aaca5acc5fd1b27e27e279f08
kapil <kchoudhary006@gmail.com>
2021-09-06 14:44:20 +0530

    Revert "Expo performance minor fic"
    This reverts commit 0f7384949c9d2bdf9ad6a9ddbc54658bf651a25e.

--- app/services/expo/generate_agor.rb
+++ app/services/expo/generate_agor.rb
@@ -24,7 +24,7 @@ module Expo
     end
 
     def filter_order_data
-      @order.slice("id", "increment_id", "order_placed_time", "created_at", "updated_at", "notes_internal", "notes_toPacker", "notes_fromPacker", "status", "tracking_num", "packing_user_id", "notes_from_buyer", "note_confirmation", "custom_field_one", "custom_field_two", :unscanned_items, :scanned_items, :multi_shipments, :activities, :count, :remaining_items_count)
+      @order.slice("id", "increment_id", "order_placed_time", "created_at", "updated_at", "notes_internal", "notes_toPacker", "notes_fromPacker", "status", "tracking_num", "packing_user_id", "notes_from_buyer", "note_confirmation", "custom_field_one", "custom_field_two", :unscanned_items, :scanned_items, :multi_shipments, :activities)
     end
 
     def filter_order_info
@@ -36,14 +36,14 @@ module Expo
       @scanned_items.each do |item|
         barcodes = {}
         images = {}
-        scanned_items << item.slice("name", "instruction", "order_item_id", "confirmation", "images", "sku","packing_placement", "barcodes", "product_id", "location", "location2", "location3", "skippable", "record_serial", "second_record_serial", "click_scan_enabled", "type_scan_enabled", "order_item_id", "custom_product_1", "custom_product_2", "custom_product_3", "custom_product_display_1", "custom_product_display_2", "custom_product_display_3", "partially_scanned", "updated_at", "product_type", "qty_remaining", "total_qty", "scanned_qty", "child_items") 
+        scanned_items << item.slice("name", "instruction", "confirmation", "images", "sku","packing_placement", "barcodes", "product_id", "location", "location2", "location3", "skippable", "record_serial", "second_record_serial", "click_scan_enabled", "type_scan_enabled", "order_item_id", "custom_product_1", "custom_product_2", "custom_product_3", "custom_product_display_1", "custom_product_display_2", "custom_product_display_3", "partially_scanned", "updated_at", "product_type", "qty_remaining", "total_qty", "scanned_qty", "child_items") 
         scanned_items.last["images"] = item["images"].map(&:as_json).collect{ |img| img.slice("id", "product_id", "image", "created_at", "updated_at")}
         scanned_items.last["barcodes"] = item["barcodes"].map(&:as_json).collect{ |barcode| barcode.slice("id", "product_id", "barcode", "created_at", "updated_at", "packing_count", "is_multipack_barcode")}
         
         scanned_child_items = []
         unless item["child_items"].nil?
           item["child_items"].each do |child_item|
-            scanned_child_items << child_item.slice("name", "instruction", "confirmation", "order_item_id", "images", "sku", "barcodes", "product_id", "location", "location2", "location3", "skippable", "record_serial", "second_record_serial", "click_scan_enabled", "type_scan_enabled", "order_item_id", "custom_product_1", "custom_product_2", "custom_product_3", "custom_product_display_1", "custom_product_display_2", "custom_product_display_3", "partially_scanned", "updated_at", "product_type", "qty_remaining", "total_qty", "scanned_qty")
+            scanned_child_items << child_item.slice("name", "instruction", "confirmation", "images", "sku", "barcodes", "product_id", "location", "location2", "location3", "skippable", "record_serial", "second_record_serial", "click_scan_enabled", "type_scan_enabled", "order_item_id", "custom_product_1", "custom_product_2", "custom_product_3", "custom_product_display_1", "custom_product_display_2", "custom_product_display_3", "partially_scanned", "updated_at", "product_type", "qty_remaining", "total_qty", "scanned_qty")
             scanned_child_items.last["images"] = child_item["images"].map(&:as_json).collect{ |img| img.slice("id", "product_id", "image", "created_at", "updated_at")}
             scanned_child_items.last["barcodes"] = child_item["barcodes"].map(&:as_json).collect{ |img| img.slice("id", "product_id", "barcode", "created_at", "updated_at", "packing_count", "is_multipack_barcode")}
           end
@@ -56,14 +56,14 @@ module Expo
     def filter_unscanned_items
       unscanned_items = []
       @unscanned_items.each do |item|
-        unscanned_items << item.slice("name", "instruction", "order_item_id", "confirmation", "images", "sku","packing_placement", "barcodes", "product_id", "location", "location2", "location3", "skippable", "record_serial", "second_record_serial", "click_scan_enabled", "type_scan_enabled", "order_item_id", "custom_product_1", "custom_product_2", "custom_product_3", "custom_product_display_1", "custom_product_display_2", "custom_product_display_3", "partially_scanned", "updated_at", "product_type", "qty_remaining", "total_qty", "scanned_qty", "child_items") 
+        unscanned_items << item.slice("name", "instruction", "confirmation", "images", "sku","packing_placement", "barcodes", "product_id", "location", "location2", "location3", "skippable", "record_serial", "second_record_serial", "click_scan_enabled", "type_scan_enabled", "order_item_id", "custom_product_1", "custom_product_2", "custom_product_3", "custom_product_display_1", "custom_product_display_2", "custom_product_display_3", "partially_scanned", "updated_at", "product_type", "qty_remaining", "total_qty", "scanned_qty", "child_items") 
         unscanned_items.last["images"] = item["images"].map(&:as_json).collect{ |img| img.slice("id", "product_id", "image", "created_at", "updated_at")}
         unscanned_items.last["barcodes"] = item["barcodes"].map(&:as_json).collect{ |barcode| barcode.slice("id", "product_id", "barcode", "created_at", "updated_at", "packing_count", "is_multipack_barcode")}
         
         unscanned_child_items = []
         unless item["child_items"].nil?
           item["child_items"].each do |child_item|
-            unscanned_child_items << child_item.slice("name", "instruction", "confirmation", "order_item_id", "images", "sku", "barcodes", "product_id", "location", "location2", "location3", "skippable", "record_serial", "second_record_serial", "click_scan_enabled", "type_scan_enabled", "order_item_id", "custom_product_1", "custom_product_2", "custom_product_3", "custom_product_display_1", "custom_product_display_2", "custom_product_display_3", "partially_scanned", "updated_at", "product_type", "qty_remaining", "total_qty", "scanned_qty")
+            unscanned_child_items << child_item.slice("name", "instruction", "confirmation", "images", "sku", "barcodes", "product_id", "location", "location2", "location3", "skippable", "record_serial", "second_record_serial", "click_scan_enabled", "type_scan_enabled", "order_item_id", "custom_product_1", "custom_product_2", "custom_product_3", "custom_product_display_1", "custom_product_display_2", "custom_product_display_3", "partially_scanned", "updated_at", "product_type", "qty_remaining", "total_qty", "scanned_qty")
             unscanned_child_items.last["images"] = child_item["images"].map(&:as_json).collect{ |img| img.slice("id", "product_id", "image", "created_at", "updated_at")}
             unscanned_child_items.last["barcodes"] = child_item["barcodes"].map(&:as_json).collect{ |img| img.slice("id", "product_id", "barcode", "created_at", "updated_at", "packing_count", "is_multipack_barcode")}
           end
