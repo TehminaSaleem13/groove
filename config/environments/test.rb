3470b832b7f40b8b21cf344b9809a1175a409610
Hampton Catlin <hcatlin@gmail.com>
2021-10-08 10:06:11 -0400

    removing redis password from test runner

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,8 +31,7 @@ Groovepacks::Application.configure do
   config.action_mailer.perform_deliveries = false
   config.action_mailer.delivery_method = :smtp
 
-  $redis = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'].to_i, driver: :hiredis,
-                     :password => ENV['REDIS_PASSWORD'])
+  $redis = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'].to_i, driver: :hiredis)
 
   config.cache_store = :redis_store, $redis.as_json['options'].merge(db: 15) # :memory_store, { size: 64.megabytes } #
 
