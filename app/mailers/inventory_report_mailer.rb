48f647dce2058b4421f75004e632d103988306e3
kapil <kchoudhary006@gmail.com>
2021-11-26 14:51:01 +0530

    Refactor Code

--- app/mailers/inventory_report_mailer.rb
+++ app/mailers/inventory_report_mailer.rb
@@ -6,47 +6,13 @@ class InventoryReportMailer < ActionMailer::Base
   def manual_inventory_report(id, tenant)
     Apartment::Tenant.switch! tenant
     @product_inv_setting = InventoryReportsSetting.last
-    @product_inv_setting.start_time ||= 7.days.ago
-    @product_inv_setting.end_time ||= Time.current
     selected_reports = ProductInventoryReport.where(id: id, type: [false, nil])
     return if selected_reports.blank?
-
-    start_time = @product_inv_setting&.start_time&.strftime('%m-%d-%Y')
-    end_time = @product_inv_setting&.end_time&.strftime('%m-%d-%Y')
-
-    days = (@product_inv_setting&.end_time&.end_of_day - @product_inv_setting&.start_time&.beginning_of_day).to_i rescue 7
-
-    headers = ['DATE RANGE', 'SKU', 'PRODUCT NAME', 'SELECTED RANGE QTY SCANNED', 'PAST 14D QTY SCANNED', 'PAST 30D QTY SCANNED', 'PAST 60D QTY SCANNED', 'PAST 90D QTY SCANNED', 'PROJ DAYS REMAINING BASED ON SELECTED RANGE', 'PROJ DAYS REMAINING BASED ON 14D', 'PROJ DAYS REMAINING BASED ON 30D', 'PROJ DAYS REMAINING BASED ON 60D', 'PROJ DAYS REMAINING BASED ON 90D', 'CURRENT AVAILABLE', 'CURRENT QOH', 'CATEGORY', 'LOCATION1', 'LOCATION2', 'LOCATION3', 'RESTOCK LEAD TIME']
 
     selected_reports.each do |report|
       products = get_products(report)
       file_name = "inventory_report_#{Time.now.strftime('%y%m%d_%H%M%S')}.csv"
-      data = CSV.generate(headers: true) do |csv|
-        csv << headers
-        products.each do |pro|
-
-          pro_orders = pro.order_items.map(&:order).reject(&:blank?)
-          inv = pro.product_inventory_warehousess
-          restock_lead_time = pro.restock_lead_time || 0
-
-          orders = Order.where('id IN (?) and scanned_on >= ? and scanned_on <= ?', pro_orders.map(&:id), @product_inv_setting.try(:start_time).try(:beginning_of_day), @product_inv_setting.try(:end_time).try(:end_of_day))
-          orders_14 = Order.where('id IN (?) and scanned_on >= ?', pro_orders.map(&:id), Time.now - 14.days) rescue []
-          orders_30 = Order.where('id IN (?) and scanned_on >= ?', pro_orders.map(&:id), Time.now - 30.days) rescue []
-          orders_60 = Order.where('id IN (?) and scanned_on >= ?', pro_orders.map(&:id), Time.now - 60.days) rescue []
-          orders_90 = Order.where('id IN (?) and scanned_on >= ?', pro_orders.map(&:id), Time.now - 90.days) rescue []
-
-          available_inv = inv.map(&:available_inv).sum
-          quantity_on_hand = inv.map(&:quantity_on_hand).sum
-
-          projected_days_remaining = ((quantity_on_hand.to_f / (orders.count.to_f / days)) - restock_lead_time) rescue 0
-          projected_days_remaining_14 = ((quantity_on_hand.to_f / (orders_14.count.to_f / 14)) - restock_lead_time) rescue 0
-          projected_days_remaining_30 = ((quantity_on_hand.to_f / (orders_30.count.to_f / 30)) - restock_lead_time) rescue 0
-          projected_days_remaining_60 = ((quantity_on_hand.to_f / (orders_60.count.to_f / 60)) - restock_lead_time) rescue 0
-          projected_days_remaining_90 = ((quantity_on_hand.to_f / (orders_90.count.to_f / 90)) - restock_lead_time) rescue 0
-
-          csv << ["#{start_time} to #{end_time}", pro.primary_sku.to_s, pro.name.tr(',', ' ').to_s, orders&.count.to_s, orders_14&.count.to_s, orders_30&.count.to_s, orders_60&.count.to_s, orders_90&.count.to_s, projected_days_remaining.round(1), projected_days_remaining_14.round(1), projected_days_remaining_30.round(1), projected_days_remaining_60.round(1), projected_days_remaining_90.round(1), available_inv, quantity_on_hand, pro.product_cats[0].try(:category), inv[0].try(:location_primary), inv[0].try(:location_secondary), inv[0].try(:location_tertiary), pro.restock_lead_time]
-        end
-      end
+      data = InventoryReport::InvProjection.new(@product_inv_setting, products).get_data
       attachments[file_name] = { mime_type: 'text/csv', content: data }
     end
     subject = 'Inventory Projection Report [' + tenant + ']'
---
+++ app/services/inventory_report/inv_projection.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+module InventoryReport
+  class InvProjection
+    attr_accessor :products
+
+    def initialize(product_inv_setting, products)
+      @product_inv_setting = product_inv_setting
+      @products = products
+    end
+
+    def get_data
+      set_start_end_time_if_not_set
+
+      CSV.generate(headers: true) do |csv|
+        csv << headers
+        products.each do |pro|
+          pro_orders = Order.joins(order_items: :product).where(order_items: { product: pro })
+          inv = pro.product_inventory_warehousess
+
+          orders_count = pro_orders.where('scanned_on >= ? and scanned_on <= ?', @product_inv_setting.start_time.beginning_of_day, @product_inv_setting.end_time.end_of_day).count
+
+          available_inv = inv.map(&:available_inv).sum
+          quantity_on_hand = inv.map(&:quantity_on_hand).sum
+
+          row = ["#{start_time} to #{end_time}", pro.primary_sku.to_s, pro.name.tr(',', ' ').to_s, orders_count]
+          row << get_orders_count(pro_orders)
+          row << get_projected_days_remaining(quantity_on_hand, restock_lead_time(pro), pro_orders)
+          row << [available_inv, quantity_on_hand, pro.product_cats[0]&.category, inv[0]&.location_primary, inv[0]&.location_secondary, inv[0]&.location_tertiary, restock_lead_time(pro)]
+
+          csv << row.flatten
+        end
+      end
+    end
+
+    private
+
+    def set_start_end_time_if_not_set
+      @product_inv_setting.start_time ||= 7.days.ago
+      @product_inv_setting.end_time ||= Time.current
+    end
+
+    def get_projected_days_remaining(qoh, rlt, pro_orders)
+      orders_count = get_orders_count(pro_orders)
+      projected_days_remaining = [pro_orders.count.zero? ? 0 : ((qoh.to_f / (pro_orders.count.to_f / days)) - rlt.to_f)]
+      [14, 30, 60, 90].each_with_index do |day_count, i|
+        projected_days_remaining << (orders_count[i].zero? ? 0 : ((qoh.to_f / (orders_count[i].to_f / day_count)) - rlt.to_f))
+      end
+      projected_days_remaining
+    end
+
+    def get_orders_count(pro_orders)
+      counts = []
+      [14, 30, 60, 90].each do |day_count|
+        counts << pro_orders.where('scanned_on >= ?', Time.now - day_count.days).count
+      end
+      counts
+    end
+
+    def restock_lead_time(product)
+      product.restock_lead_time || 0
+    end
+
+    def start_time
+      @product_inv_setting&.start_time&.strftime('%m-%d-%Y')
+    end
+
+    def end_time
+      @product_inv_setting&.end_time&.strftime('%m-%d-%Y')
+    end
+
+    def headers
+      ['DATE RANGE', 'SKU', 'PRODUCT NAME', 'SELECTED RANGE QTY SCANNED', 'PAST 14D QTY SCANNED', 'PAST 30D QTY SCANNED', 'PAST 60D QTY SCANNED', 'PAST 90D QTY SCANNED', 'PROJ DAYS REMAINING BASED ON SELECTED RANGE', 'PROJ DAYS REMAINING BASED ON 14D', 'PROJ DAYS REMAINING BASED ON 30D', 'PROJ DAYS REMAINING BASED ON 60D', 'PROJ DAYS REMAINING BASED ON 90D', 'CURRENT AVAILABLE', 'CURRENT QOH', 'CATEGORY', 'LOCATION1', 'LOCATION2', 'LOCATION3', 'RESTOCK LEAD TIME']
+    end
+
+    def days
+      (@product_inv_setting&.end_time&.end_of_day - @product_inv_setting&.start_time&.beginning_of_day).to_i
+    rescue StandardError
+      7
+    end
+  end
+end
