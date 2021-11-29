5de03332f37562832104d671f3e412358cabee7d
kapil <kchoudhary006@gmail.com>
2021-11-29 20:20:02 +0530

    Refactor Inventory Report Generation

--- app/mailers/inventory_report_mailer.rb
+++ app/mailers/inventory_report_mailer.rb
@@ -29,36 +29,10 @@ class InventoryReportMailer < ActionMailer::Base
       reports = report.present? ? [report] : ProductInventoryReport.where(scheduled: true)
     end
     @product_inv_setting = InventoryReportsSetting.last
-    headers = %w[DATE_FOR_DAILY_TOTAL SKU PRODUCT_NAME DAILY_SKU_QTY]
     reports.each do |report|
       file_name = "sku_per_day_report_#{Time.now.strftime('%y%m%d_%H%M%S')}.csv"
       products = get_products(report)
-      data = CSV.generate(headers: true) do |csv|
-        csv << headers
-        days = get_days(flag)
-        days.times do |i|
-          products.each do |pro|
-            orders = pro.order_items.map(&:order)
-            orders = [] if orders.blank?
-            if flag == true
-              orders = begin
-                          Order.where('id IN (?) and scanned_on >= ? and scanned_on <= ?', orders.map(&:id), (@product_inv_setting.start_time + i.to_s.to_i.days).beginning_of_day, (@product_inv_setting.start_time + i.to_s.to_i.days).end_of_day)
-                        rescue
-                          []
-                        end
-              date = (@product_inv_setting.start_time + i.to_s.to_i.days).strftime('%m/%d/%y')
-            else
-              orders = begin
-                          Order.where('id IN (?) and scanned_on >= ? and scanned_on <= ?', orders.map(&:id), (DateTime.now - i.to_s.to_i.days).beginning_of_day, (DateTime.now - i.to_s.to_i.days).end_of_day)
-                        rescue
-                          []
-                        end
-              date = (DateTime.now.beginning_of_day - i.to_s.to_i.days).strftime('%m/%d/%y')
-            end
-            csv << [date, pro.primary_sku, pro.name.tr(',', ' '), orders.count]
-          end
-        end
-      end
+      data = InventoryReport::SkuPerDay.new(@product_inv_setting, products, flag).get_data
       attachments[file_name] = { mime_type: 'text/csv', content: data }
       # attachments[file_name] = File.read("public/#{file_name}")
     end
@@ -66,18 +40,6 @@ class InventoryReportMailer < ActionMailer::Base
     email = @product_inv_setting.report_email
     # mail to: email, subject: subject
     mail(to: email, subject: subject)
-  end
-
-  def get_days(flag)
-    if flag == true
-      @product_inv_setting.start_time ||= 7.days.ago
-      @product_inv_setting.end_time ||= Time.current
-      days = (@product_inv_setting.end_time.to_date - @product_inv_setting.start_time.to_date).to_i
-      days = 0 if days < 0
-    else
-      days = @product_inv_setting.report_days_option
-     end
-    days
   end
 
   def get_products(report)
---
+++ app/services/inventory_report/sku_per_day.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module InventoryReport
+  class SkuPerDay
+    attr_accessor :products, :flag
+
+    def initialize(product_inv_setting, products, flag)
+      @product_inv_setting = product_inv_setting
+      @products = products
+      @flag = flag
+    end
+
+    def get_data
+      CSV.generate(headers: true) do |csv|
+        csv << headers
+        days.times do |i|
+          products.each do |pro|
+            pro_orders = Order.joins(order_items: :product).where(order_items: { product: pro })
+            if flag == true
+              orders = begin
+                          pro_orders.where('scanned_on >= ? and scanned_on <= ?', (@product_inv_setting.start_time + i.to_s.to_i.days).beginning_of_day, (@product_inv_setting.start_time + i.to_s.to_i.days).end_of_day)
+                        rescue
+                          []
+                        end
+              date = (@product_inv_setting.start_time + i.to_s.to_i.days).strftime('%m/%d/%y')
+            else
+              orders = begin
+                          pro_orders.where('scanned_on >= ? and scanned_on <= ?', (DateTime.now - i.to_s.to_i.days).beginning_of_day, (DateTime.now - i.to_s.to_i.days).end_of_day)
+                        rescue
+                          []
+                        end
+              date = (DateTime.now.beginning_of_day - i.to_s.to_i.days).strftime('%m/%d/%y')
+            end
+            csv << [date, pro.primary_sku, pro.name.tr(',', ' '), orders.count]
+          end
+        end
+      end
+    end
+
+    private
+
+    def headers
+      ['DATE FOR DAILY TOTAL', 'SKU', 'PRODUCT NAME', 'DAILY SKU QTY']
+    end
+
+    def days
+      if flag == true
+        @product_inv_setting.start_time ||= 7.days.ago
+        @product_inv_setting.end_time ||= Time.current
+        days = ((@product_inv_setting&.end_time&.end_of_day - @product_inv_setting&.start_time&.beginning_of_day).to_f / (24 * 60 * 60)).ceil
+        days = 0 if days < 0
+      else
+        days = @product_inv_setting.report_days_option
+      end
+      days
+    end
+  end
+end
