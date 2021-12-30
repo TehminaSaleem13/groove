61c17b45cc6716169aaf81393989c9d3d44c5d48
kapil <kchoudhary006@gmail.com>
2021-12-30 11:51:01 +0530

    Rspec Fix

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,14 +5,8 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
   before(:each) do
     Groovepacker::SeedTenant.new.seed
-    generalsetting = GeneralSetting.all.first
-    generalsetting.update_columns(inventory_tracking: true, hold_orders_due_to_inventory: true)
     user_role = FactoryBot.create(:role, name: 'csv_spec_tester_role', add_edit_stores: true, import_products: true)
     @user = FactoryBot.create(:user, name: 'CSV Tester', username: 'csv_spec_tester', role: user_role)
-    FactoryBot.create(:access_restriction)
-    inv_wh = FactoryBot.create(:inventory_warehouse, name: 'csv_inventory_warehouse')
-    @store = FactoryBot.create(:store, name: 'csv_store', store_type: 'CSV', inventory_warehouse: inv_wh, status: true)
-    FactoryBot.create(:csv_mapping, store_id: @store.id)
   end
 
   describe 'Import Status' do
