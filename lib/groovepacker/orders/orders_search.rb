bfa9fae9e339869b9a52b3f2518af91fdbff7efb
Hasan Nadeem <hey.hasannadeem@gmail.com>
2022-06-21 07:37:38 +0000

    Merged in GROOV-2574 exact order search (#634)
    Groov-2574 exact order search
    
    * Groov-2574 exact order search

--- lib/groovepacker/orders/orders_search.rb
+++ lib/groovepacker/orders/orders_search.rb
@@ -10,7 +10,8 @@ module Groovepacker
         sort_key = set_final_sort_key(sort_order, sort_key)
         sort_key = "store_type" if sort_key == "store_name"
         search = ActiveRecord::Base.connection.quote('%'+@params[:search]+'%')
-        base_query = get_base_query(search, sort_key, sort_order)
+        exact_search = ActiveRecord::Base.connection.quote(@params[:search])
+        base_query = get_base_query(search, exact_search, sort_key, sort_order)
         query_add = get_query_limit_offset(limit, offset)
         result_rows = Order.find_by_sql(base_query + query_add)
         ActiveRecord::Associations::Preloader.new.preload(result_rows, [:order_items, :store, :order_tags])
@@ -18,12 +19,12 @@ module Groovepacker
       end
 
       private
-      def get_base_query(search, sort_key, sort_order)
+      def get_base_query(search, exact_search, sort_key, sort_order)
         if @params["product_search_toggle"] == "true"
-          base_query = "Select orders.*, sum(order_items.qty) AS itemslength from orders LEFT JOIN stores ON (orders.store_id = stores.id) LEFT JOIN order_items ON (order_items.order_id = orders.id) LEFT JOIN products ON (products.id = order_items.product_id) LEFT JOIN product_skus ON (products.id = product_skus.product_id) LEFT JOIN product_barcodes ON (products.id = product_barcodes.product_id) WHERE increment_id like #{search} OR tracking_num LIKE '#{'%' + @params[:search]}' OR non_hyphen_increment_id like #{search} OR email like #{search} OR CONCAT(IFNULL(firstname,''),' ',IFNULL(lastname,'')) like #{search} OR postcode like #{search} OR custom_field_one like #{search} OR custom_field_two like #{search} OR notes_internal like #{search} OR tags like #{search} OR products.name like #{search} OR product_skus.sku like #{search} OR product_barcodes.barcode like #{search} OR stores.name like #{search} GROUP BY orders.id Order BY #{sort_key} #{sort_order}"
+          base_query = "Select orders.*, sum(order_items.qty) AS itemslength from orders LEFT JOIN stores ON (orders.store_id = stores.id) LEFT JOIN order_items ON (order_items.order_id = orders.id) LEFT JOIN products ON (products.id = order_items.product_id) LEFT JOIN product_skus ON (products.id = product_skus.product_id) LEFT JOIN product_barcodes ON (products.id = product_barcodes.product_id) WHERE if ((select count(*) from orders where increment_id = #{exact_search})>0,increment_id = #{exact_search} ,increment_id like #{search} OR tracking_num LIKE '#{'%' + @params[:search]}' OR non_hyphen_increment_id like #{search} OR email like #{search} OR CONCAT(IFNULL(firstname,''),' ',IFNULL(lastname,'')) like #{search} OR postcode like #{search} OR custom_field_one like #{search} OR custom_field_two like #{search} OR notes_internal like #{search} OR tags like #{search} OR products.name like #{search} OR product_skus.sku like #{search} OR product_barcodes.barcode like #{search} OR stores.name like #{search} ) GROUP BY orders.id Order BY #{sort_key} #{sort_order}"
         else
           #todo: include sku and storename in search as well in future.
-          base_query = "Select orders.*, sum(order_items.qty) AS itemslength from orders LEFT JOIN stores ON (orders.store_id = stores.id) LEFT JOIN order_items ON (order_items.order_id = orders.id) WHERE increment_id like #{search} OR tracking_num LIKE '#{'%' + @params[:search]}' OR non_hyphen_increment_id like #{search} OR email like #{search} OR CONCAT(IFNULL(firstname,''),' ',IFNULL(lastname,'')) like #{search} OR postcode like #{search} OR custom_field_one like #{search} OR custom_field_two like #{search} OR notes_internal like #{search} OR tags like #{search} OR stores.name like #{search} GROUP BY orders.id Order BY #{sort_key} #{sort_order}"
+          base_query = "Select orders.*, sum(order_items.qty) AS itemslength from orders LEFT JOIN stores ON (orders.store_id = stores.id) LEFT JOIN order_items ON (order_items.order_id = orders.id) WHERE if ((select count(*) from orders where increment_id = #{exact_search})>0,increment_id = #{exact_search},increment_id like #{search} OR tracking_num LIKE '#{'%' + @params[:search]}' OR non_hyphen_increment_id like #{search} OR email like #{search} OR CONCAT(IFNULL(firstname,''),' ',IFNULL(lastname,'')) like #{search} OR postcode like #{search} OR custom_field_one like #{search} OR custom_field_two like #{search} OR notes_internal like #{search} OR tags like #{search} OR stores.name like #{search}) GROUP BY orders.id Order BY #{sort_key} #{sort_order}"
         end
         base_query
       end
