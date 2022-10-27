c205cdbef3c7b5dfd5040a84c858a0cd701014d7
kapil <kchoudhary006@gmail.com>
2022-10-27 15:32:33 +0530

    Minor Fix

--- db/migrate/20221027092600_change_product_name_to_utf8mb4.rb
+++ db/migrate/20221027092600_change_product_name_to_utf8mb4.rb
@@ -1,5 +1,5 @@
 class ChangeProductNameToUtf8mb4 < ActiveRecord::Migration[5.1]
   def change
-    execute "ALTER TABLE products MODIFY name VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    execute "ALTER TABLE products MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
   end
 end
