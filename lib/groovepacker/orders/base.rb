ba2ad061518adc3990473cb90edc0f084722cff2
kapil <kchoudhary006@gmail.com>
2023-03-02 00:56:56 +0530

    Minor Fix

--- lib/groovepacker/orders/base.rb
+++ lib/groovepacker/orders/base.rb
@@ -10,7 +10,7 @@ module Groovepacker
         @result = params[:result]
         @params = params[:params_attrs]
         @current_user = params[:current_user]
-        @platform = params[:params_attrs][:on_ex]
+        @platform = params[:params_attrs].try(:[], :on_ex)
       end
 
       private
