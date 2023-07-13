f8f823a6027b791e6d4941699f5369cae712f2d5
kapil <kchoudhary006@gmail.com>
2023-07-13 14:16:17 +0530

    Update VARCHAR Limit

--- db/migrate/20230713072029_change_ahoy_event_to_utf8mb4.rb
+++ db/migrate/20230713072029_change_ahoy_event_to_utf8mb4.rb
@@ -1,5 +1,5 @@
 class ChangeAhoyEventToUtf8mb4 < ActiveRecord::Migration[5.1]
   def change
-    execute "ALTER TABLE ahoy_events MODIFY properties VARCHAR(10000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    execute "ALTER TABLE ahoy_events MODIFY properties VARCHAR(5000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
   end
 end
