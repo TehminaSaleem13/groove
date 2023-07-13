4504a9be577f91898f210c4ac68a78218fd6c3ae
kapil <kchoudhary006@gmail.com>
2023-07-13 14:51:18 +0530

    Update failing migration

--- db/migrate/20230713072029_change_ahoy_event_to_utf8mb4.rb
+++ db/migrate/20230713072029_change_ahoy_event_to_utf8mb4.rb
@@ -1,5 +1,10 @@
 class ChangeAhoyEventToUtf8mb4 < ActiveRecord::Migration[5.1]
-  def change
-    execute "ALTER TABLE ahoy_events MODIFY properties VARCHAR(5000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+  def up
+    change_column :ahoy_events, :properties, :text
+    execute "ALTER TABLE ahoy_events MODIFY properties LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+  end
+
+  def down
+  	change_column :ahoy_events, :properties, :string
   end
 end
