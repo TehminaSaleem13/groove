bc56c082b4cf14387dc5d0369b44a7e8ef802d42
kapil <kchoudhary006@gmail.com>
2023-08-18 18:02:18 +0530

    GROOV-3187 | Add Specs for Inventory Importer

--- spec/lib/groovepacker/stores/importers/shopify/inventory_spec.rb
+++ spec/lib/groovepacker/stores/importers/shopify/inventory_spec.rb
@@ -3,24 +3,105 @@
 require 'rails_helper'
 
 describe Groovepacker::Stores::Importers::Shopify::Inventory do
-  let(:inv_wh) { create(:inventory_warehouse, is_default: true) }
-  let(:store) { create(:store, inventory_warehouse_id: inv_wh.id, store_type: 'Shopify') }
+  subject { described_class.new(tenant_name, shopify_store.id) }
 
-  describe '#inventory' do
-    let(:params) { { select_all: true } }
-    let(:credential) { create(:shopify_credential, store: store) }
-    let(:result) { described_class.new(Apartment::Tenant.current, credential.store_id).pull_inventories }
-    let(:shopify_product_variant_id) { '123123' }
-    let(:inventory_quantity) { 5 }
+  let(:tenant) { create(:tenant, name: Apartment::Tenant.current) }
+  let(:tenant_name) { tenant.name }
+  let(:shopify_store) { create(:store, :shopify) }
 
-    before do
-      product = create(:product, :with_sku_barcode, store_id: store.id)
-      create(:sync_option, product_id: product.id, sync_with_shopify: true, shopify_product_variant_id: shopify_product_variant_id)
-      allow_any_instance_of(Groovepacker::ShopifyRuby::Client).to receive(:get_variant).with(shopify_product_variant_id).and_return(id: shopify_product_variant_id, inventory_quantity: inventory_quantity)
+  let(:inv_level1) { { id: 123, inventory_item_id: 123, available: 2 }.with_indifferent_access }
+  let(:inv_level2) { { id: 456, inventory_item_id: 456, available: 1 }.with_indifferent_access }
+  let(:inventory_levels) { [inv_level1, inv_level2] }
+
+  before do
+    subject.send :init_credential_and_client
+  end
+
+  describe 'instance variables' do
+    it 'has tenant and store_id' do
+      expect(subject.tenant).to eq(tenant_name)
+      expect(subject.store_id).to eq(shopify_store.id)
+    end
+  end
+
+  describe 'methods' do
+    describe '#pull_inventories' do
+      let(:shopify_product_variant_id) { inv_level2[:id] }
+      let(:current_inv_qty) { 10 }
+      let(:new_inv_qty) { inv_level2[:available] }
+      let(:inv_qty_difference) { new_inv_qty - current_inv_qty }
+      let(:variant) do
+        {
+          id: inv_level2[:id],
+          product_id: inv_level2[:id],
+          title: 'Default Title',
+          inventory_item_id: inv_level2[:id],
+          inventory_quantity: current_inv_qty
+        }.with_indifferent_access
+      end
+      let(:product) { create(:product, :with_sku_barcode) }
+
+      before do
+        create(:sync_option, product_id: product.id, sync_with_shopify: true, shopify_product_variant_id: shopify_product_variant_id)
+        product.primary_warehouse.update(available_inv: current_inv_qty)
+        allow_any_instance_of(Groovepacker::ShopifyRuby::Client).to receive(:inventory_levels).and_return(inventory_levels)
+        allow_any_instance_of(Groovepacker::ShopifyRuby::Client).to receive(:get_variant).and_return(variant)
+      end
+
+      it 'performs the inventory import from shopify' do
+        expect { subject.pull_inventories }.to change { product.reload.primary_warehouse.quantity_on_hand }.by(inv_qty_difference)
+      end
+    end
+  end
+
+  describe 'private methods' do
+    describe '#send_pull_inventories_products_email' do
+      let(:csv_export_mailer) { double('CsvExportMailer', deliver: true) }
+
+      before do
+        allow(CsvExportMailer).to receive(:send_push_pull_inventories_products).and_return(csv_export_mailer)
+        subject.send :send_pull_inventories_products_email
+      end
+
+      it 'triggers an email' do
+        expect(CsvExportMailer).to have_received(:send_push_pull_inventories_products)
+      end
     end
 
-    # it 'Push Inventory' do
-    #   expect(result).to include(Product.first)
-    # end
+    describe '#inventory_levels' do
+      before do
+        allow_any_instance_of(Groovepacker::ShopifyRuby::Client).to receive(:inventory_levels).and_return(inventory_levels)
+      end
+
+      it 'returns inventory levels' do
+        expect(subject.send(:inventory_levels)).to match_array(inventory_levels)
+      end
+    end
+
+    describe '#get_inventory' do
+      let(:shopify_product_variant_id) { inv_level2[:id] }
+      let(:variant) do
+        {
+          id: inv_level2[:id],
+          product_id: inv_level2[:id],
+          title: 'Default Title',
+          inventory_item_id: inv_level2[:id],
+          inventory_quantity: 10
+        }.with_indifferent_access
+      end
+
+      let(:updated_variant) do
+        variant.merge(inventory_quantity: inv_level2[:available])
+      end
+
+      before do
+        allow_any_instance_of(Groovepacker::ShopifyRuby::Client).to receive(:inventory_levels).and_return(inventory_levels)
+        allow_any_instance_of(Groovepacker::ShopifyRuby::Client).to receive(:get_variant).and_return(variant.dup)
+      end
+
+      it 'returns inventory levels' do
+        expect(subject.send(:get_inventory, shopify_product_variant_id)).to eq(updated_variant)
+      end
+    end
   end
 end
