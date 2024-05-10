1e306cf1ef1215862bae10470a5bf8660a2cbe96
kapil <kchoudhary006@gmail.com>
2024-05-10 12:38:45 +0530

    Add Loggly Token to .env.development

--- config/initializers/loggly.rb
+++ config/initializers/loggly.rb
@@ -1,4 +1,4 @@
 require 'logglier'
 
 loggly_token = ENV['LOGGLY_TOKEN'] || 'no-token-available'
-Rails.application.config.loggly_logger = Logglier.new("https://logs-01.loggly.com/inputs/#{loggly_token}/tag/groovepacker-backend/", :threaded => true)
+Rails.application.config.loggly_logger = Logglier.new("https://logs-01.loggly.com/inputs/#{loggly_token}/tag/groovepacker-backend/", threaded: true, format: :json)
