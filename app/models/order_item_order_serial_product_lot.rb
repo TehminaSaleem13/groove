84d4eb161addbb8876755a34be6c7a1cf60a299c
kapil Choudhary <kchoudhary006@gmail.com>
2024-09-06 10:55:22 +0000

    Fix validation error
    * Fix validation error
    
    * Fix pipeline for rspec

--- app/models/order_item_order_serial_product_lot.rb
+++ app/models/order_item_order_serial_product_lot.rb
@@ -2,7 +2,7 @@
 
 class OrderItemOrderSerialProductLot < ApplicationRecord
   # attr_accessible :order_item_id, :order_serial_id, :product_lot_id, :qty
-  belongs_to :order_item
+  belongs_to :order_item, optional: true
   belongs_to :order_serial
   belongs_to :product_lot, optional: true
   #===========================================================================================
