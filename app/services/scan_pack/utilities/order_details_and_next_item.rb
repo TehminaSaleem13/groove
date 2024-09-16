307f51724582ff35c47f112b03553fddc03f2157
kapil Choudhary <kchoudhary006@gmail.com>
2024-09-16 07:04:13 +0000

    GROOV-3789 | Fixed multipack barcode issue
    * Fixed multipack barcode issue

--- app/services/scan_pack/utilities/order_details_and_next_item.rb
+++ app/services/scan_pack/utilities/order_details_and_next_item.rb
@@ -9,7 +9,7 @@ module ScanPack
         @single_order.reload
         data = @single_order.attributes
         data['next_item'] = {}
-        data['unscanned_items'] = @single_order.get_unscanned_items(most_recent_scanned_product: @session[:most_recent_scanned_product])
+        data['unscanned_items'] = @single_order.get_unscanned_items(most_recent_scanned_product: @session[:most_recent_scanned_product], limit: nil)
         data['scanned_items'] = @single_order.get_scanned_items
         data['scanning_count'] = @single_order.scanning_count
         do_if_unscanned_items_present(data) unless data['unscanned_items'].empty?
