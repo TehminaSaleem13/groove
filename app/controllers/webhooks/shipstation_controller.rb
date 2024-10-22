9af837987a515a1d7007da2e7918c8e69e1d207d
kapil Choudhary <kchoudhary006@gmail.com>
2024-10-22 07:36:01 +0000

    GROOV-3809-Add-logging (pull request #1042)
    Added logging for SS webhook import
    
    * Added logging for SS webhook import

--- app/controllers/webhooks/shipstation_controller.rb
+++ app/controllers/webhooks/shipstation_controller.rb
@@ -2,6 +2,7 @@ class Webhooks::ShipstationController < ApplicationController
   before_action :set_store
 
   def import
+    Groovepacker::LogglyLogger.log(Apartment::Tenant.current, "ss-webhook-import-order", { params: params })
     end_point = URI.parse(params['resource_url']).query
     import_orders = ImportOrders.new
     import_orders.delay(priority: 95, queue: "process_ss_webhook_import_order_#{Apartment::Tenant.current}").import_ss_webhook_order(end_point, params['resource_type'], @store.id, Apartment::Tenant.current) if end_point.present?
